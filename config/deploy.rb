90cb345c9e24541a17bd96acaf991ca8b0fa8ddb
chrysalag <chrysa.lagodimou@gmail.com>
2017-04-05 18:21:50 +0200

    set panter rails deploy

---
+++ config/deploy.rb
@@ -0,0 +1,5 @@
+# config valid only for current version of Capistrano
+lock "3.7.2"
+
+set :application, "AOZ-003"
+set :repo_url, "git@github.com:panter/aoz-003.git"
---
+++ config/deploy/production.rb
@@ -0,0 +1,2 @@
+server 'aoz-003-production.panter.biz', roles: %w[ web app db ]
+set :branch, 'develop'
