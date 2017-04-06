8ce133d8bd78d916ef7c99ef37ef57e4ebd41a50
Kaspar Vollenweider <vok@panter.ch>
2017-04-06 16:12:00 +0200

    chore: remove junc config include

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,6 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-configs = YAML.load_file('../../config/database.yml')
-ActiveRecord::Base.configurations = configs
-
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
