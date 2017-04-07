a9bbb2bb1a49e0a54f20b750ccd064968c29f911
chrysalag <chrysa.lagodimou@gmail.com>
2017-04-07 14:26:13 +0200

    first signup form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.all
+    authorize User
+  end
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    authorize @user
+    if @user.update_attributes(secure_params)
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:user).permit(:id, :email, :password, :password_confirmation, :role)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :clients
   devise_for :users
+  resources :users
 
   root 'application#logged_in'
 end
