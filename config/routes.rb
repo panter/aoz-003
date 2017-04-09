14abac4d7089130654d20007a7b8e9691dd4f990
Kaspar Vollenweider <vok@panter.ch>
2017-04-09 18:54:40 +0200

    user can change password and email plus return buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :profiles
 
+  resource :user, only: [:edit,:edit_email] do
+    collection do
+      patch 'update_password'
+      patch 'update_email'
+      get 'edit_email'
+    end
+  end
+
   resources :profiles, except: [:index, :destroy]
   root 'application#logged_in'
 end
