d9e633faaa78d6f5022558e44f11c48c695e70d0
chrysalag <chrysa.lagodimou@gmail.com>
2017-04-11 14:38:47 +0200

    tests added

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, except: :destroy
 
-  #as :user do
-  #  get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-  #  put 'users' => 'devise/registrations#update', :as => 'user_registration'
-  #end
-
   root 'application#logged_in'
 end
