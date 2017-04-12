ab413168ed2193e0706f69013c349621315714a2
Kaspar Vollenweider <vok@panter.ch>
2017-04-12 08:57:47 +0200

    The user should be able to change his email address and password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
   end
@@ -10,9 +9,35 @@ class UsersController < ApplicationController
       @user.confirm
       @user.send_reset_password_instructions
       flash[:notice] = "Invitation sent to #{@user.email}"
-      render 'new'
-    else
-      render 'new'
+    end
+    render 'new'
+  end
+
+  def edit_password
+    @user = current_user
+  end
+
+  def update_password
+    @user = User.find(current_user.id)
+    return render :edit unless @user.update_with_password(password_params)
+    respond_to do |format|
+      @user.send_password_change_notification
+      bypass_sign_in @user, scope: :user
+      format.html { redirect_to @user.profile }
+    end
+  end
+
+  def edit_email
+    @user = current_user
+  end
+
+  def update_email
+    @user = User.find(current_user.id)
+    return render :edit_email unless @user.update(email_params)
+    respond_to do |format|
+      @user.send_email_changed_notification
+      bypass_sign_in @user, scope: :user
+      format.html { redirect_to @user.profile }
     end
   end
 
@@ -25,6 +50,14 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def email_params
+    params.require(:user).permit(:email)
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation, :current_password)
+  end
 
   def secure_params
     params.require(:user).permit(:id, :email, :password, :password_confirmation, :role)
