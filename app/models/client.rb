b1b89003af306de1b4eb63ca2268320cf0b86eaf
Tugce Nur Tas <ttn@gmx.ch>
2017-04-12 17:36:20 +0200

    add test

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,6 @@ class Client < ApplicationRecord
   has_many :relatives, dependent: :destroy
   belongs_to :user
   accepts_nested_attributes_for :language_skills, :relatives
+  validates :firstname, presence: true
+  validates :lastname, presence: true
 end
