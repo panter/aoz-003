3d87b7f587fa2950de39d63e49286b9da1432c4e
Kaspar Vollenweider <vok@panter.ch>
2017-04-12 16:17:26 +0200

    setup choice between chrome, phantomjs and phantomjs remote_debugging driver

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,18 @@
-require "test_helper"
+require 'test_helper'
+require 'capybara/poltergeist'
+
+Capybara.register_driver :poltergeist_debug do |app|
+  Capybara::Poltergeist::Driver.new app, inspector: true
+end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  screen_size = [1400, 1400]
+  case ENV['driver']
+  when 'chrome'
+    driven_by :selenium, using: :chrome, screen_size: screen_size
+  when 'poltergeist_debug'
+    driven_by :poltergeist_debug, screen_size: screen_size
+  else
+    driven_by :poltergeist, screen_size: screen_size
+  end
 end
