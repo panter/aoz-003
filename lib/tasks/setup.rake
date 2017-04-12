bd0f327100142fc65a822a938d1525b10b735616
Andi Staub <andi.staub@gmail.com>
2017-04-12 11:38:47 +0200

    Refactor superadmin rake task and add tests

--- lib/tasks/setup.rake
+++ lib/tasks/setup.rake
@@ -2,17 +2,9 @@ namespace :setup do
   desc "Create a superadmin user and send him a password reset mail"
   task superadmin: :environment do
     if ENV['email'].present?
-      user = User.new(
-        email: ENV['email'], password: Devise.friendly_token, role: 'superadmin'
-      )
-
-      if user.save!
-        user.send_reset_password_instructions
-
-        puts "Invitation sent to #{ user.email }"
-      end
+      User.create_user_and_send_password_reset email: ENV['email']
     else
-      puts "No email set, run `rails setup:superadmin email=email@test.com`"
+      warn 'No email set, run `rails setup:superadmin email=email@test.com`'
     end
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,31 @@ class UserTest < ActiveSupport::TestCase
 
     assert user.valid?
   end
+
+  test '#create_user_and_send_password_reset \
+    with new email creates new superadmin' do
+    out, err = capture_io do
+      assert_difference 'User.count', 1 do
+        User.create_user_and_send_password_reset email: 'superadmin@example.com'
+      end
+    end
+
+    assert_equal out, "Mail sent to superadmin@example.com\n"
+
+    user = User.first
+
+    assert_equal user.email, 'superadmin@example.com'
+    assert_equal user.role, 'superadmin'
+  end
+
+  test '#create_user_and_send_password_reset \
+    with taken email does not create superadmin' do
+    create :user, email: 'superadmin@example.com'
+
+    capture_io do
+      assert_raises(ActiveRecord::RecordInvalid) do
+        User.create_user_and_send_password_reset email: 'superadmin@example.com'
+      end
+    end
+  end
 end
