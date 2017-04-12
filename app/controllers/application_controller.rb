db5b8aee477aa9937cc3908867bcd10ae94792ae
Kaspar Vollenweider <vok@panter.ch>
2017-04-12 08:56:49 +0200

    After the superadmin has logged in he needs to fill out his user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  def after_sign_in_path_for(current_user)
+    return new_profile_path if current_user.profile.blank?
+    root_path
+  end
+
   def logged_in; end
 end
