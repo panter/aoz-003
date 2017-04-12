53a56e695286ae1cc0f54a71b1ab53361b0990c1
Andi Staub <andi.staub@gmail.com>
2017-04-12 11:38:47 +0200

    Refactor superadmin rake task and add tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
     :trackable, :validatable
 
   validates :role, inclusion: { in: ['superadmin'] }
+
+  def self.create_user_and_send_password_reset email:
+    user = User.new(
+      email: email, password: Devise.friendly_token, role: 'superadmin'
+    )
+
+    if user.save!
+      user.send_reset_password_instructions
+      puts "Mail sent to #{ user.email }"
+    end
+  end
 end
