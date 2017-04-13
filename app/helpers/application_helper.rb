1fbd18418b363dd24c37f181b467d699ecf2c258
chrysalag <chrysa.lagodimou@gmail.com>
2017-04-13 16:04:58 +0200

    add views and partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def permit_collection
+    { 'L' => 'l', 'B' => 'b' }
+  end
+
+  def gender_collection
+    { 'female' => 'f', 'male' => 'm' }
+  end
+
+  def language_level_collection
+    { 'native speaker' => 'native', 'fluent' => 'fluent', 'good' => 'good',
+      'basic' => 'basic' }
+  end
+
+  def state_collection
+    { 'registered' => 'registered', 'reserved' => 'reserved',
+      'active' => 'active', 'finished' => 'finished', 'rejected' => 'rejected'}
+  end
 end
