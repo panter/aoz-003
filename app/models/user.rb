a74436f177d77b14775c440c7c2c64aa6f723126
Andi Staub <andi.staub@gmail.com>
2017-04-18 14:53:29 +0200

    Cleanup user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable,
     :trackable, :validatable
 
-  validates :role, inclusion: { in: ['superadmin'] }
-  validates :password, presence: true
+  SUPERADMIN = 'superadmin'.freeze
 
-  def self.create_user_and_send_password_reset email:
+  validates :role, inclusion: { in: [SUPERADMIN] }
+
+  def self.create_user_and_send_password_reset(email:)
     user = User.new(
       email: email, password: Devise.friendly_token, role: 'superadmin'
     )
 
     if user.save!
       user.send_reset_password_instructions
-      puts "Mail sent to #{ user.email }"
+      puts "Mail sent to #{user.email}"
     end
   end
 end
