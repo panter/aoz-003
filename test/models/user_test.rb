b6feb4c696caac16ca575aa3901b8324edfff465
Andi Staub <andi.staub@gmail.com>
2017-04-18 14:52:10 +0200

    Remove unnecesary tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,50 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
-  def setup
-    @user = build :user
-  end
-
   test 'valid factory' do
-    assert @user.valid?
-  end
-
-  test 'email should be present' do
-    @user.email = ' '
-    assert_not @user.valid?
-  end
-
-  test 'role should be present' do
-    @user.role = '  '
-    assert_not @user.valid?
-  end
-
-  test 'password should be present' do
-    @user.password = '  '
-    assert_not @user.valid?
-  end
-
-  test 'password should be present (nonblank)' do
-    @user.password = @user.password_confirmation = ' ' * 6
-    assert_not @user.valid?
-  end
-
-  test 'password should have a minimum length' do
-    @user.password = @user.password_confirmation = 'a' * 5
-    assert_not @user.valid?
-  end
-
-  test 'email addresses should be unique' do
-    duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
-    @user.save
-    assert_not duplicate_user.valid?
+    user = build :user
+    assert user.valid?
   end
 
   test '#create_user_and_send_password_reset \
     with new email creates new superadmin' do
-    out, err = capture_io do
+    out, _err = capture_io do
       assert_difference 'User.count', 1 do
         User.create_user_and_send_password_reset email: 'superadmin@example.com'
       end
