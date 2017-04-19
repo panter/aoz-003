43af90c6417a1ea0d87b35b6ea297e4333abaae1
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-04-19 14:44:16 +0200

    add devise_invitable to users, change user controller, edit views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419090833) do
+ActiveRecord::Schema.define(version: 20170419120951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,14 @@ ActiveRecord::Schema.define(version: 20170419090833) do
     t.string "role", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer "invitation_limit"
+    t.integer "invited_by_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
