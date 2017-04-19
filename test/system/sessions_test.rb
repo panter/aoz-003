dfa715a8e85ed2341460d6c3352e57f06d3ed3e8
Kaspar Vollenweider <vok@panter.ch>
2017-04-19 18:28:22 +0200

    adapting the system tests to suite the new profile avatar

--- test/system/sessions_test.rb
+++ test/system/sessions_test.rb
@@ -2,8 +2,7 @@ require 'application_system_test_case'
 
 class SessionsTest < ApplicationSystemTestCase
   setup do
-    profile = create :profile
-    @superadmin = User.find profile.user_id
+    @superadmin = create :user_with_profile
   end
 
   test 'redirects to login page if not authenticated' do
@@ -20,20 +19,25 @@ class SessionsTest < ApplicationSystemTestCase
 
     fill_in 'Email', with: @superadmin.email
     fill_in 'Password', with: 'asdfasdf'
-    click_button 'Log in'
-
-    assert page.has_current_path? user_path(@superadmin)
+    click_on 'Log in'
+    assert page.has_current_path? root_path
     assert page.has_text? 'Signed in successfully.'
-    assert page.has_link? 'superadmin@example.com'
+
+    click_on 'Toggle navigation' if page.has_button? 'Toggle navigation'
+
+    assert page.has_link? @superadmin.email
   end
 
   test 'sign out current user' do
     login_as @superadmin, scope: :user
     visit root_path
 
-    click_link 'superadmin@example.com'
+    # trigger hamburger on mobile views
+    click_on 'Toggle navigation' if page.has_button? 'Toggle navigation'
+
+    click_on @superadmin.email
     assert page.has_link? 'Logout'
-    click_link 'Logout'
+    click_on 'Logout'
 
     assert page.has_current_path? new_user_session_path
     assert page.has_text? 'You need to sign in or sign up before continuing.'
