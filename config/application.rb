2324d07d2f5720576b2a808dd6d7e0ca55800433
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-04-20 13:41:13 +0200

    remove default locale set

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module Aoz
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Set german as default language
-    config.i18n.default_locale = :de
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
