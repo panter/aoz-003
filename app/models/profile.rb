a21c929f8dd26652d9998cdbecd788a03f375e58
Kaspar Vollenweider <vok@panter.ch>
2017-04-20 10:32:55 +0200

    better file upload validation for avatar image

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,9 +2,8 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_attached_file :avatar, styles: { thumb: '100x100#' }
 
-  validates_attachment :avatar, content_type: {
-    content_type: ['image/jpeg', 'image/gif', 'image/png']
-  }
-  validates_attachment_file_name :avatar, matches: [/png\z/, /jpe?g\z/]
   validates :first_name, :last_name, presence: true
+  validates_attachment :avatar, content_type: {
+    content_type: /\Aimage\/.*\z/
+  }
 end
