8ae22cc59fef7eb6b24f1ec79b63872a3f4c0510
Kaspar Vollenweider <vok@panter.ch>
2017-04-20 16:41:12 +0200

    adding has_vcards gem and bundle

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419090833) do
+ActiveRecord::Schema.define(version: 20170420144523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "has_vcards_addresses", force: :cascade do |t|
+    t.string "post_office_box"
+    t.string "extended_address"
+    t.string "street_address"
+    t.string "locality"
+    t.string "region"
+    t.string "postal_code"
+    t.string "country_name"
+    t.integer "vcard_id"
+    t.string "address_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["vcard_id"], name: "addresses_vcard_id_index"
+  end
+
+  create_table "has_vcards_phone_numbers", force: :cascade do |t|
+    t.string "number"
+    t.string "phone_number_type"
+    t.integer "vcard_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["phone_number_type"], name: "index_has_vcards_phone_numbers_on_phone_number_type"
+    t.index ["vcard_id"], name: "phone_numbers_vcard_id_index"
+  end
+
+  create_table "has_vcards_vcards", force: :cascade do |t|
+    t.string "full_name"
+    t.string "nickname"
+    t.string "family_name"
+    t.string "given_name"
+    t.string "additional_name"
+    t.string "honorific_prefix"
+    t.string "honorific_suffix"
+    t.boolean "active", default: true
+    t.string "type"
+    t.integer "reference_id"
+    t.string "reference_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["active"], name: "index_has_vcards_vcards_on_active"
+    t.index ["reference_id", "reference_type"], name: "index_has_vcards_vcards_on_reference_id_and_reference_type"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.bigint "user_id"
