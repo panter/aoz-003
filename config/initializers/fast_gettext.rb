4bbf66abbec6712d7e1870a64e5d049299723304
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-04-21 10:47:08 +0200

    set default locale

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,3 +1,4 @@
 FastGettext.add_text_domain 'app', path: 'locale', type: :po
 FastGettext.default_available_locales = ['en', 'de'] # all you want to allow
 FastGettext.default_text_domain = 'app'
+FastGettext.default_locale = ENV['DEFAULT_LANGUAGE'].present? ? ENV['DEFAULT_LANGUAGE'] : 'en'
