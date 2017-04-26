3b17bbe44a1e2dccf1ec6f0fcc95f23dd47799bb
Kaspar Vollenweider <vok@panter.ch>
2017-04-26 10:45:23 +0200

    model user test fixed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = build :user
-    @superadmin = create :superadmin
-    @admin = create :admin
-    @social_worker = create :social_worker
+    @user_as_superadmin = create :user, :with_profile, role: 'superadmin'
+    @admin = create :user, :with_profile, role: 'admin'
+    @user_as_social_worker = create :user, :with_profile, role: 'social_worker'
   end
 
   test 'valid factory' do
@@ -60,27 +60,27 @@ class UserTest < ActiveSupport::TestCase
 
   test '#create_user_and_send_password_reset \
     with taken email does not create superadmin' do
-    create :user
+    create :user, email: 'superadmin@example.com'
 
     capture_io do
       assert_raises(ActiveRecord::RecordInvalid) do
         User.create_user_and_send_password_reset email: 'superadmin@example.com',
-          role: User::SUPERADMIN
+          role: 'superadmin'
       end
     end
   end
 
-  test '@superadmin.superadmin? returns true if user is superadmin' do
-    assert @superadmin.superadmin?
+  test '@user_as_superadmin.superadmin? returns true if user is superadmin' do
+    assert @user_as_superadmin.superadmin?
   end
 
-  test '@superadmin.admin?, .social_worker? returns false' do
-    refute @superadmin.admin?
-    refute @superadmin.social_worker?
+  test '@user_as_superadmin.admin?, .social_worker? returns false' do
+    refute @user_as_superadmin.admin?
+    refute @user_as_superadmin.social_worker?
   end
 
   test '@admin.admin? returns true if user is admin' do
-    assert @superadmin.superadmin?
+    assert @user_as_superadmin.superadmin?
   end
 
   test '@admin.superadmin?, .social_worker? returns false' do
@@ -88,13 +88,13 @@ class UserTest < ActiveSupport::TestCase
     refute @admin.social_worker?
   end
 
-  test '@social_worker.social_worker? returns true if user is of role socialworker' do
-    assert @social_worker.social_worker?
+  test '@user_as_social_worker.social_worker? returns true if user is of role socialworker' do
+    assert @user_as_social_worker.social_worker?
   end
 
-  test '@social_worker.superadmin?, .admin? returns false' do
-    refute @social_worker.superadmin?
-    refute @social_worker.admin?
+  test '@user_as_social_worker.superadmin?, .admin? returns false' do
+    refute @user_as_social_worker.superadmin?
+    refute @user_as_social_worker.admin?
   end
 
   context 'associations' do
