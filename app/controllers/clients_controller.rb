1b044c1474de41aac5ca8677774477662fd1c455
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-04-26 23:01:49 +0200

    delete json builders

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -21,10 +21,8 @@ class ClientsController < ApplicationController
     respond_to do |format|
       if @client.save!
         format.html { redirect_to @client, notice: 'Client was successfully created.' }
-        format.json { render :show, status: :created, location: @client }
       else
         format.html { render :new }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
       end
     end
   end
