ab7acea99a7c9db98f2fd72182085b78b7a2390a
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-04-26 15:30:54 +0200

    set default locale de

---
+++ config/initializers/locale.rb
@@ -0,0 +1,8 @@
+# Where the I18n library should search for translation files
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{yml}')]
+
+# Available locales
+I18n.available_locales = [:en, :de]
+
+# Set default locale to something other than :en
+I18n.default_locale = :de
