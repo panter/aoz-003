0e5318e6aca2cd950261ee921aaef289c2ec2ec3
Kaspar Vollenweider <vok@panter.ch>
2017-04-26 10:45:23 +0200

    model user test fixed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = build :user
+    @user_as_superadmin = create :user, :with_profile, role: 'superadmin'
+    @admin = create :user, :with_profile, role: 'admin'
+    @user_as_social_worker = create :user, :with_profile, role: 'social_worker'
+  end
+
   test 'valid factory' do
     user = build :user
     assert user.valid?
@@ -24,16 +31,44 @@ class UserTest < ActiveSupport::TestCase
 
   test '#create_user_and_send_password_reset \
     with taken email does not create superadmin' do
-    subject_user = create :user
+    create :user, email: 'superadmin@example.com'
 
     capture_io do
       assert_raises(ActiveRecord::RecordInvalid) do
-        User.create_user_and_send_password_reset email: subject_user.email
+        User.create_user_and_send_password_reset email: 'superadmin@example.com',
+          role: 'superadmin'
       end
     end
   end
 
-  context "associations" do
+  test '@user_as_superadmin.superadmin? returns true if user is superadmin' do
+    assert @user_as_superadmin.superadmin?
+  end
+
+  test '@user_as_superadmin.admin?, .social_worker? returns false' do
+    refute @user_as_superadmin.admin?
+    refute @user_as_superadmin.social_worker?
+  end
+
+  test '@admin.admin? returns true if user is admin' do
+    assert @user_as_superadmin.superadmin?
+  end
+
+  test '@admin.superadmin?, .social_worker? returns false' do
+    refute @admin.superadmin?
+    refute @admin.social_worker?
+  end
+
+  test '@user_as_social_worker.social_worker? returns true if user is of role socialworker' do
+    assert @user_as_social_worker.social_worker?
+  end
+
+  test '@user_as_social_worker.superadmin?, .admin? returns false' do
+    refute @user_as_social_worker.superadmin?
+    refute @user_as_social_worker.admin?
+  end
+
+  context 'associations' do
     should have_many(:clients)
   end
 end
