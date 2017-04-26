588a214bb3537206b9d8ac7dee0e6f706734cc92
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-04-26 23:04:19 +0200

    delete destroy method  Please enter the commit message for your changes. Lines starting

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -31,19 +31,9 @@ class ClientsController < ApplicationController
     respond_to do |format|
       if @client.update(client_params)
         format.html { redirect_to @client, notice: 'Client was successfully updated.' }
-        format.json { render :show, status: :ok, location: @client }
       else
-        format.html { render :edit }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
+        format.html { render :edit } unless @client.update(profile_params)
       end
-    end
-  end
-
-  def destroy
-    @client.destroy
-    respond_to do |format|
-      format.html { redirect_to clients_url, notice: 'Client was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
