63fbb64962bd06a511eb017e741566937e2f419e
Kaspar Vollenweider <vok@panter.ch>
2017-04-26 18:47:46 +0200

    pundit: rescue error message for unauthorized access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,20 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   def after_sign_in_path_for(current_user)
     if current_user.profile.blank?
       new_profile_path
     else
       root_path
     end
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized to perform this action.'
+    redirect_to(request.referer || root_path)
   end
 end
