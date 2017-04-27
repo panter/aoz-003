ad1a58fa0614dea4a8b0bdbfa22453116bcedd78
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-04-27 13:20:39 +0200

    devise(and invitable) translations, restore default locale

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -5,4 +5,4 @@ I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{yml}')]
 I18n.available_locales = [:en, :de]
 
 # Set default locale to something other than :en
-# I18n.default_locale = :de
+I18n.default_locale = :de
