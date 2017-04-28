815e1b6b8c8e4fad7cf3d5e905ef5391be6cf531
Kaspar Vollenweider <vok@panter.ch>
2017-04-28 11:56:24 +0200

    fixed profiles system test

--- test/system/profiles_test.rb
+++ test/system/profiles_test.rb
@@ -21,38 +21,25 @@ class ProfilesTest < ApplicationSystemTestCase
     assert page.has_field? 'Avatar'
     assert page.has_field? 'Address'
     assert page.has_field? 'Profession'
+    assert page.has_field? name: 'profile[monday]'
+    assert page.has_field? name: 'profile[tuesday]'
+    assert page.has_field? name: 'profile[wednesday]'
+    assert page.has_field? name: 'profile[thursday]'
+    assert page.has_field? name: 'profile[friday]'
 
     fill_in 'First name', with: 'Hans'
     fill_in 'Last name', with: 'Muster'
+
+    page.check(name: 'profile[monday]')
     click_button 'Create Profile'
 
     assert page.has_current_path? profile_path(@user_without_profile.profile.id)
     assert page.has_text? 'Hans'
     assert page.has_text? 'Muster'
     assert page.has_text? 'Profile was successfully created.'
-  end
-
-  test 'when profile created it can be displayed' do
-    visit new_user_session_path
-    fill_in 'Email', with: @user_without_profile.email
-    fill_in 'Password', with: 'asdfasdf'
-    click_button 'Log in'
-
-    fill_in 'First name', with: 'Hans'
-    fill_in 'Last name', with: 'Muster'
-    click_button 'Create Profile'
-
-    assert page.has_link? @user_without_profile.email
-
-    click_link @user_without_profile.email
-
-    assert page.has_link? 'Show profile'
-
-    click_link 'Show profile'
-
-    assert page.has_text? 'Hans, Muster'
-    assert page.has_text? @user_without_profile.profile.first_name
-    assert page.has_text? @user_without_profile.profile.last_name
+    assert page.has_selector?('table > tbody td:nth-child(1) i.glyphicon-ok')
+    refute page.has_selector?('table > tbody td:nth-child(2) i.glyphicon-ok')
+    assert page.has_selector?('table > tbody td:nth-child(2) i.glyphicon-remove')
   end
 
   test 'user can change the password from profile page' do
