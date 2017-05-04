aa3950c2c06eeffbe6df3e6c37d276a0c6e73960
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-05-04 11:56:16 +0200

    add client translations

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,4 +4,4 @@ I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{yml}')]
 # Available locales
 I18n.available_locales = [:en, :de]
 
-I18n.default_locale = ENV['RAILS_LANGUAGE'].present? ? ENV['RAILS_LANGUAGE'].to_sym : :en
+I18n.default_locale = ENV['RAILS_LANGUAGE'].present? ? ENV['RAILS_LANGUAGE'].to_sym : :de
