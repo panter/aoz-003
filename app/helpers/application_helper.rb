cd05a2c84eb5a64a5b2227a4f9f9d1f4e5437565
Kaspar Vollenweider <vok@panter.ch>
2017-05-07 23:43:35 +0200

    clean the application helper up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,33 +1,9 @@
 module ApplicationHelper
-  def permit_collection
-    [:L, :B]
-  end
-
-  def gender_collection
-    [:female, :male]
-  end
-
-  def language_level_collection
-    [:native_speaker, :fluent, :good, :basic]
-  end
-
-  def state_collection
-    [:registered, :reserved, :active, :finished, :rejected]
-  end
-
   def week(with_weekend = false)
     if with_weekend
       [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
     else
       [:monday, :tuesday, :wednesday, :thursday, :friday]
     end
-  end
-
-  def t_c(key_name)
-    t("#{@_controller.controller_name}.#{key_name}")
-  end
-
-  def t_ca(key_name)
-    t("#{@_controller.controller_name}.#{action_name}.#{key_name}")
   end
 end
