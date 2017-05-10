11f1c41f2257c7574100e090f7b29d7774f69b66
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-05-10 19:24:22 +0200

    add first policy test for user deletion

--- test/policies/user_policy_test.rb
+++ test/policies/user_policy_test.rb
@@ -8,6 +8,16 @@ class UserPolicyTest < ActiveSupport::TestCase
     @user_as_department_manager = create :user, :with_profile, role: 'department_manager'
   end
 
+  test 'Destroy: superadmin can destroy other users' do
+    assert_permit @user_as_superadmin, @user_as_other_superadmin, 'destroy?'
+    assert_permit @user_as_superadmin, @user_as_social_worker, 'destroy?'
+    assert_permit @user_as_superadmin, @user_as_department_manager, 'destroy?'
+  end
+
+  test 'Destroy: superadmin cannot destroy itself' do
+    refute_permit @user_as_superadmin, @user_as_superadmin, 'destroy?'
+  end
+
   test 'New: superadmin can create user' do
     assert_permit @user_as_superadmin, User, 'new?', 'create?'
   end
