d0bf6869669be2d1f040f60c60eb493c41d45010
Kaspar Vollenweider <vok@panter.ch>
2017-05-16 15:12:12 +0200

    validate department name presence

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,14 @@
 class Contact < ApplicationRecord
   belongs_to :contactable, polymorphic: true, optional: true
 
+  validates :name, presence: true, if: :of_department?
+
   has_many :contact_emails
   accepts_nested_attributes_for :contact_emails, allow_destroy: true
   has_many :contact_phones
   accepts_nested_attributes_for :contact_phones, allow_destroy: true
+
+  def of_department?
+    contactable_type == 'Department'
+  end
 end
--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,10 @@
 class Department < ApplicationRecord
   has_one :contact, as: :contactable
-  accepts_nested_attributes_for :contact, allow_destroy: true
+  accepts_nested_attributes_for :contact
 
   has_and_belongs_to_many :user
+
+  def to_s
+    contact.name
+  end
 end
