07366289624829a3095c91e756ead1f9a2ef182b
Kaspar Vollenweider <vok@panter.ch>
2017-05-17 18:32:49 +0200

    fix wrong ordered route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resources :departments
   resources :users
   resources :clients
+  resources :departments
   resources :profiles, except: [:destroy, :index]
+  resources :users
 
   root 'application#home'
 end
