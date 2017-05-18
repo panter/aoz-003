21050470e730e0954f815df8d16aff86b6cf40f2
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-05-18 13:45:40 +0200

    has many in concern

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,14 +1,7 @@
 class Client < ApplicationRecord
+  include Reusable
+
   belongs_to :user
-
-  has_many :language_skills, as: :languageable, dependent: :destroy
-  accepts_nested_attributes_for :language_skills, allow_destroy: true
-
-  has_many :relatives, as: :relativeable, dependent: :destroy
-  accepts_nested_attributes_for :relatives, allow_destroy: true
-
-  has_many :schedules, as: :scheduleable, dependent: :destroy
-  accepts_nested_attributes_for :schedules
 
   validates :first_name, :last_name, presence: true
 
--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,14 +1,7 @@
 class Volunteer < ApplicationRecord
+  include Reusable
+
   acts_as_paranoid
-
-  has_many :language_skills, as: :languageable, dependent: :destroy
-  accepts_nested_attributes_for :language_skills, allow_destroy: true
-
-  has_many :relatives, as: :relativeable, dependent: :destroy
-  accepts_nested_attributes_for :relatives, allow_destroy: true
-
-  has_many :schedules, as: :scheduleable, dependent: :destroy
-  accepts_nested_attributes_for :schedules
 
   has_attached_file :avatar, styles: { thumb: '100x100#' }
 
