6c5ed2b6c38cbf5653a2f7867fe3636fbee96fea
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-05-19 17:55:53 +0200

    factories array new

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -3,15 +3,18 @@ FactoryGirl.define do
     sequence :email { |n| "superadmin#{n}@example.com" }
     password 'asdfasdf'
     role 'superadmin'
+
     trait :with_profile do
       profile { |u| u.association(:profile, first_name: :role) }
     end
+
     trait :with_clients do
       client_amt = 5
       clients do |client|
         1.step(client_amt).to_a.map { |n| client.association(:client) }
       end
     end
+
     trait :with_departments do
       department do
         Array.new(3).map { |_n| create(:department) }
