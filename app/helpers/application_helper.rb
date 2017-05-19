8d054294dc75f354db5812e2e8305446ab657c2e
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-05-19 12:39:46 +0200

    dry client, volunteer forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,10 @@ module ApplicationHelper
       [:monday, :tuesday, :wednesday, :thursday, :friday]
     end
   end
+
+  def link_to_add_polymorphic_association(*args)
+    name, f, association, html_options = *args
+    html_options[:partial] = "#{association}/fields"
+    link_to_add_association(name, f, association, html_options)
+  end
 end
