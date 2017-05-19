e46a90e43b7d1b1f15d7c5b76588404f6e69594c
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-05-19 17:33:49 +0200

    remove nationality name

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,10 +4,4 @@ class Client < ApplicationRecord
   belongs_to :user
 
   validates :first_name, :last_name, presence: true
-
-  def nationality_name
-    return '' if nationality.blank?
-    c = ISO3166::Country[nationality]
-    c.translations[I18n.locale.to_s] || c.name
-  end
 end
