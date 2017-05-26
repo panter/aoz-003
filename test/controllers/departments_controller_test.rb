bc371a5c69dce7c088c5d7f05b3ed434ee377c08
Kaspar Vollenweider <vok@panter.ch>
2017-05-26 15:01:06 +0200

    testing department parameter policies thru controller test

---
+++ test/controllers/departments_controller_test.rb
@@ -0,0 +1,70 @@
+require 'test_helper'
+
+class DepartmentsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @superadmin = create :user, :with_profile, :with_clients,
+      :with_departments, role: 'superadmin'
+    @social_worker = create :user, :with_profile, :with_clients,
+      :with_departments, role: 'social_worker'
+    @department_manager = create :user, :with_profile, :with_departments,
+      role: 'department_manager'
+  end
+
+  test 'superadmin can submit user associations' do
+    login_as @superadmin
+    params = {
+      department: {
+        user_ids: [
+          User.where(role: 'superadmin').last.id,
+          User.where(role: 'department_manager').last.id,
+          ''
+        ],
+        contact_attributes: { name: 'asdf' }
+      },
+      commit: 'Create Department'
+    }
+    assert_difference 'Department.count', 1 do
+      post departments_path, params: params
+    end
+  end
+
+  test 'superadmin can update user_ids' do
+    login_as @superadmin
+    department = @superadmin.department.first
+    params = {
+      department: {
+        user_ids: [
+          @superadmin.id,
+          @department_manager.id,
+          ''
+        ],
+        contact_attributes: { name: 'Another name' }
+      },
+      commit: 'Update Department'
+    }
+    put department_path(department.id), params: params
+    department_updated = Department.find department.id
+    assert department_updated.contact.name == 'Another name'
+    assert department_updated.user_ids.include? @department_manager.id
+  end
+
+  test 'department_manager can not submit user associations' do
+    login_as @department_manager
+    department = @department_manager.department.first
+    params = {
+      department: {
+        user_ids: [
+          @superadmin.id,
+          @department_manager.id,
+          ''
+        ],
+        contact_attributes: { name: 'new name' }
+      },
+      commit: 'Update Department'
+    }
+    put department_path(department.id), params: params
+    department_updated = Department.find department.id
+    assert department_updated.contact.name == 'new name'
+    refute department_updated.user_ids.include? @superadmin.id
+  end
+end
