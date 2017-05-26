66247d4a3569f355ef403ebdf16262db55ace155
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-05-26 15:11:58 +0200

    user-volunteer association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519123207) do
+ActiveRecord::Schema.define(version: 20170526130435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -215,10 +215,13 @@ ActiveRecord::Schema.define(version: 20170519123207) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.bigint "user_id"
     t.index ["deleted_at"], name: "index_volunteers_on_deleted_at"
+    t.index ["user_id"], name: "index_volunteers_on_user_id"
   end
 
   add_foreign_key "clients", "users"
   add_foreign_key "contact_points", "contacts"
   add_foreign_key "profiles", "users"
+  add_foreign_key "volunteers", "users"
 end
