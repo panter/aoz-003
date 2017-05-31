d5b2df09924978be8011ab0fb2031414dd23991c
Kaspar Vollenweider <vok@panter.ch>
2017-05-31 15:11:35 +0200

    putting things for params back in order

--- app/controllers/volunteer_applications_controller.rb
+++ app/controllers/volunteer_applications_controller.rb
@@ -23,9 +23,9 @@ class VolunteerApplicationsController < ApplicationController
 
   def volunteer_params
     params.require(:volunteer).permit(
-      volunteer_attributes: volunteer_attributes.push(
-        language_skills_attributes, relatives_attributes, schedules_attributes
-      )
+      volunteer_attributes: [
+        volunteer_attributes, language_skills_attributes, relatives_attributes, schedules_attributes
+      ]
     )
   end
 end
