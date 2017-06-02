57e8cf7891825448ad8c858068c9b2cd7ae4726e
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-06-02 15:55:14 +0200

    add contact fields to volunteer application

--- app/controllers/volunteer_applications_controller.rb
+++ app/controllers/volunteer_applications_controller.rb
@@ -5,6 +5,7 @@ class VolunteerApplicationsController < ApplicationController
   def new
     @volunteer = Volunteer.new
     @volunteer.schedules << Schedule.build
+    @volunteer.build_contact
   end
 
   def create
@@ -25,6 +26,16 @@ class VolunteerApplicationsController < ApplicationController
           volunteer_attributes,
           language_skills_attributes: language_skills_attributes,
           relatives_attributes: relatives_attributes,
-          schedules_attributes: schedules_attributes)
-  end
+          schedules_attributes: schedules_attributes,
+          contact_attributes: [
+            :id, :first_name, :last_name, :_destroy, :contactable_id, :contactable_type, :street,
+            :extended, :city, :postal_code,
+            contact_emails_attributes: contact_point_attrs,
+            contact_phones_attributes: contact_point_attrs]
+          )
+    end
+
+    def contact_point_attrs
+      [:id, :body, :label, :_destroy, :type, :contacts_id]
+    end
 end
