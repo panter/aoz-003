8b7d33da1d70d6e28df77069a72a5d15f6c1903c
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-06-07 11:34:43 +0200

    slim contact_attrs to concern

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,6 +1,7 @@
 class VolunteersController < ApplicationController
   include NestedAttributes
   include VolunteerAttributes
+  include ContactAttributes
   before_action :set_volunteer, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -61,15 +62,7 @@ class VolunteersController < ApplicationController
     params.require(:volunteer).permit(
       volunteer_attributes,
       language_skills_attributes, relatives_attributes, schedules_attributes,
-      contact_attributes: [
-        :id, :first_name, :last_name, :_destroy, :contactable_id, :contactable_type, :street,
-        :extended, :city, :postal_code,
-        contact_emails_attributes: contact_point_attrs,
-        contact_phones_attributes: contact_point_attrs]
+      contact_attributes: contact_attributes
     )
-  end
-
-  def contact_point_attrs
-    [:id, :body, :label, :_destroy, :type, :contacts_id]
   end
 end
