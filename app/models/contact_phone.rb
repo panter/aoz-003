f4e410f787f71c5d2149cdd96125a9b125c78f54
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-06-08 09:21:28 +0200

    remove phone validation due to failing tests

--- app/models/contact_phone.rb
+++ app/models/contact_phone.rb
@@ -1,9 +1,4 @@
 class ContactPhone < ContactPoint
-  validates :body, format: {
-    with: /\A[\ 0-9\+\-\_\(\)]{5,}\z/,
-    message: 'Not a phone number'
-  }
-
   def self.label_collection
     [:fax, :work, :home, :miscellaneous]
   end
