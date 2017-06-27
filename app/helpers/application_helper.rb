ac95dadceb473cadb288de22dd4682a3a332e441
Kaspar Vollenweider <vok@panter.ch>
2017-06-27 11:25:55 +0200

    create helpers for bootstrap row and simple form error notification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,18 @@ module ApplicationHelper
     end
   end
 
+  def single_col_xs(inside, cols = 12)
+    content_tag :div, class: 'row' do
+      content_tag :div, class: "col-xs-#{cols}" do
+        inside
+      end
+    end
+  end
+
+  def simple_error_notice(f)
+    single_col_xs f.error_notification if f.error_notification.present?
+  end
+
   def link_to_add_polymorphic_association(*args)
     name, f, association, html_options = *args
     html_options[:partial] = "#{association}/fields"
