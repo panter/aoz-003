33a25c777df80d8a08c01e986e0e42ccbf8b11cc
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-06-28 11:19:46 +0200

    add new fields to client

---
+++ db/migrate/20170628085858_add_columns_to_clients.rb
@@ -0,0 +1,8 @@
+class AddColumnsToClients < ActiveRecord::Migration[5.1]
+  def change
+    add_column :clients, :entry_year, :date
+    add_column :clients, :gender_request, :string
+    add_column :clients, :age_request, :string
+    add_column :clients, :other_request, :string
+  end
+end
