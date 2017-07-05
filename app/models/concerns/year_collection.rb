a5597a781c3b25c37359fdfc2281689c89eff28a
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-07-05 14:11:12 +0200

    rename year method to rails convention

--- app/models/concerns/year_collection.rb
+++ app/models/concerns/year_collection.rb
@@ -3,7 +3,7 @@ module YearCollection
 
   included do
     def self.year_collection
-      (90.year.ago.year.to_i...Date.today.year.to_i).to_a.map { |date| [date, Date.new(date)] }
+      (90.years.ago.year.to_i...Time.zone.today.year.to_i).to_a.map { |date| [date, Date.new(date)] }
     end
   end
 end
