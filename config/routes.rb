deac0bde1499667660a2f62c560df39d550ce128
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-07-06 11:25:13 +0200

    find volunteer url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/clients/need_accompanying', to: 'clients#without_assignments'
+  get '/clients/find_volunteer', to: 'clients#show'
 
   resources :assignments
   resources :volunteer_applications, only: [:new, :create] do
