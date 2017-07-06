481bbf60e83e6b0dcb5c91f1182725a1419b7969
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-07-06 10:51:33 +0200

    add paper trail gem

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   acts_as_paranoid
+  has_paper_trail
 end
--- db/schema.rb
+++ db/schema.rb
@@ -206,6 +206,16 @@ ActiveRecord::Schema.define(version: 20170710142357) do
     t.index ["user_id"], name: "index_volunteer_emails_on_user_id"
   end
 
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.integer "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.text "object"
+    t.datetime "created_at"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
+  end
+
   create_table "volunteers", force: :cascade do |t|
     t.date "date_of_birth"
     t.string "gender"
