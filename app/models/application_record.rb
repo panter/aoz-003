ff359ab215fd0114184bff266ed413229b14c491
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-07-06 14:04:17 +0200

    implement client versioning index

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   acts_as_paranoid
   has_paper_trail
+
+  def self.find_version_author(version)
+    version.terminator
+  end
 end
