f79c5b237e7e053e4dc61f03a1cf6bb07d1a46fe
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-07-07 13:59:25 +0200

    fix version tests

--- test/system/versions_test.rb
+++ test/system/versions_test.rb
@@ -2,33 +2,44 @@ require 'application_system_test_case'
 
 class VersionsTest < ApplicationSystemTestCase
   setup do
+    with_versioning do
     @user = create :user, email: 'superadmin@example.com'
-    @client = create :user
+    @client = create :user, id: 20
+    @client.reload
     login_as @user
+    end
   end
 
-  test 'client has a history when created' do
-    visit new_client_path
-    fill_in 'First name', with: 'asdf'
-    fill_in 'Last name', with: 'asdf'
-    click_button 'Create Client'
 
-    assert page.has_text? 'Client was successfully created.'
-    assert page.has_text? 'History'
-    assert page.has_text? 'Timepoint'
-    assert page.has_text? 'Changed by'
-    assert page.has_text? 'Event'
-    assert page.has_text? @client.versions.created_at
-    assert_equal @client.versions.author, 'superadmin@example.com'
-    assert_equal @client.versions.event, 'Create'
+  test 'client has a history when created' do
+    with_versioning do
+      visit new_client_path
+      fill_in 'First name', with: 'asdf'
+      fill_in 'Last name', with: 'asdf'
+      click_button 'Create Client'
+
+      assert page.has_text? 'Client was successfully created.'
+      assert page.has_text? 'History'
+      assert page.has_text? 'Timepoint'
+      assert page.has_text? 'Changed by'
+      assert page.has_text? 'Event'
+      assert page.has_text? 'Create'
+    end
   end
 
   test 'client has a update history when updated' do
-    visit client_path(@client)
-    fill_in 'Actual activities', with: 'asdfasdf'
-    click_button 'Update Client'
+    with_versioning do
+      visit new_client_path
+      fill_in 'First name', with: 'asdf'
+      fill_in 'Last name', with: 'asdf'
+      click_button 'Create Client'
 
-    assert page.has_text? 'Client was successfully updated.'
-    assert_equal @client.versions.event, 'Update'
+      click_link 'Edit Client'
+      fill_in 'Actual activities', with: 'asdfasdf'
+      click_button 'Update Client'
+
+      assert page.has_text? 'Client was successfully updated.'
+      assert page.has_text? 'Update'
+    end
   end
 end
