d755724135063e9e5f4cef75117f3433dc5f1a8a
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-07-07 13:56:22 +0200

    views and policies

---
+++ app/policies/assignment_policy.rb
@@ -0,0 +1,40 @@
+class AssignmentPolicy < ApplicationPolicy
+  attr_reader :user, :assignment
+
+  def initialize(user, assignment)
+    @user = user
+    @assignment = assignment
+  end
+
+  def index?
+    user.superadmin?
+  end
+
+  def show?
+    user.superadmin?
+  end
+
+  def new?
+    user.superadmin?
+  end
+
+  def edit?
+    user.superadmin?
+  end
+
+  def create?
+    user.superadmin?
+  end
+
+  def update?
+    user.superadmin?
+  end
+
+  def destroy?
+    user.superadmin?
+  end
+
+  def find_volunteer?
+    user.superadmin?
+  end
+end
