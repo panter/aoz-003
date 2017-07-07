940118080ac6d53b34a4e4de9f4ca35389801ef0
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-07-07 18:31:54 +0200

    basic case basic test

--- test/system/assignments_test.rb
+++ test/system/assignments_test.rb
@@ -1,9 +1,24 @@
 require "application_system_test_case"
 
 class AssignmentsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit assignments_url
-  #
-  #   assert_selector "h1", text: "Assignment"
-  # end
+  setup do
+    @user = create :user, :with_profile, email: 'superadmin@example.com'
+    login_as @user
+    @client = create :client
+    @volunteer = create :volunteer
+    visit clients_path
+  end
+
+  test 'assign unassigned client' do
+    click_link 'Without Assignment'
+    click_link 'Find volunteer'
+    click_link 'Reserve'
+    click_button 'Create Assignment'
+    assert page.has_text? @client.contact.full_name
+    assert page.has_text? @volunteer.contact.full_name
+    visit client_path(@client)
+    assert page.has_text? 'Reserved'
+    visit volunteer_path(@volunteer)
+    assert page.has_text? 'Active'
+  end
 end
