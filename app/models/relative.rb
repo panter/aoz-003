8d74958c705c83b61323ebdc27c1b2368cccd06c
Kaspar Vollenweider <vok@panter.ch>
2017-07-08 10:36:20 +0200

    fix the not working and obsolete full_name concern

--- app/models/relative.rb
+++ app/models/relative.rb
@@ -1,5 +1,4 @@
 class Relative < ApplicationRecord
-  include FullName
   include YearCollection
 
   belongs_to :relativeable, polymorphic: true, optional: true
@@ -7,6 +6,10 @@ class Relative < ApplicationRecord
   def to_s
     relation_human = relation ? I18n.t(relation, scope: [:relation]) : ''
     [full_name, date_of_birth.try(:year), relation_human].reject(&:blank?).join(', ')
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
   end
 
   def self.relation_collection
