fdb7f0164e986b5334e70b9a282ba602420e255a
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-07-10 13:08:28 +0200

    add model test for assignment

--- test/models/assignment_test.rb
+++ test/models/assignment_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class AssignmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @assignment = create :assignment
+  end
+
+  test 'assignment with required attributes is valid' do
+    assert @assignment.valid?
+  end
+
+  test 'assignment with no required attributes is invalid' do
+    assignment = Assignment.new
+    refute assignment.valid?
+  end
 end
