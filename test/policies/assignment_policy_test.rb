c9a73cc8f2fddb853c51b8c3a5b109d226d72e89
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-07-10 11:05:02 +0200

    assignment policy test

---
+++ test/policies/assignment_policy_test.rb
@@ -0,0 +1,84 @@
+require 'test_helper'
+
+class AssignmentPolicyTest < PolicyAssertions::Test
+  def setup
+    @superadmin = create :user, role: 'superadmin'
+    @social_worker = create :user, role: 'social_worker'
+    @department_manager = create :user, role: 'department_manager'
+    @assignment = create :assignment
+  end
+
+  test 'Create: superadmin can create' do
+    assert_permit @superadmin, Assignment, 'new?', 'create?'
+  end
+
+  test 'Create: social worker cannnot create' do
+    refute_permit @social_worker, Assignment, 'new?', 'create?'
+  end
+
+  test 'Create: department manager cannot create' do
+    refute_permit @department_manager, Assignment, 'new?', 'create?'
+  end
+
+  test 'Destroy: superadmin can destroy' do
+    assert_permit @superadmin, Assignment.first, 'destroy?'
+  end
+
+  test 'Destroy: social worker cannot destroy' do
+    refute_permit @social_worker, Assignment.first, 'destroy?'
+  end
+
+  test 'Destroy: department manager cannot destroy' do
+    refute_permit @department_manager, Assignment.first, 'destroy?'
+  end
+
+  test 'Update: superadmin can update assignment' do
+    assert_permit @superadmin, Assignment.first, 'update?', 'edit?'
+  end
+
+  test 'Update: social worker cannot update assignment' do
+    refute_permit @social_worker, Assignment.first, 'update?', 'edit?'
+  end
+
+  test 'Update: department manager cannot update assignment' do
+    refute_permit @department_manager, Assignment.first, 'update?', 'edit?'
+  end
+
+  test 'Show: social worker cannot show assignment' do
+    refute_permit @social_worker, Assignment.first, 'show?'
+  end
+
+  test 'Show: department manager cannot show assignment' do
+    refute_permit @department_manager, Assignment.first, 'show?'
+  end
+
+  test 'Show: superadmin can see all assignments' do
+    Assignment.all.each do |assignment|
+      assert_permit @superadmin, assignment, 'show?'
+    end
+  end
+
+  test 'Index: superadmin can index assignments' do
+    assert_permit @superadmin, Assignment.first, 'index?'
+  end
+
+  test 'Index: social worker cannot index assignments' do
+    refute_permit @social_worker, Assignment.first, 'index?'
+  end
+
+  test 'Index: department manager cannot index assignments' do
+    refute_permit @department_manager, Assignment.first, 'index?'
+  end
+
+  test 'Find volunteer: superadmin can find a volunteer for client' do
+    assert_permit @superadmin, Assignment.first, 'find_volunteer?'
+  end
+
+  test 'Find volunteer: social worker cannot find a volunteer for client' do
+    refute_permit @social_worker, Assignment.first, 'find_volunteer?'
+  end
+
+  test 'Find volunteer: department_manager cannot find a volunteer for client' do
+    refute_permit @department_manager, Assignment.first, 'find_volunteer?'
+  end
+end
