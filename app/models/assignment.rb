8eee08595affd4be6d8de326560e0b9dcf85cb28
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-07-12 17:04:34 +0200

    add responsible user to assignments

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,7 @@
 class Assignment < ApplicationRecord
   belongs_to :client
   belongs_to :volunteer
+  belongs_to :user
 
   def self.state_collection
     [:suggested, :active]
--- test/factories/assignments.rb
+++ test/factories/assignments.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :assignment do
     client
     volunteer
+    user
   end
 end
