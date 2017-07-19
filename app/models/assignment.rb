6e2199ef0f91cdbcd4648478fc1f0934e7ca4112
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-07-19 14:54:15 +0200

    no duplicate assignments

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,6 +3,8 @@ class Assignment < ApplicationRecord
   belongs_to :volunteer
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
 
+  validates :client_id, uniqueness: { scope: :volunteer_id, message: I18n.t('assignment_exists') }
+
   def self.state_collection
     [:suggested, :active]
   end
