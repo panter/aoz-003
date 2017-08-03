5752b764c82a13e8e70c47579544dfdcadfc3535
Kaspar Vollenweider <vok@panter.ch>
2017-08-03 09:27:44 +0200

    change utils to make use of transform_values

--- lib/access_import/plz.rb
+++ lib/access_import/plz.rb
@@ -13,13 +13,8 @@ class Plz
 
   def sanitize_record(rec)
     rec[:fk_Kanton] = rec[:"fk_Kanton Bundesland"]
-    rec = parse_int_fields(
-      rec,
-      [:pk_PlzOrt, :z_PLZ, :fk_Land, :fk_Kanton]
-    )
-    rec.except(
-      :"fk_Kanton Bundesland", :t_Mutation, :d_MutDatum
-    )
+    rec = parse_int_fields(rec, :pk_PlzOrt, :z_PLZ, :fk_Land, :fk_Kanton)
+    rec.except(:"fk_Kanton Bundesland", :t_Mutation, :d_MutDatum)
   end
 
   def all
--- lib/access_import/sprache_pro_hauptperson.rb
+++ lib/access_import/sprache_pro_hauptperson.rb
@@ -13,12 +13,11 @@ class SpracheProHauptperson
     make_mappable(:tbl_SpracheProHauptperson, :pk_SpracheProPerson, true)
   end
 
+  LANGUAGE_LEVELS = [nil, 'basic', 'good', 'fluent', 'native_speaker', nil].freeze
+
   def sanitize_record(rec)
-    level_keys = ['Le', 'Sc', 'Sp', 'Ve'].map { |k| "fk_Kenntnisstufe#{k}".to_sym }
-    rec = parse_int_fields(rec, level_keys.push(:fk_Hauptperson, :fk_Sprache, :pk_SpracheProPerson))
-    level_keys.each do |level|
-      rec[level.to_s[3..-1].to_sym] = map_kentniss_level(rec[level])
-    end
+    rec = parse_int_fields(rec, *level_keys, :fk_Hauptperson, :fk_Sprache, :pk_SpracheProPerson)
+    rec = add_kentniss_levels(rec)
     rec[:language] = @sprachen.find(rec[:fk_Sprache])
     rec.except(
       :d_MutDatum, :t_Mutation, :fk_KenntnisstufeLe, :fk_KenntnisstufeSc, :fk_KenntnisstufeSp,
@@ -26,8 +25,15 @@ class SpracheProHauptperson
     )
   end
 
-  def map_kentniss_level(kentniss_id)
-    [nil, 'basic', 'good', 'fluent', 'native_speaker', nil][kentniss_id]
+  def add_kentniss_levels(record)
+    kentnisse = record.slice(*level_keys).compact.map do |k, v|
+      [k.to_s[3..-1].to_sym, LANGUAGE_LEVELS[v]]
+    end.to_h
+    record.merge(kentnisse).except(*level_keys)
+  end
+
+  def level_keys
+    ['Le', 'Sc', 'Sp', 'Ve'].map { |k| "fk_Kenntnisstufe#{k}".to_sym }
   end
 
   def all
