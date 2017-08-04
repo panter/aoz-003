18cddd5e03b458f511a54189ebff7e7db2c9760a
Kaspar Vollenweider <vok@panter.ch>
2017-08-04 15:16:11 +0200

    entity's built in while trying to understand the rest
    some of them will be used for future import stories

---
+++ lib/access_import/accessors/fallstellen.rb
@@ -0,0 +1,11 @@
+class Fallstellen
+  def hash_all
+    make_mappable(:tbl_FallStellen, :pk_FallStelle, true)
+  end
+
+  def sanitize_record(rec)
+    rec = parse_int_fields(rec, :pk_FallStelle, :fk_PLZ)
+    rec[:email] = email(rec[:h_Email])
+    parse_datetime_fields(rec, :d_MutDatum)
+  end
+end
---
+++ lib/access_import/accessors/kontoangaben.rb
@@ -0,0 +1,17 @@
+class Kontoangaben < Accessor
+  def hash_all
+    make_mappable(:tbl_Kontoangaben, :pk_Kontoangabe, true)
+  end
+
+  def sanitize_record(rec)
+    rec = parse_int_fields(rec, :pk_Kontoangabe, :fk_Hauptperson, :z_KontoArt, :fk_PlzOrt,
+      :z_ClearingNummer)
+    parse_datetime_fields(rec, :d_MutDatum)
+  end
+
+  def where_haupt_person(hp_id)
+    @records.select do |_k, kontoangabe|
+      kontoangabe[:fk_Hauptperson] == hp_id.to_i
+    end
+  end
+end
