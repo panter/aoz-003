25e050f8b8c0008ed86f5262b5d428d51c1e73be
Kaspar Vollenweider <vok@panter.ch>
2017-08-04 15:13:44 +0200

    prequisite tables, for personen_rolle, begleitete and haupt_person

---
+++ lib/access_import/accessors/sprachen.rb
@@ -0,0 +1,18 @@
+class Sprachen < Accessor
+  def hash_all
+    make_mappable(:tbl_Sprachen, :pk_Sprache, true)
+  end
+
+  def sanitize_record(rec)
+    rec = parse_int_fields(rec, :pk_Sprache)
+    {
+      lang: language(rec[:t_Sprache]),
+      sprache: rec[:t_Sprache]
+    }
+  end
+
+  def language(sprache)
+    sprache = 'Tigrinja' if sprache == 'Tigrinia'
+    I18nData.languages(:de).select { |_k, v| v == sprache }.keys.first
+  end
+end
