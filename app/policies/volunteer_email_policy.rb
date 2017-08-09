3a2ed0385c843e6c436170317933ba284921db83
Kaspar Vollenweider <vok@panter.ch>
2017-08-09 15:59:58 +0200

    add missing policy for volunteer new?

--- app/policies/volunteer_email_policy.rb
+++ app/policies/volunteer_email_policy.rb
@@ -1,6 +1,7 @@
 class VolunteerEmailPolicy < ApplicationPolicy
   alias_method :index?,                 :superadmin?
   alias_method :show?,                  :superadmin?
+  alias_method :new?,                   :superadmin?
   alias_method :edit?,                  :superadmin?
   alias_method :create?,                :superadmin?
   alias_method :update?,                :superadmin?
