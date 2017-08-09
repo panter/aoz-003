81a9d636ebfe6c4af91acbe95a5971f3907a32a4
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-08-09 14:09:10 +0200

    add categories to journals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170807101812) do
+ActiveRecord::Schema.define(version: 20170809112804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20170807101812) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["deleted_at"], name: "index_journals_on_deleted_at"
     t.index ["journalable_type", "journalable_id"], name: "index_journals_on_journalable_type_and_journalable_id"
     t.index ["user_id"], name: "index_journals_on_user_id"
