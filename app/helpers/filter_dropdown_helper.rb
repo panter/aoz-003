e2f9e611bf3e4dacbe60c20ae5ae3d5b59979687
Kaspar Vollenweider <vok@panter.ch>
2017-08-16 09:58:52 +0200

    class variable to parameter, renameing

--- app/helpers/filter_dropdown_helper.rb
+++ app/helpers/filter_dropdown_helper.rb
@@ -2,33 +2,36 @@ module FilterDropdownHelper
   def list_eq_filter_dropdown(attribute, collection, t_scope: nil)
     set_local_q
     t_scope ||= [:simple_form, :options, controller_name.singularize.to_sym]
-    @t_scope = { scope: t_scope.push(attribute) }
-    filter_links = collection.map { |item| list_filter_link("#{attribute}_eq".to_sym, item) }
-    container_li attribute, filter_links, "#{attribute}_eq".to_sym
+    t_scope = { scope: t_scope.push(attribute) }
+    filter_links = collection.map do |item|
+      list_filter_link "#{attribute}_eq".to_sym, item, t_scope
+    end
+    dropdown_list_element attribute, filter_links, t_scope, "#{attribute}_eq".to_sym
   end
 
   def list_boolean_filter_dropdown(attribute_group, collection, t_scope: nil)
     set_local_q
-    @t_scope = {
+    t_scope = {
       scope: t_scope || [:activerecord, :attributes, controller_name.singularize.to_sym]
     }
     q_filters = collection.map { |attribute| "#{attribute}_eq".to_sym }
     filter_links = q_filters.each_with_index.map do |q_filter, index|
-      list_filter_link q_filter, collection[index].to_sym, boolean_filter_active(@local_q[q_filter])
+      list_filter_link(q_filter, collection[index].to_sym, t_scope,
+        boolean_filter_active(@local_q[q_filter]))
     end
-    container_li attribute_group, filter_links, *q_filters
+    dropdown_list_element attribute_group, filter_links, t_scope, *q_filters
   end
 
-  def container_li(attribute, filter_links, *q_filters)
+  def dropdown_list_element(attribute, filter_links, t_scope, *q_filters)
     content_tag :li, class: 'dropdown' do
-      concat dropdown_link(attribute, q_filters)
+      concat dropdown_link(attribute, t_scope, q_filters)
       concat dropdown_menu(filter_links, q_filters)
     end
   end
 
-  def list_filter_link(q_filter, filter_value, boolean_value = nil)
+  def list_filter_link(q_filter, filter_value, t_scope, boolean_value = nil)
     link_class = 'bg-success' if [filter_value.to_s, 'true'].include? @local_q[q_filter]
-    link_text = translate_value(filter_value)
+    link_text = translate_value(filter_value, t_scope)
     filter_value = true if !boolean_value.nil? && boolean_value
     filter_value = '' if !boolean_value.nil? && !boolean_value
     content_tag :li do
@@ -44,14 +47,14 @@ module FilterDropdownHelper
     end
   end
 
-  def dropdown_link(attribute, q_filter = nil)
+  def dropdown_link(attribute, t_scope, q_filter = nil)
     options = { class: 'dropdown-toggle btn btn-default btn-sm',
                 aria: { expanded: 'false', haspopup: 'true', toggle: 'dropdown' },
                 role: 'button', href: '#', data: { toggle: 'dropdown' } }
     content_tag :a, options do
       concat t_attr(attribute)
       concat ': ' if q_filter.size == 1
-      concat translate_value(@local_q[q_filter[0]]) if q_filter.size == 1
+      concat translate_value(@local_q[q_filter[0]], t_scope) if q_filter.size == 1
       concat ' '
       concat content_tag(:span, '', class: 'caret')
     end
@@ -71,9 +74,9 @@ module FilterDropdownHelper
     true if actual_filter == 'true'
   end
 
-  def translate_value(filter_value)
+  def translate_value(filter_value, t_scope)
     return t('all') if ['all', '', nil].include? filter_value
-    t(filter_value, @t_scope)
+    t(filter_value, t_scope)
   end
 
   def filter_link(q_filter, filter_value)
