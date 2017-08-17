4faa614ef088a2981cf2bd7c0ce3f04ad3b4dc60
Kaspar Vollenweider <vok@panter.ch>
2017-08-17 18:12:36 +0200

    strange mess with volunteers permitted parameters

--- app/controllers/concerns/volunteer_attributes.rb
+++ app/controllers/concerns/volunteer_attributes.rb
@@ -10,7 +10,10 @@ module VolunteerAttributes
         :music, :culture, :training, :german_course, :teenagers, :children, :state,
         :working_percent, :volunteer_experience_desc, :dancing, :health, :cooking,
         :excursions, :women, :unaccompanied, :zurich, :other_offer, :other_offer_desc,
-        :own_kids, contact_attributes, language_skills_attributes, availability_attributes
+        :trial_period, :doc_sent, :bank_account, :evaluation, :own_kids, :rejection_type,
+        :rejection_text, :intro_course, :flexible, :morning, :afternoon, :evening,
+        :workday, :weekend, :detailed_description, contact_attributes,
+        language_skills_attributes, availability_attributes
       ]
     end
   end
