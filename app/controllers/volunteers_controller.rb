4c0d4b34a0f7342e4b1e54916c9fd057b2cb3f7f
Kaspar Vollenweider <vok@panter.ch>
2017-08-17 18:12:36 +0200

    strange mess with volunteers permitted parameters

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -78,8 +78,6 @@ class VolunteersController < ApplicationController
   end
 
   def volunteer_params
-    params.require(:volunteer).permit(volunteer_attributes, :rejection_type, :rejection_text,
-      :trial_period, :intro_course, :doc_sent, :bank_account, :evaluation, :flexible,
-      :morning, :afternoon, :evening, :workday, :weekend, :detailed_description)
+    params.require(:volunteer).permit(volunteer_attributes)
   end
 end
