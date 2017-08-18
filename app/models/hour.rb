81de07c7c711774ecb72618f61d72afb79496582
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-08-18 13:23:31 +0200

    duration validation

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -2,5 +2,5 @@ class Hour < ApplicationRecord
   belongs_to :volunteer
   belongs_to :assignment
 
-  validates :duration, presence: true
+  validates :duration, presence: true, numericality: { greater_than: 0 }
 end
