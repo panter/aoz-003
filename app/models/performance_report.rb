febdca80716df3a8ba1c675bee24ca89f7eed5a8
Kaspar Vollenweider <vok@panter.ch>
2017-08-22 17:55:22 +0200

    period to start end datetime values and such

--- app/models/performance_report.rb
+++ app/models/performance_report.rb
@@ -1,3 +1,13 @@
 class PerformanceReport < ApplicationRecord
+  before_validation :make_report
   belongs_to :user
+
+  def make_report
+    binding.pry
+    {
+      volunteer: {
+        total: Volunteer.count
+      }
+    }
+  end
 end
