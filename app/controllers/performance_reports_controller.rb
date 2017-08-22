ae1e7adaa2f58554f8a6f72fd31f030d4c5c5912
Kaspar Vollenweider <vok@panter.ch>
2017-08-22 17:55:22 +0200

    period to start end datetime values and such

--- app/controllers/performance_reports_controller.rb
+++ app/controllers/performance_reports_controller.rb
@@ -18,7 +18,7 @@ class PerformanceReportsController < ApplicationController
   def edit; end
 
   def create
-    @performance_report = PerformanceReport.new(performance_report_params)
+    @performance_report = PerformanceReport.new(report_params.merge(convert_start_end))
     @performance_report.user = current_user
     authorize @performance_report
     if @performance_report.save
@@ -29,7 +29,7 @@ class PerformanceReportsController < ApplicationController
   end
 
   def update
-    if @performance_report.update(performance_report_params)
+    if @performance_report.update(report_params)
       redirect_to @performance_report, make_notice
     else
       render :edit
@@ -43,12 +43,20 @@ class PerformanceReportsController < ApplicationController
 
   private
 
+  def convert_start_end
+    dates = report_params.slice(:period_start, :period_end)
+                         .transform_values(&:to_datetime)
+    dates[:period_start] = dates[:period_start].at_beginning_of_day
+    dates[:period_end] = dates[:period_end].at_end_of_day
+    dates
+  end
+
   def set_performance_report
     @performance_report = PerformanceReport.find(params[:id])
     authorize @performance_report
   end
 
-  def performance_report_params
+  def report_params
     params.require(:performance_report).permit(:period_start, :period_end, :users_id,
       :report_content, :comment, :title, :scope, :extern)
   end
