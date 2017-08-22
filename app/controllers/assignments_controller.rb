0135fafa3b495fb5242ef9d2e7fea92dbfd8b208
Kaspar Vollenweider <vok@panter.ch>
2017-08-22 12:50:59 +0200

    temporary assignment import, for feasable performance report fiddling
    I need some feasable reporting data, to create reports on. I figured it
    to be easyer to make this import halfway, instead of mangling a seed for this

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -82,6 +82,8 @@ class AssignmentsController < ApplicationController
   end
 
   def assignment_params
-    params.require(:assignment).permit(:client_id, :volunteer_id, :state)
+    params.require(:assignment).permit(:client_id, :volunteer_id, :state, :assignment_start,
+      :assignment_end, :performance_appraisal_review, :probation_period, :home_visit,
+      :first_instruction_lesson)
   end
 end
