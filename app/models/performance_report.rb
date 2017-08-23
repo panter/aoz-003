98963759e3ec9a038731be1cdcc2d00f948006b2
Kaspar Vollenweider <vok@panter.ch>
2017-08-23 11:09:43 +0200

    performance report saves itself with before_validation callback trigger

--- app/models/performance_report.rb
+++ app/models/performance_report.rb
@@ -3,22 +3,31 @@ class PerformanceReport < ApplicationRecord
   belongs_to :user
 
   def make_report
-    binding.pry
-    {
-      volunteers: {
-        total: Volunteer.count,
-        active: Volunteer.with_active_assignments_between(period_start, period_end).count,
-        new: Volunteer.where(created_at: period_start..period_end).count
+    report_content = {
+      global: {
+        volunteers: {
+          total: Volunteer.count,
+          active: Volunteer.with_active_assignments_between(period_start, period_end).count,
+          new: Volunteer.where(created_at: period_start..period_end).count
+        },
+        assignments: {
+          ended: Assignment.end_within(period_start..period_end).count,
+          started: Assignment.start_within(period_start..period_end).count,
+          total: Assignment.count,
+          active: Assignment.active_between(period_start, period_end).count
+        }
       },
-      assignments: {
-        ended: Assignment.end_within(period_start..period_end).count,
-        started: Assignment.start_within(period_start..period_end).count,
-        total: Assignment.count,
-        active: Assignment.active_between(period_start, period_end).count
-      },
-      clients_zuerich: {
-        total: Client.zurich.count,
-        active: Assignment.active_between(period_start, period_end).zurich.count
+      zuerich: {
+        clients: {
+          total: Client.zurich.count,
+          active: Assignment.active_between(period_start, period_end).zurich.count
+        },
+        assignments: {
+          ended: Assignment.end_within(period_start..period_end).zurich.count,
+          started: Assignment.start_within(period_start..period_end).zurich.count,
+          total: Assignment.zurich.count,
+          active: Assignment.active_between(period_start, period_end).zurich.count
+        }
       }
     }
   end
