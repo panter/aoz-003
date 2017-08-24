9d9164afcfb4c265d38551dd71c7cea697729596
Kaspar Vollenweider <vok@panter.ch>
2017-08-24 11:10:24 +0200

    split the report generation up in methods

--- app/models/performance_report.rb
+++ app/models/performance_report.rb
@@ -1,44 +1,57 @@
 class PerformanceReport < ApplicationRecord
-  before_validation :make_report
+  before_validation :generate_report
   belongs_to :user
 
-  def make_report
+  def generate_report
     self.report_content = {
-      global: {
-        volunteers: {
-          total: Volunteer.count,
-          active: Volunteer.with_active_assignments_between(period_start, period_end).count,
-          new: Volunteer.created_between(period_start, period_end).count
-        },
-        clients: {
-          new: Client.created_between(period_start, period_end).count,
-          active: Client.with_active_assignment_between(period_start, period_end).count
-        },
-        assignments: {
-          ended: Assignment.end_within(period_start..period_end).count,
-          started: Assignment.start_within(period_start..period_end).count,
-          total: Assignment.count,
-          active: Assignment.active_between(period_start, period_end).count
-        }
-      },
-      zuerich: {
-        volunteers: {
-          total: Volunteer.zurich.count,
-          active: Volunteer.with_active_assignments_between(period_start, period_end).zurich.count,
-          new: Volunteer.created_between(period_start, period_end).zurich.count
-        },
-        clients: {
-          total: Client.zurich.count,
-          active: Client.with_active_assignment_between(period_start, period_end).zurich.count,
-          new: Client.created_between(period_start, period_end).zurich.count
-        },
-        assignments: {
-          ended: Assignment.end_within(period_start..period_end).zurich.count,
-          started: Assignment.start_within(period_start..period_end).zurich.count,
-          total: Assignment.zurich.count,
-          active: Assignment.active_between(period_start, period_end).zurich.count
-        }
-      }
+      global: global,
+      zuerich: zuerich
+    }
+  end
+
+  private
+
+  def global
+    {
+      volunteers: volunteers,
+      clients: clients,
+      assignments: assignments
+    }
+  end
+
+  def zuerich
+    {
+      volunteers: volunteers(true),
+      clients: clients(true),
+      assignments: assignments(true)
+    }
+  end
+
+  def volunteers(zurich = false)
+    volunteers = zurich ? Volunteer.zurich : Volunteer.all
+    {
+      total: volunteers.count,
+      active: volunteers.with_active_assignments_between(period_start, period_end).count,
+      new: volunteers.created_between(period_start, period_end).count
+    }
+  end
+
+  def clients(zurich = false)
+    clients = zurich ? Client.zurich : Client.all
+    {
+      new: clients.created_between(period_start, period_end).count,
+      active: clients.with_active_assignment_between(period_start, period_end).count,
+      total: clients.count
+    }
+  end
+
+  def assignments(zurich = false)
+    assignments = zurich ? Assignment.zurich : Assignment.all
+    {
+      ended: assignments.end_within(period_start..period_end).count,
+      started: assignments.start_within(period_start..period_end).count,
+      active: assignments.active_between(period_start, period_end).count,
+      total: assignments.count
     }
   end
 end
