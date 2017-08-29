3c0c3ad807d88d1f6d83a1eedfaa68d34cdcadd8
Kaspar Vollenweider <vok@panter.ch>
2017-08-29 16:56:07 +0200

    update add missing fields and implement into assignment import

--- db/migrate/20170825144136_add_missing_fields_for_assignments.rb
+++ db/migrate/20170825144136_add_missing_fields_for_assignments.rb
@@ -1,15 +1,17 @@
 class AddMissingFieldsForAssignments < ActiveRecord::Migration[5.1]
   def change
     change_table :assignments do |t|
-      t.datetime :progress_meeting        # Standortgespräch
-      t.datetime :first_lesson            # ErstUnterricht
-      t.datetime :home_visit              # Hausbesuch
-      t.datetime :probation_period        # Probezeit
-      t.boolean  :probation_period_report # Probezeitbericht
-      t.string   :short_description       # Kurzbezeichnung
-      t.text     :goals                   # Zielsetzung
-      t.text     :starting_topic          # Einstiegsthematik
-      t.text     :description             # Beschreibung
+      t.datetime :assignment_start
+      t.datetime :assignment_end
+      t.datetime :first_instruction_lesson     # ErstUnterricht
+      t.datetime :progress_meeting             # Standortgespräch
+      t.datetime :home_visit                   # Hausbesuch
+      t.datetime :probation_period             # Probezeit
+      t.datetime :performance_appraisal_review # Probezeitbericht
+      t.string   :short_description            # Kurzbezeichnung
+      t.text     :goals                        # Zielsetzung
+      t.text     :starting_topic               # Einstiegsthematik
+      t.text     :description                  # Beschreibung
     end
   end
 end
