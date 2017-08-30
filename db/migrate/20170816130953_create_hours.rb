2f554d3f61a91121747ff41230df4c039333ebb5
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-08-30 10:40:28 +0200

    merge migration files

--- db/migrate/20170816130953_create_hours.rb
+++ db/migrate/20170816130953_create_hours.rb
@@ -2,11 +2,13 @@ class CreateHours < ActiveRecord::Migration[5.1]
   def change
     create_table :hours do |t|
       t.date :meeting_date
-      t.integer :duration
+      t.integer :hours
+      t.integer :minutes
       t.string :activity
       t.string :comments
       t.belongs_to :volunteer
       t.belongs_to :assignment
+      t.datetime :deleted_at, index: true
 
       t.timestamps
     end
--- db/migrate/20170825090802_add_minutes_to_hours.rb
+++
@@ -1,6 +0,0 @@
-class AddMinutesToHours < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :hours, :duration, :hours
-    add_column :hours, :minutes, :integer
-  end
-end
