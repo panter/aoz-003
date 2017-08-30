ac3c0d3e6b2ac55bcbbbca96d735447c8a76e0c0
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-08-30 10:40:28 +0200

    merge migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825090802) do
+ActiveRecord::Schema.define(version: 20170824123715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,14 +96,14 @@ ActiveRecord::Schema.define(version: 20170825090802) do
   create_table "hours", force: :cascade do |t|
     t.date "meeting_date"
     t.integer "hours"
+    t.integer "minutes"
     t.string "activity"
     t.string "comments"
     t.bigint "volunteer_id"
     t.bigint "assignment_id"
+    t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
-    t.integer "minutes"
     t.index ["assignment_id"], name: "index_hours_on_assignment_id"
     t.index ["deleted_at"], name: "index_hours_on_deleted_at"
     t.index ["volunteer_id"], name: "index_hours_on_volunteer_id"
