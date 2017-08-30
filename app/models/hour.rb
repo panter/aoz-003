f100fda1278a0f7b9049d1a096211a9006b432f6
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-08-30 10:20:54 +0200

    hour minute collections

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -5,4 +5,7 @@ class Hour < ApplicationRecord
   validates :hours, presence: true, numericality: { greater_than: 0 }
   validates :minutes, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :meeting_date, presence: true
+
+  HOUR_RANGE = (1..8).to_a
+  MINUTE_RANGE = (0..59).to_a
 end
