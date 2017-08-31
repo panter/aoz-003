3598597e9b110016177a02bb6b2abeefd5342688
Kaspar Vollenweider <vok@panter.ch>
2017-08-31 18:37:59 +0200

    sorry for bypassing

--- db/migrate/20170825144136_add_missing_fields_for_assignments.rb
+++ db/migrate/20170825144136_add_missing_fields_for_assignments.rb
@@ -1,8 +1,6 @@
 class AddMissingFieldsForAssignments < ActiveRecord::Migration[5.1]
   def change
     change_table :assignments do |t|
-      t.datetime :assignment_start
-      t.datetime :assignment_end
       t.datetime :performance_appraisal_review
       t.datetime :probation_period
       t.datetime :home_visit
