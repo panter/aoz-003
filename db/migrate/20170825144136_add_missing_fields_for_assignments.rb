ff98f571ff9a4bedce0938ba307f02e34aa2f4a9
Kaspar Vollenweider <vok@panter.ch>
2017-08-31 18:11:31 +0200

    hopefully this will really do the fix now....

--- db/migrate/20170825144136_add_missing_fields_for_assignments.rb
+++ db/migrate/20170825144136_add_missing_fields_for_assignments.rb
@@ -1,13 +1,7 @@
 class AddMissingFieldsForAssignments < ActiveRecord::Migration[5.1]
   def change
     change_table :assignments do |t|
-      t.datetime :assignment_start
-      t.datetime :assignment_end
-      t.datetime :first_instruction_lesson     # ErstUnterricht
       t.datetime :progress_meeting             # Standortgespräch
-      t.datetime :home_visit                   # Hausbesuch
-      t.datetime :probation_period             # Probezeit
-      t.datetime :performance_appraisal_review # Probezeitbericht
       t.string   :short_description            # Kurzbezeichnung
       t.text     :goals                        # Zielsetzung
       t.text     :starting_topic               # Einstiegsthematik
