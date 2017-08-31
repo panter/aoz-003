df0fc9ac8970d7e198228fb19bdde8c9574104c8
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-08-31 09:49:06 +0200

    test modifications

--- test/system/assignment_journals_test.rb
+++ test/system/assignment_journals_test.rb
@@ -2,7 +2,7 @@ require 'application_system_test_case'
 
 class HoursTest < ApplicationSystemTestCase
   def setup
-    user_volunteer = create :user, role: 'volunteer', email: 'volunteer1@example.com'
+    user_volunteer = create :user, role: 'volunteer', email: 'volunteer@example.com'
     volunteer = user_volunteer.volunteer = create :volunteer
     client = create :client
     assignment = create :assignment, volunteer: volunteer, client: client
@@ -11,15 +11,15 @@ class HoursTest < ApplicationSystemTestCase
       author: superadmin, comments: 'author superadmin'
     login_as user_volunteer
     visit root_url
-    click_link 'volunteer1@example.com'
+    click_link 'volunteer@example.com'
     click_link 'Show profile'
   end
 
   test 'volunteer can create an assignment journal' do
     click_link 'New Assignment journal'
-    fill_in 'Goals', with: 'asdf'
-    fill_in 'Achievements', with: 'asdf'
-    fill_in 'Future', with: 'asdf'
+    fill_in 'Which were the most important goals during the last months?', with: 'asdf'
+    fill_in 'What could have been achieved during the last months?', with: 'asdf'
+    fill_in 'Should the assignment continue? If yes, with which goals?', with: 'asdf'
     fill_in 'Comments', with: 'asdf'
     page.check('assignment_journal_conversation')
     click_button 'Create Assignment journal'
