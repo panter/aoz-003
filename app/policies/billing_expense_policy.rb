fdad40b975825a906d3ac547514df5a3f91e88df
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-09-01 10:51:21 +0200

    change policy and tests

--- app/policies/billing_expense_policy.rb
+++ app/policies/billing_expense_policy.rb
@@ -1,9 +1,9 @@
 class BillingExpensePolicy < ApplicationPolicy
-  alias_method :index?,          :superadmin_or_volunteer_related?
-  alias_method :show?,           :superadmin_or_volunteer_related?
-  alias_method :new?,            :superadmin_or_volunteer_related?
-  alias_method :edit?,           :superadmin_or_volunteer_related?
-  alias_method :create?,         :superadmin_or_volunteer_related?
-  alias_method :update?,         :superadmin_or_volunteer_related?
-  alias_method :destroy?,        :superadmin_or_volunteer_related?
+  alias_method :index?,          :superadmin?
+  alias_method :show?,           :superadmin?
+  alias_method :new?,            :superadmin?
+  alias_method :edit?,           :superadmin?
+  alias_method :create?,         :superadmin?
+  alias_method :update?,         :superadmin?
+  alias_method :destroy?,        :superadmin?
 end
--- test/system/billing_expenses_test.rb
+++ test/system/billing_expenses_test.rb
@@ -2,34 +2,19 @@ require 'application_system_test_case'
 
 class BillingExpensesTest < ApplicationSystemTestCase
   def setup
-    @user_volunteer1 = create :user, role: 'volunteer', email: 'volunteer1@example.com'
-    @volunteer1 = @user_volunteer1.volunteer = create :volunteer
-    @client1 = create :client
-    @assignment1 = create :assignment, volunteer: @volunteer1, client: @client1
-    login_as @user_volunteer1
-    visit root_url
-    click_link 'volunteer1@example.com'
-    click_link 'Show profile'
+    @superadmin = create :user
+    @volunteer = create :volunteer
+    @client = create :client
+    assignment = create :assignment, volunteer: @volunteer, client: @client
+    login_as @superadmin
   end
 
-  test 'volunteer can create a billing expense' do
+  test 'superadmin can create a billing expense' do
+    visit volunteer_path(@volunteer)
     click_link 'New Billing expense'
-    select @client1.contact.full_name, from: 'Assignment'
+    select @client.contact.full_name, from: 'Assignment'
     select '50', from: 'Amount'
     click_button 'Create Billing expense'
     assert page.has_text? 'Billing expense was successfully created.'
-  end
-
-  test 'volunteer can see only her billing expenses' do
-    @volunteer2 = create :volunteer
-    @client2 = create :client
-    @assignment2 = create :assignment, volunteer: @volunteer2, client: @client2
-    create :billing_expense, assignment: @assignment2, volunteer: @volunteer2
-    click_link 'Billing expense index'
-    assert page.has_text? @client1.contact.full_name
-    refute page.has_text? @client2.contact.full_name
-    visit volunteer_billing_expenses_path(@volunteer2)
-    assert page.has_text? 'You are not authorized to perform this action.'
-    refute page.has_text? @client2.contact.full_name
   end
 end
