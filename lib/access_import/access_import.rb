d53d99362fd17b7ebfca134890d57e52ed01051a
Kaspar Vollenweider <vok@panter.ch>
2017-09-01 14:23:37 +0200

    softdelete import user after import and restore if softdeleted exists

--- lib/access_import/access_import.rb
+++ lib/access_import/access_import.rb
@@ -3,13 +3,16 @@ require 'securerandom'
 class AccessImport
   attr_reader :acdb
 
+  EMAIL = 'aoz_access_importer@example.com'.freeze
+
   def initialize(path)
+    ObjectSpace.define_finalizer(self, self.class.finalize)
+    @import_user = create_or_fetch_import_user
     @acdb = Mdb.open(path)
     make_class_variables(*instantiate_all_accessors)
     @sprache_pro_hauptperson.add_other_accessors(@sprachen, @sprach_kenntnisse)
     @einsatz_orte.add_other_accessors(@plz)
     @haupt_person.add_other_accessors(@plz, @laender, @sprache_pro_hauptperson)
-    @import_user = User.find_by(email: 'aoz_access_importer@example.com') || make_import_superadmin
   end
 
   def make_departments
@@ -93,18 +96,6 @@ class AccessImport
     puts "#{message}#{destination_model.class.name} from MS Access Database."
   end
 
-  def make_import_superadmin
-    import_user_email = 'aoz_access_importer@example.com'
-    password = SecureRandom.hex(60)
-    import_user = User.new(role: 'superadmin', password: password, email: import_user_email)
-    import_user.build_profile
-    import_user.profile.contact.first_name = 'AOZ Import'
-    import_user.profile.contact.last_name = 'AOZ Import'
-    import_user.profile.contact.primary_email = import_user_email
-    import_user.save
-    import_user
-  end
-
   def instantiate_all_accessors
     Dir['lib/access_import/accessors/*.rb']
       .map { |file| File.basename(file, '.*') }
@@ -119,5 +110,22 @@ class AccessImport
       class_eval { attr_reader accessor.class.name.underscore.to_sym }
       instance_variable_set("@#{accessor.class.name.underscore}", accessor)
     end
+  end
+
+  def create_or_fetch_import_user
+    return User.find_by(email: EMAIL) if User.exists?(email: EMAIL)
+    return User.deleted.find_by(email: EMAIL).restore if User.deleted.exists?(email: EMAIL)
+    password = SecureRandom.hex(60)
+    import_user = User.new(role: 'superadmin', password: password, email: EMAIL)
+    import_user.build_profile
+    import_user.profile.contact.first_name = 'AOZ Import'
+    import_user.profile.contact.last_name = 'AOZ Import'
+    import_user.profile.contact.primary_email = EMAIL
+    import_user.save
+    import_user
+  end
+
+  def self.finalize
+    proc { User.find_by(email: EMAIL).delete }
   end
 end
