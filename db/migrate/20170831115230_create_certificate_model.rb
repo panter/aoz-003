266bdd4b71514fad42835d264b1a2b73dcf2bff4
Kaspar Vollenweider <vok@panter.ch>
2017-09-06 14:55:23 +0200

    creating views for pdf generation

--- db/migrate/20170831115230_create_certificate_model.rb
+++ db/migrate/20170831115230_create_certificate_model.rb
@@ -1,11 +1,15 @@
 class CreateCertificateModel < ActiveRecord::Migration[5.1]
   def change
     create_table :certificates do |t|
-      t.jsonb :values
-
+      t.integer :hours
+      t.integer :minutes
+      t.date :duration_start
+      t.date :duration_end
+      t.text :institution
+      t.jsonb :paragraphs
+      t.jsonb :assignment_kinds
       t.references :volunteer, foreign_key: true
       t.references :user, foreign_key: true
-
       t.datetime :deleted_at, index: true
       t.timestamps
     end
