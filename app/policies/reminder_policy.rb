245e98f5cf3ec07b85d4c21088e1f84e9067ae06
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-09-07 14:40:38 +0200

    policies

---
+++ app/policies/reminder_policy.rb
@@ -0,0 +1,5 @@
+class ReminderPolicy < ApplicationPolicy
+  alias_method :index?,          :superadmin?
+  alias_method :update?,         :superadmin?
+  alias_method :destroy?,        :superadmin?
+end
