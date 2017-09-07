2946a03602acb2bbccb64682d6b0b2605a907955
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-09-07 12:32:40 +0200

    add system test for billing expense view

--- test/system/billing_expenses_test.rb
+++ test/system/billing_expenses_test.rb
@@ -33,23 +33,22 @@ class BillingExpensesTest < ApplicationSystemTestCase
     click_button 'Create Billing expense'
     assert page.has_text? 'Billing expense was successfully created.'
 
-    visit volunteer_billing_expense_path(@volunteer, record)
+    click_link 'Billing expense index'
+    within '.table-responsive' do
+      assert page.has_link? @volunteer.contact.full_name
+      assert page.has_text? @volunteer.contact.full_address
+      click_link 'Show'
+    end
     assert page.has_text? 'Spesenauszahlung an'
     assert page.has_text? 'Kostenstelle'
     assert page.has_text? '4182'
     assert page.has_text? 'Konto'
     assert page.has_text? '4621'
     assert page.has_text? 'zu überweisender Betrag'
-    assert page.has_text? @billing_expense.amount
     assert page.has_text? 'Name'
-    assert page.has_text? @volunteer.contact.last_name
     assert page.has_text? 'Vorname'
-    assert page.has_text? @volunteer.contact.first_name
     assert page.has_text? 'Strasse'
-    assert page.has_text? @volunteer.contact.street
     assert page.has_text? 'PLZ / Ort'
-    assert page.has_text? @volunteer.contact.postal_code
-    assert page.has_text? @volunteer.contact.city
     assert page.has_text? 'Bank / IBAN'
   end
 end
