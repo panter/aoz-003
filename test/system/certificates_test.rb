f707e707a320ad29a89caa46a4b4f35dad67fe29
Kaspar Vollenweider <vok@panter.ch>
2017-09-08 12:03:00 +0200

    add some system and policy tests

--- test/system/certificates_test.rb
+++ test/system/certificates_test.rb
@@ -4,7 +4,8 @@ class CertificatesTest < ApplicationSystemTestCase
   def setup
     @user = create :user
     @volunteer = create :volunteer, :with_assignment, state: 'resigned'
-    @hour = create :hour, volunteer: @volunteer, assignment: @volunteer.assignments.first, hours: 2,
+    @assignment = @volunteer.assignments.first
+    @hour = create :hour, volunteer: @volunteer, assignment: @assignment, hours: 2,
       minutes: 15
     login_as @user
   end
@@ -12,8 +13,42 @@ class CertificatesTest < ApplicationSystemTestCase
   test 'only resigned volunteer can get a certificate' do
     active_volunteer = create :volunteer, state: 'active'
     visit volunteer_path(active_volunteer)
-    refute page.has_link? 'Create certificate'
+    assert page.has_no_link? 'Create certificate'
     visit volunteer_path(@volunteer)
     assert page.has_link? 'Create certificate'
+  end
+
+  test 'Creating volunteer certificate form has right content prefilled' do
+    visit volunteer_path(@volunteer)
+    click_link 'Create certificate'
+    assert page.has_text? 'New Certificate'
+    assert page.has_field? 'Text body', text: 'Die **AOZ** ist ein Unternehmen der Stadt Zürich und'
+    assert page.has_field? 'Hours', with: 2
+    click_button 'Edit more fields'
+    assert page.has_field? 'Name', with: @volunteer.contact.full_name
+    assert page.find_field('Street').value.include? @volunteer.contact.street
+    assert page.find_field('City').value.include? @volunteer.contact.city
+    assert page.find_field('Funktion').value.include? 'Förderung der sozialen und beruflichen Integ'
+    assert page.find_field('Institution').value.include? '**AOZ** Zürich, Flüelastrasse 32, 8047'
+    page.find_button('Update Certificate').trigger('click')
+    assert page.has_text? 'AOZ Zürich, Flüelastrasse 32, 8047'
+    assert page.has_text? @volunteer.contact.full_name
+    assert page.has_text? @volunteer.certificates.first.created_at.to_date.to_s
+    assert page.has_link? 'Print'
+  end
+
+  test 'Creating certificate with custom values' do
+    visit volunteer_path(@volunteer)
+    click_link 'Create certificate'
+    click_button 'Edit more fields'
+    fill_in 'Hours', with: 555
+    fill_in 'Name', with: 'This bogus test name'
+    fill_in 'Institution', with: 'The Testology Institute'
+    fill_in 'Text body', with: '**Bold** or not *bold*, that is this tests Question?<br>***both***'
+    page.find_button('Update Certificate').trigger('click')
+    assert page.has_text? '555'
+    assert page.has_text? 'This bogus test name'
+    assert page.has_text? 'The Testology Institute'
+    assert page.has_text? 'Bold or not bold, that is this tests Question? both'
   end
 end
