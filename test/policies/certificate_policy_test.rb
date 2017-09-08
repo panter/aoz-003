66ba70204b9ce79d7bce42e864b7691d296b3dac
Kaspar Vollenweider <vok@panter.ch>
2017-09-08 12:03:00 +0200

    add some system and policy tests

---
+++ test/policies/certificate_policy_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class CertificatePolicyTest < PolicyAssertions::Test
+  test 'superadmin can use all actions' do
+    assert_permit(create(:user), Certificate, 'new?', 'create?', 'index?', 'show?', 'edit?',
+      'update?', 'destroy?')
+  end
+
+  test 'other have no access' do
+    refute_permit(create(:social_worker), Certificate, 'new?', 'create?', 'index?', 'show?',
+      'edit?',
+      'update?', 'destroy?')
+    refute_permit(create(:department_manager), Certificate, 'new?', 'create?', 'index?', 'show?',
+      'edit?',
+      'update?', 'destroy?')
+    refute_permit(create(:user_volunteer), Certificate, 'new?', 'create?', 'index?', 'show?',
+      'edit?',
+      'update?', 'destroy?')
+  end
+end
