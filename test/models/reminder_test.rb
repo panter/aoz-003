710cfefe6b222af475f7fc1c7d0dea361d199cdc
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-09-13 17:19:38 +0200

    model test

---
+++ test/models/reminder_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class ReminderTest < ActiveSupport::TestCase
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
+  test 'create reminder by factory method' do
+    @assignment = create(:assignment)
+    @volunteer = @assignment.volunteer
+    @email = @volunteer.contact.primary_email
+    r = Reminder.create_for(@volunteer, @assignment)
+    mailer = ReminderMailer.reminder_email(@volunteer, r, @email).deliver
+    assert_equal [@email], mailer.to
+    assert_equal ['info@aoz-freiwillige.ch'], mailer.from
+    assert_equal "Reports' confirmation", mailer.subject
+
+    mail_body = mailer.body.encoded
+    assert_match @volunteer.contact.first_name, mail_body
+    assert_match @volunteer.contact.last_name, mail_body
+    assert_match 'Please confirm the hour report and assignment journal for', mail_body
+    assert_match @assignment.client.contact.full_name, mail_body
+
+    assert_equal 1, ActionMailer::Base.deliveries.size
+  end
+
+  test 'creates batch reminders when criteria are met' do
+    10.times do
+      create :assignment, state: 'active'
+    end
+    Reminder.conditionally_create_reminders
+    assert_equal Reminder.count, 10
+    create :assignment, period_start: 1.month.ago
+    create :assignment, confirmation: true
+    create :assignment, state: 'suggested'
+    Reminder.conditionally_create_reminders
+    assert_equal Reminder.count, 10
+  end
+end
