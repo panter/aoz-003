0b906f0d80dbef82df279a8d28300389a8907828
Kaspar Vollenweider <vok@panter.ch>
2017-09-13 14:49:13 +0200

    fix missing softdeleted user with custom getter on journal

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -2,13 +2,17 @@ class Journal < ApplicationRecord
   include ImportRelation
 
   belongs_to :user
-  belongs_to :journalable, polymorphic: true, required: false
-
   belongs_to :assignment, required: false
+
+  belongs_to :journalable, polymorphic: true, required: false
 
   default_scope { order(created_at: :desc) }
 
   CATEGORIES = [:telephone, :conversation, :email, :feedback, :file].freeze
 
   validates :category, presence: true
+
+  def user
+    super || User.deleted.find(user_id)
+  end
 end
