fa6d3c6887b724662197597e4a893f1423325a35
Kaspar Vollenweider <vok@panter.ch>
2017-09-14 10:33:41 +0200

    straight forward relation passing in the transformers

--- lib/access_import/journal_transform.rb
+++ lib/access_import/journal_transform.rb
@@ -1,14 +1,13 @@
 class JournalTransform < Transformer
-  def prepare_attributes(journal, person, assignment, user_id)
+  def prepare_attributes(journal, person, assignment, user)
     {
       body: journal[:m_Text],
-      journalable_type: person.class.name,
-      journalable_id: person.id,
-      assignment_id: assignment&.id,
+      journalable: person,
+      assignment: assignment,
       created_at: journal[:d_ErfDatum],
       updated_at: journal[:d_MutDatum],
       category: CATEGORY_MAP[journal[:fk_JournalKategorie]],
-      user_id: user_id,
+      user: user,
       import_attributes: access_import(
         :tbl_Journal, journal[:pk_Journal], journal: journal
       )
