9d07e2f87af2757273a80432dc0ab816aefed8ce
Kaspar Vollenweider <vok@panter.ch>
2017-09-14 12:13:15 +0200

    safer find_by for softdeleted User and optional instead of required

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -2,7 +2,7 @@ class Journal < ApplicationRecord
   include ImportRelation
 
   belongs_to :user
-  belongs_to :assignment, required: false
+  belongs_to :assignment, optional: true
 
   belongs_to :journalable, polymorphic: true, required: false
 
@@ -13,6 +13,6 @@ class Journal < ApplicationRecord
   validates :category, presence: true
 
   def user
-    super || User.deleted.find(user_id)
+    super || User.deleted.find_by(id: user_id)
   end
 end
