0e06ccbca8f3bdbd9de1eedf70a7f24e08e869c2
Kaspar Vollenweider <vok@panter.ch>
2017-09-19 16:24:51 +0200

    can_manage policy used in consistant manner

--- app/policies/volunteer_policy.rb
+++ app/policies/volunteer_policy.rb
@@ -1,15 +1,15 @@
 class VolunteerPolicy < ApplicationPolicy
-  alias_method :index?,                 :superadmin?
-  alias_method :new?,                   :superadmin?
-  alias_method :create?,                :superadmin?
-  alias_method :destroy?,               :superadmin?
-  alias_method :seeking_clients?,       :superadmin?
-  alias_method :checklist?,             :superadmin?
+  alias_method :index?,           :superadmin?
+  alias_method :new?,             :superadmin?
+  alias_method :create?,          :superadmin?
+  alias_method :destroy?,         :superadmin?
+  alias_method :seeking_clients?, :superadmin?
+  alias_method :checklist?,       :superadmin?
 
-  alias_method :show?,                  :superadmin_or_volunteers_record?
-  alias_method :edit?,                  :superadmin_or_volunteers_record?
-  alias_method :update?,                :superadmin_or_volunteers_record?
+  alias_method :show?,   :superadmin_or_volunteers_record?
+  alias_method :edit?,   :superadmin_or_volunteers_record?
+  alias_method :update?, :superadmin_or_volunteers_record?
 
-  alias_method :supervisor_privileges?, :superadmin?
-  alias_method :supervisor?,            :superadmin?
+  # credential policies
+  alias_method :can_manage?, :superadmin?
 end
