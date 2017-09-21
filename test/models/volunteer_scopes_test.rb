a48498fa8a9aadd9b4f6f37c7cd4605a5d028e37
Kaspar Vollenweider <vok@panter.ch>
2017-09-21 12:19:45 +0200

    starting to fix volunteer scopes
    - testing for fixed inactive assignment scope
    - remove some distincts, as they break other scopes

--- test/models/volunteer_scopes_test.rb
+++ test/models/volunteer_scopes_test.rb
@@ -2,17 +2,24 @@ require 'test_helper'
 
 class VolunteerScopesTest < ActiveSupport::TestCase
   def setup
-    @now = Time.zone.now
+    @today = Time.zone.now.to_date
     @with_assignment = create :volunteer
     @with_multiple_assignments = create :volunteer
     @with_inactive_assignment = create :volunteer
+    @with_active_and_inactive_assignment = create :volunteer
     @no_assignment = create :volunteer
+    assignments_make_parameters.map { |parameters| make_assignment(*parameters) }
+  end
+
+  def assignments_make_parameters
     [
       ['start_60_days_ago', @with_assignment, @now.days_ago(60), nil],
       ['start_in_one_month', @with_multiple_assignments, @now.next_month.end_of_month, nil],
       ['start_7_days_ago', @with_multiple_assignments, @now.days_ago(7), nil],
       ['end_15_days_ago', @with_multiple_assignments, @now.days_ago(30), @now.days_ago(15)],
       ['end_future', @with_multiple_assignments, @now.days_ago(5), @now.next_month.end_of_month],
+      ['active_asignment', @with_active_and_inactive_assignment, @today - 300, nil],
+      ['inactive_asignment', @with_active_and_inactive_assignment, @today - 300, @today - 200],
       ['end_30_days_ago', @with_inactive_assignment, @now.days_ago(60), @now.days_ago(30)]
     ].map { |parameters| make_assignment(*parameters) }
     @group_offer_member = create :volunteer
@@ -22,36 +29,40 @@ class VolunteerScopesTest < ActiveSupport::TestCase
   end
 
   test 'with_assignments returns only the ones that have assignments' do
-    query = Volunteer.with_assignments
+    query = Volunteer.with_assignments.distinct
     assert query.include? @with_assignment
     assert query.include? @with_multiple_assignments
     assert query.include? @with_inactive_assignment
     refute query.include? @no_assignment
     refute query.include? @group_offer_member
     refute query.include? @group_offer_responsible
-    assert_equal 3, query.count
   end
 
   test 'with_active_assignments returns only volunteers that have active assignments' do
-    query = Volunteer.with_active_assignments
+    query = Volunteer.with_active_assignments.distinct
     assert query.include? @with_assignment
     assert query.include? @with_multiple_assignments
+    assert query.include? @with_active_and_inactive_assignment
     refute query.include? @with_inactive_assignment
     refute query.include? @no_assignment
     refute query.include? @group_offer_member
     refute query.include? @group_offer_responsible
-    assert_equal 2, query.count
+  end
+
+  test 'with_inactive_assignments' do
+    query = Volunteer.with_inactive_assignments.distinct
+    assert query.include? @with_inactive_assignment
+    refute query.include? @with_active_and_inactive_assignment
   end
 
   test 'with_active_assignments_between returns volunteers with active assignments in date range' do
-    query = Volunteer.with_active_assignments_between(@now.days_ago(16), @now.days_ago(8))
+    query = Volunteer.with_active_assignments_between(@today - 16, @today - 8)
     assert query.include? @with_assignment
     assert query.include? @with_multiple_assignments
     refute query.include? @with_inactive_assignment
     refute query.include? @no_assignment
     refute query.include? @group_offer_member
     refute query.include? @group_offer_responsible
-    assert_equal 2, query.count
   end
 
   test 'without_assignment returns volunteers with no assignments' do
