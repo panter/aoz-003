a60d19d2002df0960bb61b493bd1189d6a28b625
Kaspar Vollenweider <vok@panter.ch>
2017-09-21 15:12:54 +0200

    remove seeking client test from normal volunteer test ...
    ... because volunteer_scopes_test covers this

--- test/models/volunteer_test.rb
+++ test/models/volunteer_test.rb
@@ -14,19 +14,6 @@ class VolunteerTest < ActiveSupport::TestCase
     assert new_volunteer.contact.present?
   end
 
-  test 'only accepted, active further and inactive volunteers should show up for assignment' do
-    @accepted = create :volunteer, state: Volunteer::ACCEPTED
-    @active_further = create :volunteer, state: Volunteer::ACTIVE_FURTHER
-    @inactive = create :volunteer, state: Volunteer::INACTIVE
-    @registered = create :volunteer, state: Volunteer::REGISTERED
-    @contacted = create :volunteer, state: Volunteer::CONTACTED
-    @active = create :volunteer, state: Volunteer::ACTIVE
-    @rejected = create :volunteer, state: Volunteer::REJECTED
-    @resigned = create :volunteer, state: Volunteer::RESIGNED
-    result = Volunteer.seeking_clients
-    assert_equal [@inactive, @active_further, @accepted], result.to_a
-  end
-
   test 'external field is default false' do
     assert_equal false, @volunteer.external
   end
