5a57d7d415dd31b0d9e058a1701278a6798877cc
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-09-21 17:48:27 +0200

    add missing deleted_at column to categories

--- db/schema.rb
+++ db/schema.rb
@@ -163,8 +163,10 @@ ActiveRecord::Schema.define(version: 20170922120423) do
     t.string "category_name"
     t.string "category_state", default: "active"
     t.bigint "group_offers_id"
+    t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["deleted_at"], name: "index_group_offer_categories_on_deleted_at"
     t.index ["group_offers_id"], name: "index_group_offer_categories_on_group_offers_id"
   end
 
