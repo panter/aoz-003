59cb529d77f2dcb6c33c91a6bee941b1872255ab
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-09-22 18:48:48 +0200

    test department manager options

--- test/factories/group_offers.rb
+++ test/factories/group_offers.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :group_offer do
     title 'MyString'
     necessary_volunteers 5
+    association :responsible, factory: :volunteer
   end
 end
--- test/models/volunteer_scopes_test.rb
+++ test/models/volunteer_scopes_test.rb
@@ -15,6 +15,10 @@ class VolunteerScopesTest < ActiveSupport::TestCase
       ['end_future', @with_multiple_assignments, @now.days_ago(5), @now.next_month.end_of_month],
       ['end_30_days_ago', @with_inactive_assignment, @now.days_ago(60), @now.days_ago(30)]
     ].map { |parameters| make_assignment(*parameters) }
+    @group_offer_member = create :volunteer
+    @group_offer_responsible = create :volunteer
+    group_offer = create :group_offer, responsible: @group_offer_responsible
+    group_offer.volunteers << @group_offer_member
   end
 
   test 'with_assignments returns only the ones that have assignments' do
@@ -23,6 +27,8 @@ class VolunteerScopesTest < ActiveSupport::TestCase
     assert query.include? @with_multiple_assignments
     assert query.include? @with_inactive_assignment
     refute query.include? @no_assignment
+    refute query.include? @group_offer_member
+    refute query.include? @group_offer_responsible
     assert_equal 3, query.count
   end
 
@@ -32,6 +38,8 @@ class VolunteerScopesTest < ActiveSupport::TestCase
     assert query.include? @with_multiple_assignments
     refute query.include? @with_inactive_assignment
     refute query.include? @no_assignment
+    refute query.include? @group_offer_member
+    refute query.include? @group_offer_responsible
     assert_equal 2, query.count
   end
 
@@ -41,6 +49,8 @@ class VolunteerScopesTest < ActiveSupport::TestCase
     assert query.include? @with_multiple_assignments
     refute query.include? @with_inactive_assignment
     refute query.include? @no_assignment
+    refute query.include? @group_offer_member
+    refute query.include? @group_offer_responsible
     assert_equal 2, query.count
   end
 
@@ -50,7 +60,42 @@ class VolunteerScopesTest < ActiveSupport::TestCase
     refute query.include? @with_multiple_assignments
     refute query.include? @with_inactive_assignment
     assert query.include? @no_assignment
-    assert_equal 1, query.count
+    assert query.include? @group_offer_member
+    assert query.include? @group_offer_responsible
+    assert_equal 3, query.count
+  end
+
+  test 'not_in_any_group_offer returns volunteers not in group offer (as members)' do
+    query = Volunteer.not_in_any_group_offer
+    assert query.include? @with_assignment
+    assert query.include? @with_multiple_assignments
+    assert query.include? @with_inactive_assignment
+    assert query.include? @no_assignment
+    refute query.include? @group_offer_member
+    assert query.include? @group_offer_responsible
+    assert_equal 5, query.count
+  end
+
+  test 'not_responsible returns volunteers not responsible for group offer' do
+    query = Volunteer.not_responsible
+    assert query.include? @with_assignment
+    assert query.include? @with_multiple_assignments
+    assert query.include? @with_inactive_assignment
+    assert query.include? @no_assignment
+    assert query.include? @group_offer_member
+    refute query.include? @group_offer_responsible
+    assert_equal 5, query.count
+  end
+
+  test 'without_active_assignment.not_responsible.not_in_any_group_offer' do
+    query = Volunteer.without_active_assignment.not_responsible.not_in_any_group_offer
+    refute query.include? @with_assignment
+    assert query.include? @with_multiple_assignments
+    assert query.include? @with_inactive_assignment
+    refute query.include? @no_assignment
+    refute query.include? @group_offer_member
+    refute query.include? @group_offer_responsible
+    assert_equal 2, query.count
   end
 
   def make_assignment(title, volunteer, start_date = nil, end_date = nil)
