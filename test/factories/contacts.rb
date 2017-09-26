8a4c3f7ed3baa0fd8dfac4e4f1c22f2f33ff6302
Kaspar Vollenweider <vok@panter.ch>
2017-09-26 17:16:40 +0200

    change back contact factory because it broke tests

--- test/factories/contacts.rb
+++ test/factories/contacts.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :contact do
-    first_name Faker::Name.first_name
-    last_name Faker::Name.last_name
+    sequence :first_name { |n| "first name #{n}" }
+    sequence :last_name { |n| "last name #{n}" }
     extended Faker::Address.secondary_address
     street Faker::Address.street_address
     city Faker::Address.city
     postal_code Faker::Number.between(1100, 7500).to_s
-    primary_email Faker::Internet.email
+    sequence :primary_email { |n| "primary_#{n}@example.com" }
     primary_phone Faker::PhoneNumber.phone_number
     secondary_phone Faker::PhoneNumber.cell_phone
 
