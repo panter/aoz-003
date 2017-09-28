a137cb3247afca59d1773d5fc3e91c02497945c7
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-09-28 11:31:03 +0200

    filter by category

--- test/system/group_offer_filters_test.rb
+++ test/system/group_offer_filters_test.rb
@@ -4,12 +4,20 @@ class GroupOfferFiltersTest < ApplicationSystemTestCase
   def setup
     @d1 = create :department
     @d2 = create :department
-    @open_d1 = create(:group_offer, title: 'o1', offer_state: 'open', department: @d1)
-    @full_d1 = create(:group_offer, title: 'f1', offer_state: 'full', department: @d1)
-    @part_d1 = create(:group_offer, title: 'p1', offer_state: 'partially_occupied', department: @d1)
-    @open_d2 = create(:group_offer, title: 'o2', offer_state: 'open', department: @d2)
-    @full_d2 = create(:group_offer, title: 'f2', offer_state: 'full', department: @d2)
-    @part_d2 = create(:group_offer, title: 'p2', offer_state: 'partially_occupied', department: @d2)
+    @c1 = create :group_offer_category
+    @c2 = create :group_offer_category
+    @open_d1 = create(:group_offer, title: 'o1', offer_state: 'open', department: @d1,
+      group_offer_category: @c1)
+    @full_d1 = create(:group_offer, title: 'f1', offer_state: 'full', department: @d1,
+      group_offer_category: @c2)
+    @part_d1 = create(:group_offer, title: 'p1', offer_state: 'partially_occupied', department: @d1,
+      group_offer_category: @c1)
+    @open_d2 = create(:group_offer, title: 'o2', offer_state: 'open', department: @d2,
+      group_offer_category: @c2)
+    @full_d2 = create(:group_offer, title: 'f2', offer_state: 'full', department: @d2,
+      group_offer_category: @c1)
+    @part_d2 = create(:group_offer, title: 'p2', offer_state: 'partially_occupied', department: @d2,
+      group_offer_category: @c2)
     login_as create(:user)
     visit group_offers_path
   end
@@ -70,7 +78,35 @@ class GroupOfferFiltersTest < ApplicationSystemTestCase
     end
   end
 
-  test 'filter by department and state' do
+  test 'filter by category' do
+    within '.section-navigation#filters' do
+      click_link 'Group offer category: All'
+    end
+    visit current_url
+    within 'tbody' do
+      assert page.has_text? @open_d1.title
+      assert page.has_text? @full_d1.title
+      assert page.has_text? @part_d1.title
+      assert page.has_text? @open_d2.title
+      assert page.has_text? @full_d2.title
+      assert page.has_text? @part_d2.title
+    end
+    within '.section-navigation#filters' do
+      click_link 'Group offer category: All'
+      click_link @c1.to_s
+    end
+    visit current_url
+    within 'tbody' do
+      assert page.has_text? @open_d1.title
+      refute page.has_text? @full_d1.title
+      assert page.has_text? @part_d1.title
+      refute page.has_text? @open_d2.title
+      assert page.has_text? @full_d2.title
+      refute page.has_text? @part_d2.title
+    end
+  end
+
+  test 'filter by department, state and category' do
     within '.section-navigation#filters' do
       click_link 'Offer state: All'
       click_link 'Open'
@@ -98,6 +134,19 @@ class GroupOfferFiltersTest < ApplicationSystemTestCase
       assert page.has_text? @open_d1.title
       assert page.has_text? @full_d1.title
       assert page.has_text? @part_d1.title
+      refute page.has_text? @open_d2.title
+      refute page.has_text? @full_d2.title
+      refute page.has_text? @part_d2.title
+    end
+    within '.section-navigation#filters' do
+      click_link 'Group offer category: All'
+      click_link @c2.to_s
+    end
+    visit current_url
+    within 'tbody' do
+      refute page.has_text? @open_d1.title
+      assert page.has_text? @full_d1.title
+      refute page.has_text? @part_d1.title
       refute page.has_text? @open_d2.title
       refute page.has_text? @full_d2.title
       refute page.has_text? @part_d2.title
