a0e776c305dd90a04e8044e8158759cae31a5be8
Kaspar Vollenweider <vok@panter.ch>
2017-09-29 13:12:11 +0200

    volunteer external internal switch user handling

--- test/models/volunteer_test.rb
+++ test/models/volunteer_test.rb
@@ -22,19 +22,26 @@ class VolunteerTest < ActiveSupport::TestCase
     external_volunteer = create :volunteer, external: true
     volunteer_user = create :user_volunteer
     external_volunteer.user = volunteer_user
-    refute external_volunteer.valid?
+    assert external_volunteer.invalid?
     assert_equal ['must be blank'], external_volunteer.errors.messages[:user]
     external_volunteer.external = false
     assert external_volunteer.valid?
   end
 
-  test 'a internal volunteer turned into a external, having a user, user gets softdeleted' do
+  test 'an internal volunteer turned into a external, having a user, user gets softdeleted' do
     volunteer_user = create :user_volunteer
     volunteer = create :volunteer, state: 'accepted', external: false, user: volunteer_user
     assert volunteer.valid?
-    volunteer.update external: true
-    volunteer.save
-    refute volunteer_user.deleted_at.nil?
-    assert volunteer.user_id.nil?
+    volunteer.update(external: true)
+    assert volunteer.user.deleted?
+  end
+
+  test 'an external volunteer used to be internal with user turned back internal gets back user' do
+    volunteer_user = create :user_volunteer
+    volunteer = create :volunteer, state: 'accepted', external: false, user: volunteer_user
+    volunteer.update(external: true)
+    volunteer.reload
+    volunteer.update(external: false)
+    refute volunteer.user.deleted?
   end
 end
