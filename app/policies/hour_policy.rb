a6b0d489749ab00885f305ca1c75e3242c8ccd66
Kaspar Vollenweider <vok@panter.ch>
2017-10-06 12:49:35 +0200

    fix showing hours index with volunteer not having any hours error

--- app/policies/hour_policy.rb
+++ app/policies/hour_policy.rb
@@ -1,7 +1,12 @@
 class HourPolicy < ApplicationPolicy
   def index?
-    superadmin? || volunteer? && user.volunteer.id == record.volunteer.id
+    superadmin? || volunteer? && handle_record_or_class
   end
+
+  def handle_record_or_class
+    record.class == Class ? true : user.volunteer.id == record.volunteer.id
+  end
+
   alias_method :show?,           :superadmin_or_volunteer_related?
   alias_method :new?,            :superadmin_or_volunteer_related?
   alias_method :edit?,           :superadmin_or_volunteer_related?
