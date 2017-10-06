a6d13546264edc18a12327299ecdee97e9ccd4bc
Kaspar Vollenweider <vok@panter.ch>
2017-10-06 12:02:29 +0200

    converting novell methods to maybe somewhat different namaing

--- app/policies/volunteer_policy.rb
+++ app/policies/volunteer_policy.rb
@@ -2,28 +2,28 @@ class VolunteerPolicy < ApplicationPolicy
   class Scope < ApplicationScope
     def resolve
       return all if superadmin?
-      return seeking_clients if department_manager? || social_worker?
+      return seeking_clients if department_manager_or_social_worker?
       none
     end
     alias :seeking_clients :resolve
   end
 
   # controller action policies
-  alias_method :index?,           :superadmin_or_department_manager?
-  alias_method :new?,             :superadmin_or_department_manager?
-  alias_method :create?,          :superadmin_or_department_manager?
-  alias_method :seeking_clients?, :superadmin_or_department_manager?
+  alias_method :index?,           :volunteer_managing_user?
+  alias_method :new?,             :volunteer_managing_user?
+  alias_method :create?,          :volunteer_managing_user?
+  alias_method :seeking_clients?, :volunteer_managing_user?
 
   alias_method :destroy?,         :superadmin?
   alias_method :index_xls?,       :superadmin?
 
-  def superadmin_departmentmanager_or_volunteers_profile?
-    superadmin_or_department_manager? || user_owns_record?
+  def volunteer_managing_or_volunteers_profile?
+    volunteer_managing_user? || user_owns_record?
   end
 
-  alias_method :show?,   :superadmin_departmentmanager_or_volunteers_profile?
-  alias_method :edit?,   :superadmin_departmentmanager_or_volunteers_profile?
-  alias_method :update?, :superadmin_departmentmanager_or_volunteers_profile?
+  alias_method :show?,   :volunteer_managing_or_volunteers_profile?
+  alias_method :edit?,   :volunteer_managing_or_volunteers_profile?
+  alias_method :update?, :volunteer_managing_or_volunteers_profile?
 
   # suplementary policies
   alias_method :can_manage?, :superadmin?
