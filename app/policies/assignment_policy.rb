36bda6d67437f94516d19fe18bd4c83723747fc4
Kaspar Vollenweider <vok@panter.ch>
2017-10-06 10:56:35 +0200

    somewhat cleaning up the policies

--- app/policies/assignment_policy.rb
+++ app/policies/assignment_policy.rb
@@ -2,27 +2,25 @@ class AssignmentPolicy < ApplicationPolicy
   class Scope < ApplicationScope
     def resolve
       return all if superadmin?
-      scope.where(creator_id: user.id) if department_manager?
+      return scope.where(creator_id: user.id) if department_manager? || social_worker?
       none
     end
-
-    def seeking_clients
-      return scope.seeking_clients_will_take_more if superadmin?
-      return scope.seeking_clients if department_manager?
-      none
-    end
+    alias :seeking_clients :resolve
   end
 
+  # controller action policies
   alias_method :index?,          :superadmin_or_department_manager?
-  alias_method :show?,           :admin_or_department_manager_or_assignment_related?
-  alias_method :edit?,           :admin_or_department_manager_or_assignment_related?
-  alias_method :update?,         :admin_or_department_manager_or_assignment_related?
   alias_method :new?,            :superadmin_or_department_manager?
   alias_method :create?,         :superadmin_or_department_manager?
-  alias_method :destroy?,        :superadmin?
   alias_method :find_volunteer?, :superadmin_or_department_manager?
   alias_method :find_client?,    :superadmin_or_department_manager?
 
-  # credential policies
-  alias_method :supervisor?,     :superadmin_or_department_manager?
+  alias_method :show?,   :admin_or_department_manager_or_assignment_related?
+  alias_method :edit?,   :admin_or_department_manager_or_assignment_related?
+  alias_method :update?, :admin_or_department_manager_or_assignment_related?
+
+  alias_method :destroy?, :superadmin?
+
+  # supplemental policies
+  alias_method :supervisor?, :superadmin_or_department_manager?
 end
