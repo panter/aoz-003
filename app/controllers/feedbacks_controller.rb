5fdeeaf06f2f43b22e66f4f00474fc97d7870e87
Kaspar Vollenweider <vok@panter.ch>
2017-10-06 17:18:55 +0200

    further in polymorphizing the feedbacks

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,28 +1,26 @@
 class FeedbacksController < ApplicationController
   before_action :set_feedback, only: [:show, :edit, :update, :destroy]
-  before_action :set_assignment
+  before_action :set_feedback_about
 
   def index
     authorize Feedback
-    @feedbacks = policy_scope(Feedback).where(assignment: @assignment)
+    @feedbacks = policy_scope(Feedback).where(feedbackable: @assignment)
   end
 
   def show; end
 
   def new
-    @feedback = Feedback.new(assignment: @assignment,
-      volunteer: @assignment.volunteer)
+    @feedback = Feedback.new(feedbackable: @feedback_about, volunteer: current_user.volunteer)
     authorize @feedback
   end
 
   def edit; end
 
   def create
-    @feedback = Feedback.new(feedback_params
-      .merge(author_id: current_user.id))
+    @feedback = Feedback.new(feedback_params.merge(author_id: current_user.id))
     authorize @feedback
     if @feedback.save
-      redirect_to assignment_feedback_path(@assignment, @feedback), make_notice
+      redirect_to url_for([@feedback_about, @feedback]), make_notice
     else
       render :new
     end
@@ -30,7 +28,7 @@ class FeedbacksController < ApplicationController
 
   def update
     if @feedback.update(feedback_params)
-      redirect_to assignment_feedback_path(@assignment, @feedback), make_notice
+      redirect_to url_for([@feedback_about, @feedback]), make_notice
     else
       render :edit
     end
@@ -38,7 +36,7 @@ class FeedbacksController < ApplicationController
 
   def destroy
     @feedback.destroy
-    redirect_to assignment_feedbacks_path, make_notice
+    redirect_back(fallback_location: url_for(@feedback_about))
   end
 
   private
@@ -48,12 +46,14 @@ class FeedbacksController < ApplicationController
     authorize @feedback
   end
 
-  def set_assignment
-    @assignment = Assignment.find(params[:assignment_id]) if params[:assignment_id]
+  def set_feedback_about
+    return @feedback_about = Assignment.find(params[:assignment_id]) if params[:assignment_id]
+    return @feedback_about = GroupOffer.find(params[:group_offer_id]) if params[:group_offer_id]
+    @feedback_about = nil
   end
 
   def feedback_params
-    params.require(:feedback).permit(:goals, :achievements, :future, :comments,
-      :conversation, :assignment_id, :volunteer_id)
+    params.require(:feedback).permit(:goals, :achievements, :future, :comments, :conversation,
+      :feedbackable_id, :feedbackable_type, :volunteer_id)
   end
 end
---
+++ db/migrate/20171006140658_modify_feedbacks_to_be_polymorphic.rb
@@ -0,0 +1,17 @@
+class ModifyFeedbacksToBePolymorphic < ActiveRecord::Migration[5.1]
+  def up
+    add_reference :feedbacks, :feedbackable, polymorphic: true, index: true
+    Feedback.with_deleted.each do |feedback|
+      feedback.update(feedbackable_id: feedback.assignment_id, feedbackable_type: 'Assignment')
+    end
+    remove_belongs_to :feedbacks, :assignment
+  end
+
+  def down
+    add_reference :feedbacks, :assignments, foreign_key: true
+    Feedback.with_deleted.where(feedbackable_type: 'Assignment').each do |feedback|
+      feedback.update(assignment_id: feedback.feedbackable_id)
+    end
+    remove_belongs_to :feedbacks, :feedbackable
+  end
+end
