6243bdc5c45cc53fbe2025c7a80c31b8fbaf2773
Kaspar Vollenweider <vok@panter.ch>
2017-10-06 16:31:39 +0200

    fix title to search for in test

--- test/system/hours_test.rb
+++ test/system/hours_test.rb
@@ -9,7 +9,8 @@ class HoursTest < ApplicationSystemTestCase
     @client1.contact.first_name = @client1.contact.last_name = 'Client1'
     @assignment1 = create :assignment, volunteer: @volunteer1, client: @client1
     @group_offer_category = create :group_offer_category
-    @group_offer1 = create :group_offer, volunteers: [@volunteer1], group_offer_category: @group_offer_category
+    @group_offer1 = create :group_offer, volunteers: [@volunteer1],
+      group_offer_category: @group_offer_category, title: 'GroupOfferNumberOne'
     login_as @user_volunteer1
     visit root_url
     click_link 'volunteer1@example.com'
@@ -73,14 +74,15 @@ class HoursTest < ApplicationSystemTestCase
   test 'volunteer can see only her and group_offers' do
     @user_volunteer2 = create :user, role: 'volunteer', email: 'volunteer2@example.com'
     @volunteer2 = @user_volunteer2.volunteer = create :volunteer
-    @group_offer2 = create :group_offer, volunteers: [@volunteer2], group_offer_category: @group_offer_category
+    @group_offer2 = create :group_offer, volunteers: [@volunteer2],
+      group_offer_category: @group_offer_category, title: 'GroupOfferNumberTwo'
 
     @group_offer_hour1 = create :hour, hourable: @group_offer1, volunteer: @volunteer1
     @group_offer_hour2 = create :hour, hourable: @group_offer2, volunteer: @volunteer2
     click_link 'Hour reports'
     assert page.has_text? @group_offer1.to_label
     # somehow this is still showing up, but don't understand why..
-    # refute page.has_text? @group_offer2.to_label
+    refute page.has_text? @group_offer2.to_label
     visit volunteer_hours_path(@volunteer2)
     assert page.has_text? 'You are not authorized to perform this action.'
     refute page.has_text? @group_offer2.to_label
