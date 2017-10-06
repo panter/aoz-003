a965b941079ed38549c7c584461caf53b2ae92ab
Kaspar Vollenweider <vok@panter.ch>
2017-10-06 12:15:23 +0200

    fix alignment and grouping in policies

--- app/policies/group_offer_policy.rb
+++ app/policies/group_offer_policy.rb
@@ -11,14 +11,18 @@ class GroupOfferPolicy < ApplicationPolicy
     end
   end
 
-  alias_method :index?,                 :superadmin_or_department_manager?
-  alias_method :show?,                  :superadmin_or_departments_offer?
-  alias_method :new?,                   :superadmin_or_department_manager?
-  alias_method :edit?,                  :superadmin_or_departments_offer?
-  alias_method :create?,                :superadmin_or_department_manager?
-  alias_method :update?,                :superadmin_or_departments_offer?
-  alias_method :destroy?,               :superadmin?
+  # controller action policies
+  alias_method :index?,               :superadmin_or_department_manager?
+  alias_method :show?,                :superadmin_or_departments_offer?
+  alias_method :new?,                 :superadmin_or_department_manager?
+  alias_method :edit?,                :superadmin_or_departments_offer?
+  alias_method :create?,              :superadmin_or_department_manager?
+  alias_method :update?,              :superadmin_or_departments_offer?
+  alias_method :archived?,            :superadmin_or_department_manager?
+  alias_method :change_active_state?, :superadmin_or_departments_offer?
+
+  alias_method :destroy?, :superadmin?
+
+  # supplemental policies
   alias_method :supervisor_privileges?, :superadmin?
-  alias_method :archived?,              :superadmin_or_department_manager?
-  alias_method :change_active_state?,   :superadmin_or_departments_offer?
 end
--- app/policies/hour_policy.rb
+++ app/policies/hour_policy.rb
@@ -1,9 +1,9 @@
 class HourPolicy < ApplicationPolicy
-  alias_method :index?,          :superadmin_or_volunteer_related?
-  alias_method :show?,           :superadmin_or_volunteer_related?
-  alias_method :new?,            :superadmin_or_volunteer_related?
-  alias_method :edit?,           :superadmin_or_volunteer_related?
-  alias_method :create?,         :superadmin_or_volunteer_related?
-  alias_method :update?,         :superadmin_or_volunteer_related?
-  alias_method :destroy?,        :superadmin_or_volunteer_related?
+  alias_method :index?,   :superadmin_or_volunteer_related?
+  alias_method :show?,    :superadmin_or_volunteer_related?
+  alias_method :new?,     :superadmin_or_volunteer_related?
+  alias_method :edit?,    :superadmin_or_volunteer_related?
+  alias_method :create?,  :superadmin_or_volunteer_related?
+  alias_method :update?,  :superadmin_or_volunteer_related?
+  alias_method :destroy?, :superadmin_or_volunteer_related?
 end
