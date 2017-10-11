1fac0112a6946924c90095d59fb538e6d2a58153
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-10-11 17:22:58 +0200

    correct hour policies

--- app/policies/hour_policy.rb
+++ app/policies/hour_policy.rb
@@ -1,12 +1,6 @@
 class HourPolicy < ApplicationPolicy
-  def index?
-    superadmin? || volunteer? && handle_record_or_class
-  end
-
-  def handle_record_or_class
-    record.class == Class ? true : user.volunteer.id == record.volunteer.id
-  end
-
+  alias_method :supervisor?,     :superadmin?
+  alias_method :index?,          :superadmin_or_volunteer_related?
   alias_method :show?,           :superadmin_or_volunteer_related?
   alias_method :new?,            :superadmin_or_volunteer_related?
   alias_method :edit?,           :superadmin_or_volunteer_related?
