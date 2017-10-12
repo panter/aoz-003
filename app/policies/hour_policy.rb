457c577c9c259fdfdff95abe282c7b43ee1de554
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-10-12 14:11:27 +0200

    add back working policy

--- app/policies/hour_policy.rb
+++ app/policies/hour_policy.rb
@@ -1,13 +1,13 @@
 class HourPolicy < ApplicationPolicy
-  class Scope < ApplicationScope
-    def resolve
-      return resolve_volunteer if superadmin?
-      resolve_volunteer_and_hour if volunteer?
-    end
+  def index?
+    superadmin? || volunteer? && handle_record_or_class
+  end
+
+  def handle_record_or_class
+    record.class == Class ? true : user.volunteer.id == record.volunteer.id
   end
 
   alias_method :supervisor?,     :superadmin?
-  alias_method :index?,          :superadmin_or_volunteer_related?
   alias_method :show?,           :superadmin_or_volunteer_related?
   alias_method :new?,            :superadmin_or_volunteer_related?
   alias_method :edit?,           :superadmin_or_volunteer_related?
