5a82f86c939d5397e9315c9b7549c6da84d6a059
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-10-12 15:42:47 +0200

    fix callback overwrite

--- app/models/group_assignment.rb
+++ app/models/group_assignment.rb
@@ -3,13 +3,18 @@ class GroupAssignment < ApplicationRecord
   belongs_to :volunteer
   has_many :group_assignment_logs
 
-  after_save :save_group_assignment_logs, if: :saved_change_to_start_date?
-  after_save :save_group_assignment_logs, if: :saved_change_to_end_date?
+  after_save :save_group_assignment_logs, if: :dates_updated?
   before_destroy :save_group_assignment_logs
 
   def save_group_assignment_logs
     group_assignment_logs.create!(group_offer_id: group_offer_id, volunteer_id: volunteer_id,
       group_assignment_id: id, title: group_offer.title, start_date: start_date_before_last_save,
       end_date: end_date_before_last_save, responsible: responsible)
+  end
+
+  private
+
+  def dates_updated?
+    saved_change_to_start_date? || saved_change_to_end_date?
   end
 end
