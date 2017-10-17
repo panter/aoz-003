5f27148abfc6df3de533c8779c36c2be346953df
Kaspar Vollenweider <vok@panter.ch>
2017-10-17 13:08:13 +0200

    some factorized seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 include ApplicationHelper
 Faker::Config.locale = 'de'
 
+EMAIL_DOMAIN = '@example.com'.freeze
+
 def random_relation
   [
     'wife', 'husband', 'mother', 'father', 'daughter',
@@ -41,142 +43,81 @@ def make_lang_skills
 end
 
 [:superadmin, :social_worker, :department_manager].each do |role|
-  user = FactoryGirl.create(:user, role: role, email: "#{role}@example.com", password: 'asdfasdf')
-  user.profile.contact.primary_email = "#{role}@example.com"
+  user = FactoryGirl.create(:user, role: role, email: "#{role}#{EMAIL_DOMAIN}",
+    password: 'asdfasdf')
+  user.profile.contact.primary_email = "#{role}#{EMAIL_DOMAIN}"
   user.save!
 end
 
-FactoryGirl.create(:user_volunteer, volunteer: FactoryGirl.create(:volunteer),
-  email: 'volunteer@example.com', password: 'asdfasdf')
-
+FactoryGirl.create(
+  :user_volunteer,
+  password: 'asdfasdf',
+  email: "volunteer#{EMAIL_DOMAIN}",
+  volunteer: FactoryGirl.create(:volunteer)
+)
 
 User.where(role: ['superadmin', 'social_worker']).each do |user|
   next if user.clients.count > 1
-  user.clients = Array.new(4).map do
-    Client.new do |client|
-      client.build_contact(
-        first_name: Faker::Name.first_name,
-        last_name: Faker::Name.last_name,
-        primary_email: Faker::Internet.email,
-        primary_phone: Faker::PhoneNumber.phone_number,
-        street: Faker::Address.street_address,
-        postal_code: Faker::Address.zip_code,
-        city: Faker::Address.city
-      )
-      client.journals = [
-        Journal.new(
-          subject: Faker::Lorem.sentence(rand(2..5)),
-          body: Faker::Lorem.sentence(rand(2..5)),
-          user: user,
-          category: random_category
-        ),
-        Journal.new(
-          subject: Faker::Lorem.sentence(rand(2..5)),
-          body: Faker::Lorem.sentence(rand(2..5)),
-          user: user,
-          category: random_category
-        )
-      ]
-      client.birth_year = Faker::Date.birthday(18, 65)
-      client.salutation = ['mr', 'mrs'].sample
-      client.nationality = ISO3166::Country.codes.sample
-      client.relatives = make_relatives
-      client.language_skills = make_lang_skills
-      client.age_request = random_age_request
-      client.gender_request = random_gender_request
-      availability_collection.each do |availability|
-        client[availability] = [true, false].sample
-      end
-    end
+
+  journals = [
+    Journal.new(
+      subject: Faker::Lorem.sentence(rand(2..5)),
+      body: Faker::Lorem.sentence(rand(2..5)),
+      user: user,
+      category: random_category
+    ),
+    Journal.new(
+      subject: Faker::Lorem.sentence(rand(2..5)),
+      body: Faker::Lorem.sentence(rand(2..5)),
+      user: user,
+      category: random_category
+    )
+  ]
+  user.clients << Array.new(2).map do
+    FactoryGirl.create :client_seed, journals: journals, relatives: make_relatives
   end
   user.save
 end
 
 if Department.count < 1
-  department = Department.new do |d|
-    d.contact = Contact.new do |c|
-      c.last_name = 'Bogus Department'
-      c.street = Faker::Address.street_address
-      c.postal_code = Faker::Address.zip_code
-      c.city = Faker::Address.city
-      c.primary_email = Faker::Internet.email
-      c.primary_phone = Faker::PhoneNumber.phone_number
-    end
-    d.user.push User.find_by(role: 'department_manager')
+  group_offer_category = FactoryGirl.create :group_offer_category
+  # first department for user with login
+  department_manager = User.find_by role: 'department_manager'
+  group_offers = [
+    FactoryGirl.create(:group_offer, group_offer_category: group_offer_category),
+    FactoryGirl.create(:group_offer, group_offer_category: group_offer_category),
+  ]
+  FactoryGirl.create :department, user: [department_manager], group_offers: group_offers
+
+  # additional departments
+  3.times do
+    group_offers = [
+      FactoryGirl.create(:group_offer, group_offer_category: group_offer_category),
+      FactoryGirl.create(:group_offer, group_offer_category: group_offer_category),
+    ]
+    FactoryGirl.create :department, user: [FactoryGirl.create(:department_manager)],
+      group_offers: group_offers
   end
-  department.save!
 end
 
-Volunteer.acceptance_collection.each do |state|
-  vol = Volunteer.new do |volunteer|
-    volunteer.acceptance = state
-    volunteer.build_contact(
-      title: Faker::Name.title,
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      postal_code: Faker::Address.zip_code,
-      city: Faker::Address.city,
-      street: Faker::Address.street_address,
-      primary_email: Faker::Internet.email,
-      primary_phone: Faker::PhoneNumber.phone_number
-    )
-    volunteer.journals = [
-      Journal.new(
-        subject: Faker::Lorem.sentence(rand(2..5)),
-        body: Faker::Lorem.sentence(rand(2..5)),
-        user: User.first,
-        category: random_category
-      ),
-      Journal.new(
-        subject: Faker::Lorem.sentence(rand(2..5)),
-        body: Faker::Lorem.sentence(rand(2..5)),
-        user: User.first,
-        category: random_category
-      )
-    ]
-    volunteer.birth_year = Faker::Date.birthday(18, 75)
-    volunteer.profession = Faker::Company.profession
-    volunteer.salutation = ['mr', 'mrs'].sample
-    volunteer.working_percent = "#{rand(2..10)}0"
-    Volunteer::SINGLE_ACCOMPANIMENTS.each do |bool_attr|
-      volunteer[bool_attr] = [true, false].sample
-    end
-    Volunteer::GROUP_ACCOMPANIMENTS.each do |bool_attr|
-      volunteer[bool_attr] = [true, false].sample
-    end
-    [:nationality, :additional_nationality].each { |n| volunteer[n] = ISO3166::Country.codes.sample }
-    volunteer.education = "#{Faker::Educator.secondary_school}, #{Faker::Educator.university}"
-    [:motivation, :expectations, :strengths, :interests].each do |attribute|
-      volunteer[attribute] = Faker::Lorem.sentence(rand(2..5))
-    end
-    volunteer.experience = [true, false].sample
-    volunteer.zurich = [true, false].sample
-    volunteer.strengths = "#{Faker::Job.key_skill}, #{Faker::Job.key_skill}"
-    volunteer.language_skills = make_lang_skills
-    availability_collection.each do |availability|
-      volunteer[availability] = [true, false].sample
-    end
-  end
-  vol.save!
+# Create volunteers for each acceptance type
+Volunteer.acceptance_collection.each do |acceptance|
+  vol = FactoryGirl.create(:volunteer_seed, acceptance: acceptance)
+  # Create a volunteer user for the volunteer if accepted
   if vol.accepted?
     FactoryGirl.create(:user, role: 'volunteer', volunteer: vol, email: vol.contact.primary_email)
   end
 end
 
+# Create VolunteerEmails
 if VolunteerEmail.count < 1
-  5.times do
-    VolunteerEmail.new do |ve|
-      ve.subject = Faker::Lorem.sentence
-      ve.title = Faker::Lorem.sentence
-      ve.body = Array.new(5).map do
-        Faker::ChuckNorris.fact
-      end.join(' ')
-      ve.active = true
-      ve.user = User.first
-    end.save
+  FactoryGirl.create :volunteer_email_seed, active: true
+  2.times do
+    FactoryGirl.create :volunteer_email_seed, active: false
   end
 end
 
+# Create assignments
 if Assignment.count < 1
   10.times do
     client = FactoryGirl.create :client, state: Client::ACTIVE
@@ -190,6 +131,7 @@ if Assignment.count < 1
   end
 end
 
+# create Assignment Journal
 Assignment.all.each do |assignment|
   FactoryGirl.create(:assignment_journal, volunteer: assignment.volunteer, assignment: assignment,
     author_id: [User.superadmins.last.id, assignment.volunteer&.user&.id].compact.sample)
--- test/factories/journals.rb
+++ test/factories/journals.rb
@@ -4,5 +4,12 @@ FactoryGirl.define do
     sequence :body { |n| " Bogus_text_body_#{n}" * 30 }
     user
     category Journal::CATEGORIES.sample
+
+    trait :faker_text do
+      subject Faker::Hipster.sentence
+      body Faker::Hipster.paragraph
+    end
+
+    factory :journal_seed, traits: [:faker_text]
   end
 end
