ee81eae39d0eed47eae165369cef3b3ea2ad3817
Kaspar Vollenweider <vok@panter.ch>
2017-10-17 17:48:37 +0200

    some mangling with the relation and the forms

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,4 +1,6 @@
 class Feedback < ApplicationRecord
+  attr_reader :feedbackable_id_and_type
+
   belongs_to :volunteer
   belongs_to :author, class_name: 'User'
   belongs_to :feedbackable, polymorphic: true, optional: true
@@ -9,5 +11,13 @@ class Feedback < ApplicationRecord
 
   def group_offer?
     feedbackable_type == 'GroupOffer'
+  end
+
+  def feedbackable_id_and_type=(id_and_type)
+    self.feedbackable_id, self.feedbackable_type = id_and_type.split(',', 2)
+  end
+
+  def feedbackable_id_and_type
+    "#{feedbackable_id},#{feedbackable_type}"
   end
 end
