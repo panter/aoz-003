7a46cdf0d477af9681eeb3e33c99464d1547ad5c
Kaspar Vollenweider <vok@panter.ch>
2017-10-17 13:08:13 +0200

    some factorized seeding

--- test/factories/contacts.rb
+++ test/factories/contacts.rb
@@ -10,6 +10,15 @@ FactoryGirl.define do
     primary_phone Faker::PhoneNumber.phone_number
     secondary_phone Faker::PhoneNumber.cell_phone
 
+    trait :faker_name do
+      first_name Faker::Name.first_name
+      last_name Faker::Name.last_name
+    end
+
+    trait :faker_email do
+      primary_email Faker::Internet.email
+    end
+
     trait :zuerich do
       city 'Zürich'
       postal_code Client.zuerich_zips.sample
@@ -21,5 +30,6 @@ FactoryGirl.define do
 
     factory :contact_department, traits: [:zuerich, :department]
     factory :contact_zuerich, traits: [:zuerich]
+    factory :contact_seed, traits: [:faker_name, :faker_email]
   end
 end
