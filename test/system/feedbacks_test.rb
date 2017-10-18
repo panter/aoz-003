b809d2b9a3ba2ee2945c1ba4efa41c167f13f3d4
Kaspar Vollenweider <vok@panter.ch>
2017-10-18 13:15:25 +0200

    system tests and some fixes to get things up

--- test/system/feedbacks_test.rb
+++ test/system/feedbacks_test.rb
@@ -10,27 +10,33 @@ class FeedbacksTest < ApplicationSystemTestCase
       volunteer: @volunteer, author: @superadmin, comments: 'author superadmin'
     login_as @user_volunteer
     @other_volunteer = create :volunteer, user: create(:user_volunteer)
+    @group_offer = create :group_offer, necessary_volunteers: 2, title: 'some_group_offer',
+    volunteers: [@volunteer, @other_volunteer]
+    create :feedback, feedbackable: @group_offer, author: @user_volunteer,
+    volunteer: @volunteer, comments: 'some_group_offer_feedback'
+    create :feedback, feedbackable: @group_offer, author: @superadmin, volunteer: @volunteer,
+      comments: 'some_group_offer_superadmin_feedback'
+    create :feedback, volunteer: @other_volunteer, feedbackable: @group_offer,
+      author: @superadmin, comments: 'other_volunteers_group_offer_feedback'
   end
 
-  test 'volunteer can create an feedback' do
+  test 'volunteer can see assignment feedbacks index' do
     visit root_url
     click_link 'volunteer@example.com'
     click_link 'Show profile'
-    click_link 'New Feedback'
-    fill_in 'Which were the most important goals during the last months?', with: 'asdf'
-    fill_in 'What could have been achieved during the last months?', with: 'asdf'
-    fill_in 'Should the assignment continue? If yes, with which goals?', with: 'asdf'
-    fill_in 'Comments', with: 'asdf'
-    page.check('feedback_conversation')
-    click_button 'Create Feedback'
-    assert page.has_text? 'Feedback was successfully created.'
+    within '.assignments-table' do
+      click_link 'Feedback index'
+    end
+    refute page.has_text? 'author superadmin'
   end
 
-  test 'volunteer can see only her feedbacks' do
+  test 'volunteer can see group offer feedbacks index' do
     visit root_url
     click_link 'volunteer@example.com'
     click_link 'Show profile'
-    click_link 'Feedback index'
+    within '.group-assignments-table' do
+      click_link 'Feedback index'
+    end
     refute page.has_text? 'author superadmin'
   end
 
@@ -46,28 +52,116 @@ class FeedbacksTest < ApplicationSystemTestCase
   end
 
   test 'group offer feedbacks index contains only the feedbacks related to that group offer' do
-    group_offer = create :group_offer, necessary_volunteers: 2, title: 'some_group_offer',
-      volunteers: [@volunteer, @other_volunteer]
-    some_feedback = create :feedback, feedbackable: group_offer, author: @user_volunteer,
-      volunteer: @volunteer, comments: 'some_feedback'
-    some_superadmin_feedback = create :feedback, feedbackable: group_offer, author: @superadmin,
-      volunteer: @volunteer, comments: 'some_superadmin_feedback'
-    other_group_offer = create :group_offer, necessary_volunteers: 2, title: 'some_other_group_offer',
-      volunteers: [@volunteer, @other_volunteer]
-    other_feedback = create :feedback, volunteer: @volunteer, feedbackable: other_group_offer,
+    other_group_offer = create :group_offer, title: 'some_other_group_offer',
+      volunteers: [@volunteer, @other_volunteer], necessary_volunteers: 2
+    create :feedback, volunteer: @volunteer, feedbackable: other_group_offer,
       author: @user_volunteer, comments: 'other_feedback'
     login_as @user_volunteer
-    visit group_offer_feedbacks_path(group_offer)
+    visit group_offer_feedbacks_path(@group_offer)
+    assert page.has_text? 'some_group_offer_feedback'
+    refute page.has_text? 'some_group_offer_superadmin_feedback'
+    refute page.has_text? 'other_volunteers_group_offer_feedback'
+    login_as @superadmin
+    visit group_offer_feedbacks_path(@group_offer)
+    assert page.has_text? 'some_group_offer_feedback'
+    assert page.has_text? 'some_group_offer_superadmin_feedback'
+    assert page.has_text? 'other_volunteers_group_offer_feedback'
+  end
+
+  test 'assignment feedbacks index contains only the feedbacks related to that assignment' do
+    create :feedback, volunteer: @volunteer, author: @user_volunteer, feedbackable: @assignment,
+      comments: 'some_feedback'
+    create :feedback, volunteer: @volunteer, author: @superadmin, feedbackable: @assignment,
+      comments: 'some_superadmin_feedback'
+    other_assignment = create :assignment, volunteer: @volunteer
+    create :feedback, feedbackable: other_assignment, volunteer: @volunteer,
+      author: @user_volunteer, comments: 'other_feedback'
+    login_as @user_volunteer
+    visit assignment_feedbacks_path(@assignment)
     assert page.has_text? 'some_feedback'
     refute page.has_text? 'some_superadmin_feedback'
     refute page.has_text? 'other_feedback'
     login_as @superadmin
-    visit group_offer_feedbacks_path(group_offer)
+    visit assignment_feedbacks_path(@assignment)
+    assert page.has_text? 'some_feedback'
+    assert page.has_text? 'some_superadmin_feedback'
+    refute page.has_text? 'other_feedback'
+  end
+
+
+  test 'volunteer feedbacks index contains only the feedbacks related to that group offer' do
+    create :feedback, volunteer: @volunteer, author: @user_volunteer, feedbackable: @assignment,
+      comments: 'some_assignment_feedback'
+    create :feedback, volunteer: @volunteer, author: @superadmin, feedbackable: @assignment,
+      comments: 'some_assignment_superadmin_feedback'
+    other_assignment = create :assignment, volunteer: @volunteer
+    create :feedback, feedbackable: other_assignment, volunteer: @volunteer,
+      author: @user_volunteer, comments: 'other_assignment_feedback'
+    login_as @user_volunteer
+    visit volunteer_feedbacks_path(@assignment)
+    assert page.has_text? 'some_assignment_feedback'
+    refute page.has_text? 'some_assignment_superadmin_feedback'
+    assert page.has_text? 'other_assignment_feedback'
+    assert page.has_text? 'some_group_offer_feedback'
+    refute page.has_text? 'some_group_offer_superadmin_feedback'
+    refute page.has_text? 'other_volunteers_group_offer_feedback'
+    login_as @superadmin
+    visit volunteer_feedbacks_path(@assignment)
+    assert page.has_text? 'some_assignment_feedback'
+    assert page.has_text? 'some_assignment_superadmin_feedback'
+    assert page.has_text? 'other_assignment_feedback'
+    assert page.has_text? 'some_group_offer_feedback'
+    assert page.has_text? 'some_group_offer_superadmin_feedback'
+    refute page.has_text? 'other_volunteers_group_offer_feedback'
   end
 
   test 'volunteer can create only their feedbacks' do
     assignment = create :assignment, volunteer: create(:volunteer)
     visit new_assignment_feedback_path(assignment)
     assert page.has_text? 'You are not authorized to perform this action.'
+  end
+
+  test 'create new assignment feedback' do
+    login_as @user_volunteer
+    visit volunteer_path(@volunteer)
+    within '.assignments-table' do
+      click_link 'New Feedback'
+    end
+    fill_in 'Which were the most important goals during the last months?',
+      with: 'important_goals_answer_given'
+    fill_in 'What could have been achieved during the last months?',
+      with: 'achievment_answer_given'
+    fill_in 'Should the assignment continue? If yes, with which goals?',
+      with: 'continue_answer_given'
+    fill_in 'Comments', with: 'comments_given'
+    page.check('feedback_conversation')
+    click_button 'Create Feedback'
+    assert page.has_text? 'Feedback was successfully created.'
+    assert page.has_text? 'important_goals_answer_given'
+    assert page.has_text? 'achievment_answer_given'
+    assert page.has_text? 'continue_answer_given'
+    assert page.has_text? 'comments_given'
+  end
+
+  test 'create new group_offer feedback' do
+    login_as @user_volunteer
+    visit volunteer_path(@volunteer)
+    within '.group-assignments-table' do
+      click_link 'New Feedback'
+    end
+    fill_in 'Which were the most important goals during the last months?',
+      with: 'important_goals_answer_given'
+    fill_in 'What could have been achieved during the last months?',
+      with: 'achievment_answer_given'
+    fill_in 'Should the assignment continue? If yes, with which goals?',
+      with: 'continue_answer_given'
+    fill_in 'Comments', with: 'new_comments_given'
+    page.check('feedback_conversation')
+    click_button 'Create Feedback'
+    assert page.has_text? 'Feedback was successfully created.'
+    assert page.has_text? 'important_goals_answer_given'
+    assert page.has_text? 'achievment_answer_given'
+    assert page.has_text? 'continue_answer_given'
+    assert page.has_text? 'new_comments_given'
   end
 end
