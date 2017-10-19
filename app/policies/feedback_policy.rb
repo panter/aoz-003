61f936f061b44baa323976fbdd3c87c430fd7137
Casaper <vok@panter.ch>
2017-10-19 12:43:23 +0200

    feedback_policy checks fixed and tests for it
    - feedback policy did not check if volunteer is eligeble to give
      feedback on feedbackable
    - policy tests that check for this

--- app/policies/feedback_policy.rb
+++ app/policies/feedback_policy.rb
@@ -6,22 +6,23 @@ class FeedbackPolicy < ApplicationPolicy
     end
   end
 
-  alias_method :index?, :superadmin_or_volunteer?
-
   def superadmin_or_volunteers_feedback?
-    superadmin? || volunteer? && of_and_from_volunteer?
+    superadmin? || volunteer? && of_and_from_volunteer? && in_feedbackable?
   end
 
   def of_and_from_volunteer?
-    if record.feedbackable.class == Assignment
-      return record.feedbackable.volunteer.id == user.volunteer.id &&
-          user.volunteer.id == record.volunteer.id &&
-          user.id == record.author.id
-    end
-    user.volunteer.id == record.volunteer.id && user.id == record.author.id &&
-      record.feedbackable.volunteers.ids.include?(user.volunteer.id)
+    user.volunteer.id == record.volunteer.id && user.id == record.author.id
   end
 
+  def in_feedbackable?
+    if record.feedbackable.class == Assignment
+      record.feedbackable.volunteer.id == user.volunteer.id
+    else
+      record.feedbackable.volunteers.ids.include? user.volunteer.id
+    end
+  end
+
+  alias_method :index?,   :superadmin_or_volunteer?
   alias_method :new?,     :superadmin_or_volunteers_feedback?
   alias_method :show?,    :superadmin_or_volunteers_feedback?
   alias_method :edit?,    :superadmin_or_volunteers_feedback?
