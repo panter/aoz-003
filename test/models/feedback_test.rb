b0d6532157463eab6e401240e3f3300d02d250a5
Kaspar Vollenweider <vok@panter.ch>
2017-10-19 19:27:44 +0200

    remove obsolete test that factory test covers

--- test/models/feedback_test.rb
+++ test/models/feedback_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class FeedbackTest < ActiveSupport::TestCase
-  test 'feedback is valid' do
-    feedback = build :feedback
-    assert feedback.valid?
-  end
-
   test 'feedback with no comments is invalid' do
     volunteer = create :volunteer, user: create(:user_volunteer)
     invalid_feedback = Feedback.new(volunteer: volunteer, author: volunteer.user)
