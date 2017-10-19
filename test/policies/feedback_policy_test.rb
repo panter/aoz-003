886aade349a3935866ed2f32b44a2205364b770c
Casaper <vok@panter.ch>
2017-10-19 12:43:23 +0200

    feedback_policy checks fixed and tests for it
    - feedback policy did not check if volunteer is eligeble to give
      feedback on feedbackable
    - policy tests that check for this

--- test/policies/feedback_policy_test.rb
+++ test/policies/feedback_policy_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class FeedbackPolicyTest < PolicyAssertions::Test
+  def setup
+    @volunteer = create :volunteer, user: create(:user_volunteer)
+    @user_volunteer = @volunteer.user
+    @superadmin = create :user
+    @other_volunteer = create :volunteer, user: create(:user_volunteer)
+  end
+
   test 'superadmin can use all actions' do
     assert_permit(create(:user), Feedback, 'new?', 'create?', 'index?', 'show?', 'edit?',
       'update?', 'destroy?')
@@ -13,13 +20,36 @@ class FeedbackPolicyTest < PolicyAssertions::Test
       'show?', 'edit?', 'update?', 'destroy?')
   end
 
-  test 'volunteer has limited access' do
-    user_volunteer = create :user_volunteer
-    volunteer = create :volunteer, user: user_volunteer
-    foreign_feedback = create :feedback, volunteer: volunteer, author: create(:user)
-    feedback_volunteer = create :feedback, volunteer: volunteer, author: user_volunteer
-    refute_permit(user_volunteer, foreign_feedback, 'show?', 'edit?', 'update?', 'destroy?')
-    assert_permit(user_volunteer, feedback_volunteer, 'index?', 'show?', 'edit?', 'update?',
-      'destroy?')
+  test 'volunteer has limited access to assignment feedbacks' do
+    assignment = create :assignment, volunteer: @volunteer
+    other_assignment = create :assignment, volunteer: @other_volunteer
+    superadmin_feedback = create :feedback, volunteer: @volunteer, author: @superadmin,
+      feedbackable: assignment
+    feedback_volunteer = create :feedback, volunteer: @volunteer, author: @user_volunteer,
+      feedbackable: assignment
+    foreign_feedback = create :feedback, feedbackable: other_assignment,
+      volunteer: @other_volunteer, author: @other_volunteer.user
+    refute_permit(@user_volunteer, superadmin_feedback, 'show?', 'edit?', 'update?', 'destroy?')
+    refute_permit(@user_volunteer, foreign_feedback, 'show?', 'edit?', 'update?', 'destroy?',
+      'new?', 'create?')
+    assert_permit(@user_volunteer, feedback_volunteer, 'index?', 'show?', 'edit?', 'update?',
+      'destroy?', 'new?', 'create?')
+  end
+
+  test 'volunteer has limited access to group_offer feedbacks' do
+    group_offer = create :group_offer, volunteers: [@volunteer, @other_volunteer]
+    other_group_offer = create :group_offer,
+      volunteers: [create(:volunteer, user: create(:user_volunteer)), @other_volunteer]
+    superadmin_feedback = create :feedback, volunteer: @volunteer, author: @superadmin,
+      feedbackable: group_offer
+    feedback_volunteer = create :feedback, volunteer: @volunteer, author: @user_volunteer,
+      feedbackable: group_offer
+    foreign_feedback = create :feedback, volunteer: @other_volunteer, author: @other_volunteer.user,
+      feedbackable: other_group_offer
+    refute_permit(@user_volunteer, superadmin_feedback, 'show?', 'edit?', 'update?', 'destroy?')
+    refute_permit(@user_volunteer, foreign_feedback, 'show?', 'edit?', 'update?', 'destroy?',
+      'new?', 'create?')
+    assert_permit(@user_volunteer, feedback_volunteer, 'index?', 'show?', 'edit?', 'update?',
+      'destroy?', 'new?', 'create?')
   end
 end
