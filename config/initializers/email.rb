c249fbce7aebdc6e718fd19c65cdbb719e815780
Kaspar Vollenweider <vok@panter.ch>
2017-10-19 16:34:26 +0200

    get default_url_options for mailer from env variable

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   ActionMailer::Base.delivery_method = :sendmail
 
   ActionMailer::Base.default_url_options = {
-    host: 'staging.aoz-freiwillige.ch',
+    host: ENV['DEVISE_EMAIL_DOMAIN'] || 'staging.aoz-freiwillige.ch',
     protocol: 'https'
   }
 else
