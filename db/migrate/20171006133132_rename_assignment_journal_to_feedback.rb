012038dde341dbc18c838359b6d7d4511abc5778
Kaspar Vollenweider <vok@panter.ch>
2017-10-20 09:54:09 +0200

    use change instead of up and down

--- db/migrate/20171006133132_rename_assignment_journal_to_feedback.rb
+++ db/migrate/20171006133132_rename_assignment_journal_to_feedback.rb
@@ -1,9 +1,5 @@
 class RenameAssignmentJournalToFeedback < ActiveRecord::Migration[5.1]
-  def up
+  def change
     rename_table :assignment_journals, :feedbacks
-  end
-
-  def down
-    rename_table :feedbacks, :assignment_journals
   end
 end
