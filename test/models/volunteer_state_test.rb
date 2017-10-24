12c49a155372340c28b422f7e817b8041dc5b5da
Kaspar Vollenweider <vok@panter.ch>
2017-10-24 16:58:14 +0200

    test inactive? method

--- test/models/volunteer_state_test.rb
+++ test/models/volunteer_state_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class VolunteerStateTest < ActiveSupport::TestCase
-  test 'assignment activness makes volunteers state' do
+  test 'active? method' do
     volunteer = create :volunteer
     refute volunteer.active?
     assignment_one = create :assignment, volunteer: volunteer, period_start: 50.days.ago, period_end: nil
@@ -14,5 +14,20 @@ class VolunteerStateTest < ActiveSupport::TestCase
     assert volunteer.active?
     assignment_two.update(period_end: 10.days.ago)
     refute volunteer.active?
+  end
+
+  test 'inactive? method' do
+    volunteer = create :volunteer
+    assert volunteer.inactive?
+    assignment_one = create :assignment, volunteer: volunteer, period_start: 50.days.ago, period_end: nil
+    refute volunteer.inactive?
+    assignment_one.update(period_end: 40.days.ago)
+    assert volunteer.inactive?
+    assignment_two = create :assignment, volunteer: volunteer, period_start: 50.days.ago, period_end: nil
+    refute volunteer.inactive?
+    assignment_two.update(period_end: Time.zone.today + 100)
+    refute volunteer.inactive?
+    assignment_two.update(period_end: 10.days.ago)
+    assert volunteer.inactive?
   end
 end
