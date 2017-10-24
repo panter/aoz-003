e82ac7a989f8cdc1dcbab15661b5a361b1ee5362
Kaspar Vollenweider <vok@panter.ch>
2017-10-24 17:13:46 +0200

    add testing for group_assignment membership to state tests

--- test/models/volunteer_state_test.rb
+++ test/models/volunteer_state_test.rb
@@ -14,6 +14,16 @@ class VolunteerStateTest < ActiveSupport::TestCase
     assert volunteer.active?
     assignment_two.update(period_end: 10.days.ago)
     refute volunteer.active?
+    group_assignment_one = GroupAssignment.create(volunteer: volunteer, period_start: 20.days.ago,
+      period_end: nil, group_offer: create(:group_offer))
+    assert volunteer.active?
+    group_assignment_one.update(period_end: 10.days.ago)
+    refute volunteer.active?
+    group_assignment_two = GroupAssignment.create(volunteer: volunteer, period_start: 20.days.ago,
+      period_end: Time.zone.today + 100, group_offer: create(:group_offer))
+    assert volunteer.active?
+    group_assignment_two.update(period_end: 10.days.ago)
+    refute volunteer.active?
   end
 
   test 'inactive? method' do
@@ -29,5 +39,42 @@ class VolunteerStateTest < ActiveSupport::TestCase
     refute volunteer.inactive?
     assignment_two.update(period_end: 10.days.ago)
     assert volunteer.inactive?
+    group_assignment_one = GroupAssignment.create(volunteer: volunteer, period_start: 20.days.ago,
+      period_end: nil, group_offer: create(:group_offer))
+    refute volunteer.inactive?
+    group_assignment_one.update(period_end: 10.days.ago)
+    assert volunteer.inactive?
+    group_assignment_two = GroupAssignment.create(volunteer: volunteer, period_start: 20.days.ago,
+      period_end: Time.zone.today + 100, group_offer: create(:group_offer))
+    refute volunteer.inactive?
+    group_assignment_two.update(period_end: 10.days.ago)
+    assert volunteer.inactive?
+  end
+
+  test 'state method' do
+    volunteer = create :volunteer, acceptance: 0
+    assert_equal 'undecided', volunteer.state
+    volunteer.accepted!
+    assert_equal :inactive, volunteer.state
+    assignment_one = create :assignment, volunteer: volunteer, period_start: 50.days.ago, period_end: nil
+    assert_equal :active, volunteer.state
+    assignment_one.update(period_end: 40.days.ago)
+    assert_equal :inactive, volunteer.state
+    assignment_two = create :assignment, volunteer: volunteer, period_start: 50.days.ago, period_end: nil
+    assert_equal :active, volunteer.state
+    assignment_two.update(period_end: Time.zone.today + 100)
+    assert_equal :active, volunteer.state
+    assignment_two.update(period_end: 10.days.ago)
+    assert_equal :inactive, volunteer.state
+    group_assignment_one = GroupAssignment.create(volunteer: volunteer, period_start: 20.days.ago,
+      period_end: nil, group_offer: create(:group_offer))
+    assert_equal :active, volunteer.state
+    group_assignment_one.update(period_end: 10.days.ago)
+    assert_equal :inactive, volunteer.state
+    group_assignment_two = GroupAssignment.create(volunteer: volunteer, period_start: 20.days.ago,
+      period_end: Time.zone.today + 100, group_offer: create(:group_offer))
+    assert_equal :active, volunteer.state
+    group_assignment_two.update(period_end: 10.days.ago)
+    assert_equal :inactive, volunteer.state
   end
 end
