b7eb768e1b219ddcfa811bab504c4c9425ec7654
Kaspar Vollenweider <vok@panter.ch>
2017-10-25 14:40:28 +0200

    add common scopes of group_assignment and assignment to a concern

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,5 +1,6 @@
 class Assignment < ApplicationRecord
   include ImportRelation
+  include GroupAssignmentAndAssignmentScopes
 
   after_update :delete_reminder, if: :saved_change_to_confirmation?
 
@@ -23,45 +24,18 @@ class Assignment < ApplicationRecord
 
   validates :client_id, uniqueness: { scope: :volunteer_id, message: I18n.t('assignment_exists') }
 
-  scope :default_order, (-> { order(created_at: :desc) })
 
-  scope :created_between, ->(start_date, end_date) { where(created_at: start_date..end_date) }
   scope :created_before, ->(max_time) { where('assignments.created_at < ?', max_time) }
   scope :created_after, ->(min_time) { where('assignments.created_at > ?', min_time) }
 
-  scope :no_end, (-> { where(period_end: nil) })
-  scope :has_end, (-> { where.not(period_end: nil) })
 
   scope :ended, (-> { where('assignments.period_end < ?', Time.zone.today) })
-  scope :end_before, ->(date) { where('period_end < ?', date) }
-  scope :end_after, ->(date) { where('period_end > ?', date) }
-  scope :end_within, ->(date_range) { where(period_end: date_range) }
 
-  scope :end_in_future, (-> { where('period_end > ?', Time.zone.today) })
-  scope :not_ended, (-> { no_end.or(end_in_future) })
-
-  scope :no_start, (-> { where(period_start: nil) })
-  scope :started, (-> { where('period_start < ?', Time.zone.today) })
-  scope :will_start, (-> { where('period_start > ?', Time.zone.today) })
-  scope :start_before, ->(date) { where('period_start < ?', date) }
-  scope :start_after, ->(date) { where('period_start > ?', date) }
-  scope :start_within, ->(date_range) { where(period_start: date_range) }
-
-  scope :started_six_months_ago, (-> { where('period_start < ?', 6.months.ago.to_date) })
 
   def started_six_months_ago?
     period_start < 6.months.ago.to_date
   end
 
-  scope :active, (-> { not_ended.started })
-  scope :inactive, (-> { ended.or(no_start) })
-
-  scope :active_between, lambda { |start_date, end_date|
-    no_end.start_before(end_date)
-          .or(
-            start_before(end_date).end_after(start_date)
-          )
-  }
 
   scope :zurich, (-> { joins(:client).merge(Client.zurich) })
   scope :not_zurich, (-> { joins(:client).merge(Client.not_zurich) })
@@ -69,7 +43,6 @@ class Assignment < ApplicationRecord
   scope :internal, (-> { joins(:volunteer).merge(Volunteer.internal) })
   scope :external, (-> { joins(:volunteer).merge(Volunteer.external) })
 
-  scope :with_hours, (-> { joins(:hours) })
 
   def ended?
     period_end && period_end < Time.zone.today
