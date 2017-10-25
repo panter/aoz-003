c575753c7bde6a85ad104563689c69ea6b8b5240
Kaspar Vollenweider <vok@panter.ch>
2017-10-25 15:07:50 +0200

    trying to fix group offer xlsx export test

--- test/integration/group_offers_xlsx_export_test.rb
+++ test/integration/group_offers_xlsx_export_test.rb
@@ -3,24 +3,27 @@ require 'test_helper'
 class GroupOffersXlsxExportTest < ActionDispatch::IntegrationTest
   def setup
     @superadmin = create :user
-    @group_offers = Array.new(10).map { create :group_offer }
-    login_as @superadmin
-    get group_offers_url(format: :xlsx)
   end
 
   test 'xlsx file is downloadable' do
+    @group_offers = Array.new(10).map { create :group_offer }
+    login_as @superadmin
+    get group_offers_url(format: :xlsx)
     assert_equal 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
       response.content_type
   end
 
   test 'xlsx files has the right columns' do
+    create :group_offer
+    login_as @superadmin
+    get group_offers_url(format: :xlsx)
     excel_file = Tempfile.new
     excel_file.write(response.body)
     excel_file.close
     wb = Roo::Spreadsheet.open(excel_file.path, extension: 'xlsx')
 
     assert_equal wb.cell(1, 1), 'Title'
-    assert_equal wb.cell(2, 1), @group_offers.last.title
+    assert_equal wb.cell(2, 1), @group_offers.first.title
     assert_equal wb.cell(1, 2), 'Location'
     assert_equal wb.cell(1, 3), 'Availability'
     assert_equal wb.cell(1, 4), 'Target group'
