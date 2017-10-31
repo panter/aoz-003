dcf28984b8a004a4a03ecab766cacc0791d6c532
Kaspar Vollenweider <vok@panter.ch>
2017-10-31 19:13:34 +0100

    More mangling with display and order in forms

--- app/models/language_skill.rb
+++ app/models/language_skill.rb
@@ -1,21 +1,23 @@
 class LanguageSkill < ApplicationRecord
   belongs_to :languageable, polymorphic: true, optional: true
 
-  scope :native_languages, lambda {
-    where(level: 'native_speaker')
-      .order("CASE WHEN language = 'DE' THEN 1 WHEN language != 'DE' THEN 2 END")
-  }
-  scope :foreign_languages, -> { where.not(id: native_languages.first&.id) }
-
   LANGUAGE_LEVELS = [:native_speaker, :fluent, :good, :basic].freeze
 
   scope :native_languages, lambda {
     where(level: 'native_speaker')
       .order("CASE WHEN language = 'DE' THEN 1 ELSE 2 END")
   }
+
+  scope :german, (-> { where(language: 'DE') })
+
   scope :foreign_languages, lambda {
     return none unless native_language.id
     where.not(id: native_language.id)
+  }
+
+  scope :lang_order, lambda {
+    order("CASE WHEN language = 'DE' THEN 1 ELSE 2 END")
+      .order("CASE WHEN level = 'native_speaker' THEN 3 ELSE 4 END")
   }
 
   def self.native_language
