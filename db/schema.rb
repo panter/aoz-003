0badc8882bfe0363cf7f5ee51188986a14a40919
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-11-01 10:17:10 +0100

    journal migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027140514) do
+ActiveRecord::Schema.define(version: 20171101081636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
