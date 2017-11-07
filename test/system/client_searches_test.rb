3a4d318ced6a60ffe6f904e420610f4026def087
Kaspar Vollenweider <vok@panter.ch>
2017-11-07 18:00:18 +0100

    fix first tests for client search

--- test/system/client_searches_test.rb
+++ test/system/client_searches_test.rb
@@ -3,20 +3,27 @@ require 'application_system_test_case'
 class ClientSearchesTest < ApplicationSystemTestCase
   def setup
     @superadmin = create :user
-    @clients = ('a'..'f').to_a.map do |letter|
+    @clients = ('a'..'z').to_a.map do |letter|
       client_one = create :client
-      client_one.contact.update(last_name: letter * 5 + client_one.contact.last_name)
+      client_one.contact.update(first_name: (letter * 5) + client_one.contact.first_name)
       client_two = create :client
-      client_two.contact.update(last_name: letter * 5 + client_two.contact.last_name)
+      client_two.contact.update(last_name: (letter * 5) + client_two.contact.last_name)
       [client_one, client_two]
     end
-  end
-
-  test 'enter search text brings suggestions' do
     login_as @superadmin
     visit clients_path
-    fill_in 'Suche', with: 'aaaa'
-    assert page.has_text? @clients[0][0].contact.last_name
-    assert page.has_text? @clients[0][1].contact.last_name
+  end
+
+  test 'basic_non_suggests_search_works' do
+    fill_in name: 'q[contact_full_name_cont]', with: 'zzzz'
+    click_button 'Suchen'
+    assert page.has_text? @clients.last.first.contact.full_name
+    assert page.has_text? @clients.last.last.contact.full_name
+  end
+
+  test 'enter_search_text_brings_suggestions' do
+    fill_in name: 'q[contact_full_name_cont]', with: 'zzzz'
+    assert page.has_text? @clients.last.first.contact.full_name
+    assert page.has_text? @clients.last.last.contact.full_name
   end
 end
