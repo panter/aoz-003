2718884adba9f39d68f5fde4d157e2c0a28aaa9a
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-11-07 14:59:03 +0100

    data migration

--- db/migrate/20171103121704_add_entry_date_to_client.rb
+++ db/migrate/20171103121704_add_entry_date_to_client.rb
@@ -1,6 +1,9 @@
 class AddEntryDateToClient < ActiveRecord::Migration[5.1]
   def change
-    remove_column :clients, :entry_year, :date
     add_column :clients, :entry_date, :string
+    Client.find_each do |client|
+      client.update(entry_date: client.entry_year)
+    end
+    remove_column :clients, :entry_year, :date
   end
 end
