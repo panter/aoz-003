e71ea4432b0fd53721282d712b91ea4ec050b9e0
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-11-07 13:14:04 +0100

    replace entry year with entry date (textfield)

--- db/migrate/20171103121704_add_entry_date_to_client.rb
+++ db/migrate/20171103121704_add_entry_date_to_client.rb
@@ -1,5 +1,6 @@
 class AddEntryDateToClient < ActiveRecord::Migration[5.1]
   def change
+    remove_column :clients, :entry_year, :date
     add_column :clients, :entry_date, :string
     Client.find_each do |client|
       client.update(entry_date: client.entry_year)
