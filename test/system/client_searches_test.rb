9fcd95a3bcc8b4e6b3d312c21c1df352aae3911d
Kaspar Vollenweider <vok@panter.ch>
2017-11-08 08:49:25 +0100

    playing around with possibilities to test search suggest drop down

--- test/system/client_searches_test.rb
+++ test/system/client_searches_test.rb
@@ -22,8 +22,11 @@ class ClientSearchesTest < ApplicationSystemTestCase
   end
 
   test 'enter_search_text_brings_suggestions' do
-    fill_in name: 'q[contact_full_name_cont]', with: 'zzzz'
+    fill_autocomplete 'q[contact_full_name_cont]', with: 'zzzz'
+    # fill_in name: 'q[contact_full_name_cont]', with: 'zzzz'
+    assert false
     assert page.has_text? @clients.last.first.contact.full_name
     assert page.has_text? @clients.last.last.contact.full_name
   end
+
 end
