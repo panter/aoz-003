6024b2244d71c39d7836b8b7c125dd2ee515383f
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-11-08 17:59:31 +0100

    Revert "fix deprecated change checker really and test for it"
    This reverts commit 430b9c542ba1b96bd8f1a520856f50ef6f817c44.

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -41,14 +41,4 @@ class ContactTest < ActiveSupport::TestCase
     refute @contact.valid?
     assert_equal @contact.errors.keys, [:last_name]
   end
-
-  test 'update_first_or_lastname_changes_full_name_atribute' do
-    volunteer = create :volunteer
-    full_name_before = volunteer.contact.full_name
-    volunteer.contact.update(first_name: 'aaaaaaaa')
-    assert_not_equal full_name_before, volunteer.contact.full_name
-    full_name_before = volunteer.contact.full_name
-    volunteer.contact.update(last_name: 'zzzzzzzzz')
-    assert_not_equal full_name_before, volunteer.contact.full_name
-  end
 end
