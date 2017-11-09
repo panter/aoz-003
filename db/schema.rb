e77d62d0b35a27e1b52517cfd963be843404557c
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-11-09 16:57:03 +0100

    add description to group offer categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109152750) do
+ActiveRecord::Schema.define(version: 20171109154602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -198,6 +198,7 @@ ActiveRecord::Schema.define(version: 20171109152750) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["deleted_at"], name: "index_group_offer_categories_on_deleted_at"
   end
 
