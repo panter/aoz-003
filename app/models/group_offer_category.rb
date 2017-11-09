ffb829f6aa642b6a0a91ee971783980a4e3718e5
Ales Rosina <ales.rosina@gmail.com>
2017-11-09 16:16:22 +0100

    Added volunteer - category offer relations to models

--- app/models/group_offer_category.rb
+++ app/models/group_offer_category.rb
@@ -1,7 +1,10 @@
 class GroupOfferCategory < ApplicationRecord
   has_many :group_offers
+  has_and_belongs_to_many :volunteers
 
   CATEGORY_STATES = [:active, :inactive].freeze
+
+  scope :active, -> { where(category_state: :active) }
 
   def to_s
     category_name
