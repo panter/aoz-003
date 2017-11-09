16673260ee890fb14afa5ea899f2b0f8b007a10a
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-11-09 16:57:03 +0100

    add description to group offer categories

--- db/schema.rb
+++ db/schema.rb
@@ -209,6 +209,7 @@ ActiveRecord::Schema.define(version: 20171116140737) do
     t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["deleted_at"], name: "index_group_offer_categories_on_deleted_at"
   end
 
