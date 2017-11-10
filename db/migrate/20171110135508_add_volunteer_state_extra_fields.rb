c43a60589e11dff1c81e92c2be5eb0930b151801
Kaspar Vollenweider <vok@panter.ch>
2017-11-10 16:52:28 +0100

    add another activness_might_end field

--- db/migrate/20171110135508_add_volunteer_state_extra_fields.rb
+++ db/migrate/20171110135508_add_volunteer_state_extra_fields.rb
@@ -2,6 +2,7 @@ class AddVolunteerStateExtraFields < ActiveRecord::Migration[5.1]
   def change
     change_table :volunteers do |t|
       t.boolean :active, default: false
+      t.date :activeness_might_end
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -482,6 +482,7 @@ ActiveRecord::Schema.define(version: 20171116140737) do
     t.datetime "undecided_at"
     t.boolean "take_more_assignments", default: false
     t.boolean "active", default: false
+    t.date "activeness_might_end"
     t.index ["deleted_at"], name: "index_volunteers_on_deleted_at"
     t.index ["user_id"], name: "index_volunteers_on_user_id"
   end
