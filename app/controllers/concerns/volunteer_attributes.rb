51cbf30fb1294e5b03e3d5e5c0f32211a293c6fc
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-11-14 22:51:00 +0100

    remove deprecated attributes on volunteer

--- app/controllers/concerns/volunteer_attributes.rb
+++ app/controllers/concerns/volunteer_attributes.rb
@@ -6,10 +6,7 @@ module VolunteerAttributes
       [
         :birth_year, :salutation, :avatar, :nationality, :additional_nationality,
         :profession, :education, :motivation, :experience, :expectations, :strengths,
-        :skills, :interests, :man, :woman, :family, :kid, :sport, :creative,
-        :music, :culture, :training, :german_course, :teenagers, :children,
-        :working_percent, :volunteer_experience_desc, :dancing, :health, :cooking,
-        :excursions, :women, :unaccompanied, :zurich, :other_offer, :other_offer_desc,
+        :skills, :interests, :man, :woman, :family, :kid, :unaccompanied, :other_offer_desc,
         :trial_period, :doc_sent, :bank_account, :evaluation, :own_kids, :rejection_type,
         :rejection_text, :intro_course, :flexible, :morning, :afternoon, :evening,
         :workday, :weekend, :detailed_description, contact_attributes,
