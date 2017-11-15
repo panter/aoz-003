b592549c08c16d6c8fe08d83c1ea20c4beaac800
Kaspar Vollenweider <vok@panter.ch>
2017-11-15 10:39:21 +0100

    remove real email from imported volunteer but keep in Import table
    - import non valid unique email to prevent accidental email delivery
    - keep real email for the record within the Import model

--- lib/access_import/volunteer_transform.rb
+++ lib/access_import/volunteer_transform.rb
@@ -10,15 +10,16 @@ class VolunteerTransform < Transformer
   #
   def prepare_attributes(personen_rolle)
     haupt_person = @haupt_person.find(personen_rolle[:fk_Hauptperson])
+    original_email = haupt_person[:email]
     {
       salutation: haupt_person[:salutation],
       birth_year: haupt_person[:d_Geburtsdatum],
       nationality: haupt_person[:nationality],
       language_skills_attributes: language_skills_attributes(haupt_person[:sprachen]),
-      contact_attributes: contact_attributes(haupt_person),
+      contact_attributes: contact_attributes(haupt_person.merge(email: "importiert#{Time.zone.now.to_f}@example.com")),
       import_attributes: access_import(
         :tbl_Personenrollen, personen_rolle[:pk_PersonenRolle], personen_rolle: personen_rolle,
-        haupt_person: haupt_person
+        haupt_person: haupt_person.merge(email: original_email)
       )
     }
   end
