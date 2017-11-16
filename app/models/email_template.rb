3f683ebd3ba553f66a9340b40d828cdaa03965af
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-11-16 18:12:27 +0100

    one active per kind

--- app/models/email_template.rb
+++ app/models/email_template.rb
@@ -1,11 +1,17 @@
 class EmailTemplate < ApplicationRecord
+  before_save :ensure_exactly_one_active_per_kind
+
   enum kind: { Anmeldung: 0, Probezeit: 1, Tandem: 2, Groupenangebot: 3 }
   validates :kind, presence: true
 
-  scope :default_order, -> { order(created_at: :desc) }
-  scope :active_mail, -> { find_by(active: true) }
+  default_scope { order(created_at: :desc) }
 
   def self.kind_collection
     kinds.keys.map(&:to_sym)
+  end
+
+  def ensure_exactly_one_active_per_kind
+    return unless active && changed.include?('active')
+    EmailTemplate.where(kind: kind, active: true).update(active: false)
   end
 end
