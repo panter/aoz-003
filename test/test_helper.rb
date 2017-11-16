ba114df72913dc866ba07600a2f2320ec4393cfe
Kaspar Vollenweider <vok@panter.ch>
2017-11-16 23:45:07 +0100

    add test for users xlsx export

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -28,4 +28,22 @@ class ActiveSupport::TestCase
     DatabaseCleaner.clean
     super
   end
+
+  def get_xls_from_response(url)
+    get url
+    excel_file = Tempfile.new
+    excel_file.write(response.body)
+    excel_file.close
+    Roo::Spreadsheet.open(excel_file.path, extension: 'xlsx')
+  end
+
+  def assert_xls_cols_equal(wb, row, offset, *columns)
+    columns.each_with_index do |column, index|
+      assert_equal column, wb.cell(row, index + 1 + offset)
+    end
+  end
+
+  def assert_xls_row_empty(wb, row, cols = 8)
+    (1..cols).to_a.each { |column| assert_nil wb.cell(row, column) }
+  end
 end
