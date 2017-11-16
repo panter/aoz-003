374017b63e181de0cacec0b841af42bdda292d5e
Kaspar Vollenweider <vok@panter.ch>
2017-11-16 13:02:40 +0100

    replace system active filter test with integration test
    - because travis ci kept on failing with the system test

--- test/factories/assignments.rb
+++ test/factories/assignments.rb
@@ -16,11 +16,23 @@ FactoryBot.define do
       period_end { 1.year.ago.to_date.end_of_year - 2 }
     end
 
+    trait :active do
+      period_start { 10.days.ago }
+      period_end nil
+    end
+
+    trait :inactive do
+      period_start { 10.days.ago }
+      period_end { 5.days.ago }
+    end
+
     trait :blank_period do
       period_start { nil }
       period_end { nil }
     end
 
     factory :assignment_blank_period, traits: [:blank_period]
+    factory :assignment_active, traits: [:active]
+    factory :assignment_inactive, traits: [:inactive]
   end
 end
