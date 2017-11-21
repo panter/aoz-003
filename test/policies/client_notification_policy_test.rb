68e7957e009314e3d74d33fa32c373ed413fa1ae
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-11-21 14:49:57 +0100

    fix tests

--- test/policies/client_notification_policy_test.rb
+++ test/policies/client_notification_policy_test.rb
@@ -1,25 +1,25 @@
 require 'test_helper'
 
 class ClientNotificationPolicyTest < PolicyAssertions::Test
-  attr_reader :superadmin, :social_worker, :client_notificiation
+  attr_reader :superadmin, :social_worker, :client_notification
 
   def setup
     @superadmin = create :user, role: 'superadmin'
     @social_worker = create :user, role: 'social_worker'
-    @client_notificiation = create :client_notificiation, user: @superadmin
+    @client_notification = create :client_notification, user: @superadmin
   end
 
   test 'superadmin gets actions granted' do
     assert_permit(
-      superadmin, client_notificiation,
-      'index?', 'clients_index', 'edit?', 'create?', 'update?', 'destroy?'
+      superadmin, client_notification,
+      'index?', 'clients_index?', 'edit?', 'create?', 'update?', 'destroy?'
     )
   end
 
   test 'socialworker get no access' do
     refute_permit(
-      social_worker, client_notificiation,
-      'index?', 'clients_index', 'edit?', 'create?', 'update?', 'destroy?'
+      social_worker, client_notification,
+      'index?', 'clients_index?', 'edit?', 'create?', 'update?', 'destroy?'
     )
   end
 end
