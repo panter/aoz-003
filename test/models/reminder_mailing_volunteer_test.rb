eb53882f08bcf22259847f8d49492b00353b9a94
Kaspar Vollenweider <vok@panter.ch>
2017-11-22 17:16:04 +0100

    testing template variable substitution

--- test/models/reminder_mailing_volunteer_test.rb
+++ test/models/reminder_mailing_volunteer_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ReminderMailingVolunteerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @superadmin = create :user
+    @volunteer = create :volunteer_with_user
+    @assignment_probation = create :assignment, period_start: 7.weeks.ago, volunteer: @volunteer
+  end
+
+  test 'template variables are substituted' do
+    reminder_mailing = ReminderMailing.new(kind: :probation_period, creator: @superadmin,
+      subject: 'hallo %{Anrede} %{Name}', reminder_mailing_volunteers: [@assignment_probation],
+      body: 'hallo %{Anrede} %{Name} %{EinsatzStart} %{Begleitung}')
+    reminder_mailing.save
+    mailing_volunteer = reminder_mailing.reminder_mailing_volunteers.first
+    assert mailing_volunteer.body.include? "#{@volunteer.contact.first_name} #{@volunteer.contact.last_name}"
+    assert mailing_volunteer.body.include? I18n.t("salutation.#{@volunteer.salutation}")
+    assert mailing_volunteer.body.include? I18n.l(@assignment_probation.period_start)
+    assert mailing_volunteer.body.include? @assignment_probation.to_label
+  end
 end
