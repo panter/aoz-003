79cfce2dece6f761c383b5c5b3b71d6eee6cd731
Kaspar Vollenweider <vok@panter.ch>
2017-11-22 17:18:03 +0100

    fix send replaced with public_send

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -5,11 +5,11 @@ class ApplicationRecord < ActiveRecord::Base
   # translate enum fields value
   def t_enum(enum_field)
     I18n.t("activerecord.attributes.#{model_name.i18n_key}."\
-      "#{enum_field.to_s.pluralize}.#{send(enum_field)}")
+      "#{enum_field.to_s.pluralize}.#{public_send(enum_field)}")
   end
 
   # simple form translation 'simple_form.options.model_name.enum_field.key'
   def self.enum_collection(enum_field)
-    send(enum_field.to_s.pluralize).keys.map(&:to_sym)
+    public_send(enum_field.to_s.pluralize).keys.map(&:to_sym)
   end
 end
