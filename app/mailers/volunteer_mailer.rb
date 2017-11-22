e1d54b8a9729629a0428b610acb0705793c2ac1f
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-11-22 18:15:51 +0100

    replace variables in signup email if used

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -3,6 +3,12 @@ class VolunteerMailer < ApplicationMailer
   def welcome_email(volunteer, signup_email)
     @volunteer = volunteer
     @signup_email = signup_email
-    mail(to: volunteer.contact.primary_email, subject: @signup_email.subject)
+    subject = signup_email.subject
+    subject.gsub! '%<anrede>', t("salutation.#{volunteer.salutation}")
+    subject.gsub! '%<name>', "#{volunteer.contact.first_name} #{volunteer.contact.last_name}"
+    body = signup_email.body
+    body.gsub! '%<anrede>', t("salutation.#{volunteer.salutation}")
+    body.gsub! '%<name>', "#{volunteer.contact.first_name} #{volunteer.contact.last_name}"
+    mail(to: @volunteer.contact.primary_email, subject: @signup_email.subject)
   end
 end
