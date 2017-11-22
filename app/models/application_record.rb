32ac88eaf8fac950604c36b1dcceed22ee07f38e
Kaspar Vollenweider <vok@panter.ch>
2017-11-22 12:04:18 +0100

    enum translation methods on application record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,15 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   acts_as_paranoid
+
+  # translate enum fields value
+  def t_enum(enum_field)
+    I18n.t("activerecord.attributes.#{model_name.i18n_key}."\
+      "#{enum_field.to_s.pluralize}.#{send(enum_field)}")
+  end
+
+  # simple form translation 'simple_form.options.model_name.enum_field.key'
+  def self.enum_collection(enum_field)
+    send(enum_field.to_s.pluralize).keys.map(&:to_sym)
+  end
 end
