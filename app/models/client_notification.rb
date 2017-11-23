ad98a9401cdd20f909b2830d00f481b45353586d
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-11-23 12:53:47 +0100

    replace active scope with class method

--- app/models/client_notification.rb
+++ app/models/client_notification.rb
@@ -3,12 +3,15 @@ class ClientNotification < ApplicationRecord
   before_save :ensure_exactly_one_active
 
   default_scope { order(created_at: :desc) }
-  scope :active, -> { find_by(active: true) }
 
   belongs_to :user, -> { with_deleted }
 
+  def self.active
+    where(active: true)
+  end
+
   def ensure_exactly_one_active
     return unless active && changed.include?('active')
-    ClientNotification.active.update(active: false)
+    ClientNotification.update(active: false)
   end
 end
