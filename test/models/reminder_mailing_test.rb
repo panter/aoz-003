4cfa827c360ee2ccfafba64d9e7bff1e52d4aa00
Kaspar Vollenweider <vok@panter.ch>
2017-11-24 22:24:33 +0100

    fixing the started tests

--- test/models/reminder_mailing_test.rb
+++ test/models/reminder_mailing_test.rb
@@ -7,7 +7,7 @@ class ReminderMailingTest < ActiveSupport::TestCase
     @assignment_probation = create :assignment, period_start: 7.weeks.ago, volunteer: @volunteer
     @group_offer = create :group_offer
     @group_assignment_probation = GroupAssignment.create(volunteer: @volunteer,
-      group_offer: @group_offer, period_start: 7.weeks.ago)
+      group_offer: @group_offer, period_start: 6.weeks.ago.to_date + 2)
   end
 
   test 'reminder_mailing_has_right_relations' do
@@ -15,6 +15,10 @@ class ReminderMailingTest < ActiveSupport::TestCase
       subject: 'aaa', creator: @superadmin, reminder_mailing_volunteers: [
         @assignment_probation, @group_assignment_probation
       ])
+    # pretend to set form select value
+    reminder_mailing.reminder_mailing_volunteers.each do |rmv|
+      rmv.selected = '1'
+    end
     reminder_mailing.save
     assert reminder_mailing.users.include? @volunteer.user
     assert reminder_mailing.volunteers.include? @volunteer
