feafcc29a66a68437ae9ff0fcf3c60134d169b8a
Kaspar Vollenweider <vok@panter.ch>
2017-11-24 23:19:22 +0100

    fixed factory so it validates

--- test/factories/reminder_mailings.rb
+++ test/factories/reminder_mailings.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :reminder_mailing do
-    association :creator, factory: :user
+    association :creator, factory: :user_fake_email
     body { Faker::Lorem.paragraph }
     subject { Faker::Lorem.sentence }
+    kind 'probation_period'
+    after(:build) do |reminder_mailing|
+      volunteer = create(:volunteer_with_user)
+      assignment = create(:assignment, volunteer: volunteer, period_start: 6.weeks.ago.to_date + 5)
+      reminder_mailing.reminder_mailing_volunteers << ReminderMailingVolunteer.new(volunteer: volunteer, reminder_mailable: assignment, selected: '1')
+    end
   end
 end
