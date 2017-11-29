d4e020bc0b695054d5def48b2dab6fd7088fd026
Kaspar Vollenweider <vok@panter.ch>
2017-11-29 12:15:52 +0100

    add order by created_at and updted_at order scopes to applicaiton record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,12 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   acts_as_paranoid
 
+  # order scopes
+  scope :created_desc, (-> { order('created_at desc') })
+  scope :created_asc, (-> { order('created_at asc') })
+  scope :updated_desc, (-> { order('updated_at desc') })
+  scope :updated_asc, (-> { order('updated_at asc') })
+
   # translate enum fields value
   def t_enum(enum_field)
     I18n.t("activerecord.attributes.#{model_name.i18n_key}."\
