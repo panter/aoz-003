deda0daec78c796e22fa273661f57692bfb77ff8
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-11-29 18:19:22 +0100

    add since_last_submitted for feedbacks

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -43,6 +43,11 @@ class FeedbacksController < ApplicationController
     redirect_back(fallback_location: url_for(@feedbackable))
   end
 
+  def since_last_submitted
+    authorize Feedback
+    @since_last_submitted = Feedback.since_last_submitted
+  end
+
   private
 
   def set_feedbackable
