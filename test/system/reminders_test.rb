ae734c89564a999f729a97f4787075d6b143d557
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-11-29 17:34:00 +0100

    replace confirmation with submitted at for assignments

---
+++ test/system/reminders_test.rb
@@ -0,0 +1,36 @@
+require 'application_system_test_case'
+
+class RemindersTest < ApplicationSystemTestCase
+  def setup
+    @user = create :user
+    @now = Time.zone.today
+    @volunteer = create :volunteer
+    @trial_volunteer = create :volunteer
+    @assignment = create :assignment, period_start: 7.months.ago, period_end: nil,
+      volunteer: @volunteer, creator: @user
+    @trial_assignment = create :assignment, period_start: 7.weeks.ago, period_end: nil,
+      volunteer: @trial_volunteer, creator: @user
+    Reminder.conditionally_create_reminders
+    login_as @user
+    visit reminders_path
+  end
+
+  test 'when reminder is sent, send button becomes sent date' do
+    assert page.has_text? @now, count: 1
+    click_button 'Send'
+    assert page.has_text? @now, count: 2
+  end
+
+  test 'can delete reminder' do
+    assert page.has_text? @volunteer.contact.full_name
+    click_button 'Quittieren'
+    refute page.has_text? @volunteer.contact.full_name
+  end
+
+  test 'can delete trial period reminder' do
+    visit trial_end_reminders_path
+    assert page.has_text? @trial_volunteer.contact.full_name
+    click_link 'Quittieren'
+    refute page.has_text? @trial_volunteer.contact.full_name
+  end
+end
