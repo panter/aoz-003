3ea45b8dc51b12293aefa19f432b305f23917635
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-11-30 16:25:04 +0100

    confirmation of hours and feedbacks

--- db/migrate/20171129162936_add_submitted_at_to_assignment_and_group_assignment.rb
+++ db/migrate/20171129162936_add_submitted_at_to_assignment_and_group_assignment.rb
@@ -1,7 +1,7 @@
 class AddSubmittedAtToAssignmentAndGroupAssignment < ActiveRecord::Migration[5.1]
   def change
     remove_column :assignments, :confirmation, :boolean
-    add_column :assignments, :submitted_at, :date
-    add_column :group_assignments, :submitted_at, :date
+    add_column :assignments, :submitted_at, :datetime
+    add_column :group_assignments, :submitted_at, :datetime
   end
 end
