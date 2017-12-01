531f040e63db31fb947a243b6b6688c9b1d12a76
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-12-01 11:45:57 +0100

    fix tests

--- test/policies/group_assignments_policy_test.rb
+++ test/policies/group_assignments_policy_test.rb
@@ -6,20 +6,32 @@ class GroupAssignmentPolicyTest < PolicyAssertions::Test
     @superadmin = create :user
     @other_volunteer = create :volunteer_with_user
     @department_manager = create :department_manager
-    @group_offer = create :group_offer, department: @department_manager.department.first
-    @group_assignment = GroupAssignment.create(volunteer: @volunteer, group_offer: @group_offer)
-    @other_group_assignment = GroupAssignment.create(volunteer: @other_volunteer, group_offer: @other_group_offer)
+    @social_worker = create :social_worker
+    @group_assignment = GroupAssignment.create(volunteer: @volunteer,
+      group_offer: (create :group_offer))
+    @other_group_assignment = GroupAssignment.create(volunteer: @other_volunteer,
+      group_offer: (create :group_offer))
   end
 
-  test 'superadmin can use all actions' do
-    assert_permit(
-      @superadmin, @group_assignment, @other_group_assignment,
-      'show?', 'last_submitted_hours_and_feedbacks?'
-      )
+  test 'superadmin_can_use_all_actions' do
+    assert_permit(@superadmin, GroupAssignment, 'update?', 'show?',
+      'last_submitted_hours_and_feedbacks?')
   end
 
-  test 'volunteer has limited access' do
-    assert_permit(@volunteer, @group_assignment, 'last_submitted_hours_and_feedbacks?')
-    refute_permit(@volunteer, @other_group_assignment, 'last_submitted_hours_and_feedbacks?')
+  test 'department_manager_can_use_all_actions' do
+    assert_permit(@department_manager, GroupAssignment, 'update?', 'show?',
+      'last_submitted_hours_and_feedbacks?')
+  end
+
+  test 'social_worker_has_no_access' do
+    refute_permit(@social_worker, GroupAssignment, 'update?', 'show?',
+      'last_submitted_hours_and_feedbacks?')
+  end
+
+  test 'volunteer_has_limited_access' do
+    assert_permit(@volunteer.user, @group_assignment, 'update?', 'show?',
+      'last_submitted_hours_and_feedbacks?')
+    refute_permit(@volunteer.user, @other_group_assignment, 'update?', 'show?',
+      'last_submitted_hours_and_feedbacks?')
   end
 end
