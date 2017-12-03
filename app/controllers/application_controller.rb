e579c4c531f9a7f5a7231350bd682fbcdf695d9e
Kaspar Vollenweider <vok@panter.ch>
2017-12-03 12:41:06 +0100

    add redirect to TrialFeedbacks link from mail, but not yet logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
+  before_action :store_user_location!, if: :storable_location?
+
   protect_from_forgery with: :exception
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -13,6 +15,8 @@ class ApplicationController < ActionController::Base
   delegate :volunteer?, to: :current_user
 
   def after_sign_in_path_for(current_user)
+    hit_location = stored_location_for(current_user)
+    return hit_location if hit_location[/\/volunteers\/[0-9]+\/(assignments|group_assignments)\/[0-9]+\//].present?
     return volunteer_path(current_user.volunteer.id) if volunteer?
     return new_profile_path if current_user.profile.blank?
     if policy(Department).manager_with_department?
@@ -36,6 +40,14 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
 
   def user_not_authorized
     flash[:alert] = t('not_authorized')
