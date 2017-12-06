621f2a0240932ffd80d1b3ac74dc045f980c77f2
Kaspar Vollenweider <vok@panter.ch>
2017-12-06 14:49:42 +0100

    half year mailings are sent

--- test/policies/reminder_mailing_policy_test.rb
+++ test/policies/reminder_mailing_policy_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class ReminderMailingPolicyTest < PolicyAssertions::Test
   test 'only superadmin is permitted to all actions' do
-    actions = ['index?', 'new_half_year?', 'new_probation_period?', 'show?', 'initiate_mailing?',
-               'create?', 'edit?', 'update?', 'destroy?']
+    actions = ['index?', 'new_half_year?', 'new_probation_period?', 'show?', 'send_probation?',
+               'send_half_year?', 'create?', 'edit?', 'update?', 'destroy?']
     assert_permit(create(:user), ReminderMailing, *actions)
 
     refute_permit(create(:social_worker), ReminderMailing, *actions)
