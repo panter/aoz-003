51d1706d266af7feea177d2e470180249a230cf5
Kaspar Vollenweider <vok@panter.ch>
2017-12-06 17:27:38 +0100

    relation imposibble fixes

--- test/factories/group_offers.rb
+++ test/factories/group_offers.rb
@@ -1,8 +1,15 @@
 FactoryBot.define do
   factory :group_offer do
-    association :creator, factory: :user
-    group_offer_category
+    association :creator, factory: :user_fake_email
     title { Faker::Lorem.sentence }
     necessary_volunteers 5
+
+    after(:build) do |group_offer|
+      if GroupOfferCategory.any?
+        group_offer.group_offer_category ||= GroupOfferCategory.all.sample
+      else
+        group_offer.group_offer_category = create(:group_offer_category)
+      end
+    end
   end
 end
--- test/factories/trial_feedbacks.rb
+++ test/factories/trial_feedbacks.rb
@@ -1,8 +1,11 @@
 FactoryBot.define do
   factory :trial_feedback do
     association :trial_feedbackable, factory: :assignment
-    volunteer
-    association :author, factory: :user
     body { Faker::Lorem.paragraph }
+
+    after(:build) do |trial_feedback|
+      trial_feedback.volunteer ||= trial_feedback.trial_feedbackable.volunteer
+      trial_feedback.author ||= trial_feedback.volunteer.user || create(:user_fake_email)
+    end
   end
 end
