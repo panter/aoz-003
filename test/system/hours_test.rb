ab03be1147332b1dca512c5d4b91baa13004b03d
Kaspar Vollenweider <vok@panter.ch>
2017-12-07 16:06:00 +0100

    fix hours test not working correctly

--- test/system/hours_test.rb
+++ test/system/hours_test.rb
@@ -3,14 +3,19 @@ require 'application_system_test_case'
 class HoursTest < ApplicationSystemTestCase
   def setup
     @user = create :user
-    @user_volunteer1 = create :user, role: 'volunteer', email: 'volunteer1@example.com'
-    @volunteer1 = @user_volunteer1.volunteer = create :volunteer
+    @volunteer1 = create :volunteer_with_user
+    @volunteer1.user.update(email: 'volunteer1@example.com')
+    @volunteer1.contact.update(primary_email: 'volunteer1@example.com')
+    @user_volunteer1 = @volunteer1.user
     @client1 = create :client
     @client1.contact.first_name = @client1.contact.last_name = 'Client1'
-    @assignment1 = create :assignment, volunteer: @volunteer1, client: @client1
+    @assignment1 = create :assignment, volunteer: @volunteer1, client: @client1,
+      period_start: 7.weeks.ago
     @group_offer_category = create :group_offer_category
-    @group_offer1 = create :group_offer, volunteers: [@volunteer1],
-      group_offer_category: @group_offer_category, title: 'GroupOfferNumberOne'
+    @group_offer1 = create :group_offer, title: 'GroupOfferNumberOne',
+      group_offer_category: @group_offer_category
+    create :group_assignment, group_offer: @group_offer1, volunteer: @volunteer1,
+      period_start: 7.weeks.ago
     login_as @user_volunteer1
     visit root_url
     click_link 'volunteer1@example.com'
@@ -71,19 +76,22 @@ class HoursTest < ApplicationSystemTestCase
     refute page.has_text? client2.contact.full_name
   end
 
-  test 'volunteer can see only her group_offers' do
-    @user_volunteer2 = create :user, role: 'volunteer', email: 'volunteer2@example.com'
-    @volunteer2 = @user_volunteer2.volunteer = create :volunteer
-    @group_offer2 = create :group_offer, volunteers: [@volunteer2],
-      group_offer_category: @group_offer_category, title: 'GroupOfferNumberTwo'
+  test 'volunteer_can_see_only_her_group_offers' do
+    volunteer2 = create :volunteer_with_user
+    volunteer2.user.update(email: 'volunteer2@example.com')
+    volunteer2.contact.update(primary_email: 'volunteer2@example.com')
+    group_offer2 = create :group_offer, group_offer_category: @group_offer_category,
+      title: 'GroupOfferNumberTwo'
+    create :group_assignment, group_offer: group_offer2, volunteer: volunteer2,
+      period_start: 7.weeks.ago
 
-    @group_offer_hour1 = create :hour, hourable: @group_offer1, volunteer: @volunteer1
-    @group_offer_hour2 = create :hour, hourable: @group_offer2, volunteer: @volunteer2
+    create :hour, hourable: @group_offer1, volunteer: @volunteer1
+    create :hour, hourable: group_offer2, volunteer: volunteer2
     click_link 'Hour reports'
     assert page.has_text? @group_offer1.to_label
-    refute page.has_text? @group_offer2.to_label
-    visit volunteer_hours_path(@volunteer2)
+    refute page.has_text? group_offer2.to_label
+    visit volunteer_hours_path(volunteer2)
     assert page.has_text? 'You are not authorized to perform this action.'
-    refute page.has_text? @group_offer2.to_label
+    refute page.has_text? group_offer2.to_label
   end
 end
