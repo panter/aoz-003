c67217eca3f9f15fcb8cd389107ecfb811f4881a
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-12-07 15:39:49 +0100

    replace update redirect

--- app/controllers/group_assignments_controller.rb
+++ app/controllers/group_assignments_controller.rb
@@ -1,15 +1,6 @@
 class GroupAssignmentsController < ApplicationController
   before_action :set_group_assignment
 
-  def update
-    if @group_assignment.update(group_assignment_params)
-      redirect_to last_submitted_hours_and_feedbacks_group_assignment_path,
-        notice: 'Die Stunden und Feedbacks wurden erfolgreich bestätigt.'
-    else
-      redirect_to last_submitted_hours_and_feedbacks_group_assignment_path
-    end
-  end
-
   def show
     respond_to do |format|
       format.pdf do
@@ -25,14 +16,16 @@ class GroupAssignmentsController < ApplicationController
     @last_submitted_feedbacks = @group_assignment.feedbacks_since_last_submitted
   end
 
+  def update_submitted_at
+    @group_assignment.update(submitted_at: Time.zone.now)
+    redirect_to last_submitted_hours_and_feedbacks_group_assignment_path,
+      notice: 'Die Stunden und Feedbacks wurden erfolgreich bestätigt.'
+  end
+
   private
 
   def set_group_assignment
     @group_assignment = GroupAssignment.find(params[:id])
     authorize @group_assignment
-  end
-
-  def group_assignment_params
-    params.require(:group_assignment).permit(:submitted_at)
   end
 end
--- test/policies/assignment_policy_test.rb
+++ test/policies/assignment_policy_test.rb
@@ -13,29 +13,29 @@ class AssignmentPolicyTest < PolicyAssertions::Test
   test 'superadmin_can_use_all_actions' do
     assert_permit(@superadmin, Assignment, 'new?', 'create?', 'edit?', 'update?', 'show?', 'index?',
       'destroy?', 'find_volunteer?', 'find_client?', 'supervisor?',
-      'last_submitted_hours_and_feedbacks?')
+      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
   end
 
   test 'department_manager_can_use_all_actions_but_destroy' do
     assert_permit(@department_manager, Assignment, 'new?', 'create?', 'edit?', 'update?', 'show?',
       'index?', 'find_volunteer?', 'find_client?', 'supervisor?',
-      'last_submitted_hours_and_feedbacks?')
+      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
     refute_permit(@department_manager, Assignment, 'destroy?')
   end
 
   test 'social_worker_has_no_access' do
     refute_permit(@social_worker, Assignment, 'new?', 'update?', 'edit?', 'show?', 'index?',
       'destroy?', 'find_volunteer?', 'find_client?', 'supervisor?',
-      'last_submitted_hours_and_feedbacks?')
+      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
   end
 
   test 'volunteer_has_limited_access' do
     assert_permit(@volunteer.user, @assignment, 'edit?', 'update?', 'show?',
-      'last_submitted_hours_and_feedbacks?')
+      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
     refute_permit(@volunteer.user, Assignment, 'new?', 'create?', 'index?',
       'destroy?', 'supervisor?', 'find_client?', 'find_volunteer?')
     refute_permit(@volunteer.user, @other_assignment, 'new?', 'create?', 'edit?', 'update?',
       'show?', 'index?', 'destroy?', 'supervisor?', 'find_client?', 'find_volunteer?',
-      'last_submitted_hours_and_feedbacks?')
+      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
   end
 end
