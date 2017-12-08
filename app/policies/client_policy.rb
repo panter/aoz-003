541d83efce60012cb049f9da2b8b64931ddfbf19
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2017-12-08 22:49:57 +0100

    client index and form changes

--- app/policies/client_policy.rb
+++ app/policies/client_policy.rb
@@ -8,20 +8,16 @@ class ClientPolicy < ApplicationPolicy
   end
 
   # controller action policies
-  alias_method :index?,  :user_managing_volunteer?
-  alias_method :search?, :user_managing_volunteer?
-  alias_method :new?,    :user_managing_volunteer?
-  alias_method :create?, :user_managing_volunteer?
-
-  alias_method :show?,   :user_managing_volunteer?
-  alias_method :edit?,   :superadmin_or_social_workers_record?
-  alias_method :update?, :superadmin_or_social_workers_record?
-
-  alias_method :destroy?,           :superadmin?
-  alias_method :need_accompanying?, :superadmin_or_department_manager?
-  alias_method :with_assignment?,   :superadmin_or_department_manager?
+  alias_method :index?,    :user_managing_volunteer?
+  alias_method :search?,   :user_managing_volunteer?
+  alias_method :new?,      :user_managing_volunteer?
+  alias_method :create?,   :user_managing_volunteer?
+  alias_method :show?,     :user_managing_volunteer?
+  alias_method :edit?,     :superadmin_or_social_workers_record?
+  alias_method :update?,   :superadmin_or_social_workers_record?
+  alias_method :destroy?,  :superadmin?
 
   # suplementary policies
-  alias_method :comments?,         :superadmin?
-  alias_method :state?,            :superadmin?
+  alias_method :comments?, :superadmin?
+  alias_method :state?,    :superadmin?
 end
