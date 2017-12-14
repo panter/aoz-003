bcc1648747d0fba3cfc047258f2549f4131dc925
Tugce Nur Tas <tas.tugcenur@gmail.com>
2017-12-14 11:56:50 +0100

    add volunteer email to assignments excel and test

--- test/integration/assignments_xlsx_export_test.rb
+++ test/integration/assignments_xlsx_export_test.rb
@@ -16,22 +16,23 @@ class AssignmentsXlsxExportTest < ActionDispatch::IntegrationTest
     assignment = create :assignment, period_start: 3.months.ago, period_end: 2.days.ago
     wb = get_xls_from_response(assignments_url(format: :xlsx))
 
-    assert_xls_cols_equal(wb, 1, 0, 'id', 'Freiwillige/r', 'Klient/in', 'Start date', 'End date',
-      'State', 'Created at', 'Updated at')
+    assert_xls_cols_equal(wb, 1, 0, 'id', 'Freiwillige/r', 'Freiwillige/r Mailadresse',
+      'Klient/in', 'Start date', 'End date', 'State', 'Created at', 'Updated at')
 
-    assert_xls_cols_equal(wb, 2, 0, assignment.id, assignment.volunteer.contact.full_name,
-      assignment.client.contact.full_name, assignment.period_start, assignment.period_end,
+    assert_xls_cols_equal(wb, 2, 0, assignment.id,
+      assignment.volunteer.contact.full_name, assignment.volunteer.contact.primary_email,
+      assignment.period_start, assignment.period_end, assignment.client.contact.full_name,
       I18n.t("simple_form.options.assignment.state.#{assignment.state}"))
-    assert_equal assignment.created_at.to_date, wb.cell(2, 7).to_date
-    assert_equal assignment.updated_at.to_date, wb.cell(2, 8).to_date
+    assert_equal assignment.created_at.to_date, wb.cell(2, 8).to_date
+    assert_equal assignment.updated_at.to_date, wb.cell(2, 9).to_date
 
     assert_equal(
       assignment.updated_at.localtime.to_time.to_s.slice(0..-9),
-      wb.cell(2, 8).to_time.to_s.slice(0..-9)
+      wb.cell(2, 9).to_time.to_s.slice(0..-9)
     )
     assert_equal(
       assignment.created_at.localtime.to_time.to_s.slice(0..-9),
-      wb.cell(2, 7).to_time.to_s.slice(0..-9)
+      wb.cell(2, 8).to_time.to_s.slice(0..-9)
     )
   end
 
