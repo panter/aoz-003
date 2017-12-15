7bd94c019be64e81a6319576c0687fba6072b8b4
Kaspar Vollenweider <vok@panter.ch>
2017-12-15 21:09:11 +0100

    remove comment and shorten too long line

--- test/factories/reminder_mailings.rb
+++ test/factories/reminder_mailings.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     association :creator, factory: :user_fake_email
     body { Faker::Lorem.paragraph }
     subject { Faker::Lorem.sentence }
-    # kind 'trial_period'
 
     trait :half_year do
       kind { 0 }
@@ -20,7 +19,8 @@ FactoryBot.define do
     after(:build) do |reminder_mailing|
       volunteer = create(:volunteer_with_user)
       assignment = create(:assignment, volunteer: volunteer, period_start: 6.weeks.ago.to_date + 5)
-      reminder_mailing.reminder_mailing_volunteers << ReminderMailingVolunteer.new(volunteer: volunteer, reminder_mailable: assignment, picked: true)
+      reminder_mailing.reminder_mailing_volunteers << ReminderMailingVolunteer.new(
+        volunteer: volunteer, reminder_mailable: assignment, picked: true)
     end
   end
 end
