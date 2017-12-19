0ade05627544355231c142fca36eddaebebc49ef
Kaspar Vollenweider <vok@panter.ch>
2017-12-19 19:01:40 +0100

    effectiveley done categories are in there, and others are manually selectable

--- app/models/group_assignment_log.rb
+++ app/models/group_assignment_log.rb
@@ -3,4 +3,5 @@ class GroupAssignmentLog < ApplicationRecord
   belongs_to :volunteer
   belongs_to :group_assignment, optional: true
   has_many :reminder_mailing_volunteers, as: :reminder_mailable, dependent: :destroy
+  has_one :group_offer_category, through: :group_offer
 end
