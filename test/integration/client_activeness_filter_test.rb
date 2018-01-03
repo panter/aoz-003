29071bb806cbad92a0683fd7921d18230bfdafc8
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2018-01-03 12:33:27 +0100

    test new client filters

---
+++ test/integration/client_activeness_filter_test.rb
@@ -0,0 +1,60 @@
+require 'test_helper'
+
+class ClientActivenessFilterTest < ActionDispatch::IntegrationTest
+  def setup
+    @superadmin = create :user
+    @client_accepted = create :client, acceptance: 'accepted'
+    create :assignment_active, client: @client_accepted
+    @client_rejected = create :client, acceptance: 'rejected'
+    create :assignment_active, client: @client_rejected
+    @client_resigned = create :client, acceptance: 'resigned'
+    create :assignment_active, client: @client_resigned
+    login_as @superadmin
+  end
+
+  test 'client_tandem_active_filter_returns_accepted_active_client' do
+    get clients_path(q: { active_eq: 'true' })
+    assert response.body.include? @client_accepted.contact.full_name
+    refute response.body.include? @client_rejected.contact.full_name
+    refute response.body.include? @client_resigned.contact.full_name
+  end
+
+  test 'client_tandem_inactive_filter_returns_accepted_inactive_client' do
+    get clients_path(q: { active_eq: 'false' })
+    refute response.body.include? @client_accepted.contact.full_name
+    refute response.body.include? @client_rejected.contact.full_name
+    refute response.body.include? @client_resigned.contact.full_name
+  end
+
+  test 'client_acceptance_tandem_filters_work_together' do
+    get clients_path(q: { acceptance_eq: '0', active_eq: 'true' })
+    assert response.body.include? @client_accepted.contact.full_name
+    refute response.body.include? @client_rejected.contact.full_name
+    refute response.body.include? @client_resigned.contact.full_name
+
+    get clients_path(q: { acceptance_eq: '0', active_eq: 'false' })
+    refute response.body.include? @client_accepted.contact.full_name
+    refute response.body.include? @client_rejected.contact.full_name
+    refute response.body.include? @client_resigned.contact.full_name
+
+    get clients_path(q: { acceptance_eq: '1', active_eq: 'true' })
+    refute response.body.include? @client_accepted.contact.full_name
+    refute response.body.include? @client_rejected.contact.full_name
+    refute response.body.include? @client_resigned.contact.full_name
+
+    get clients_path(q: { acceptance_eq: '1', active_eq: 'false' })
+    refute response.body.include? @client_accepted.contact.full_name
+    refute response.body.include? @client_rejected.contact.full_name
+    refute response.body.include? @client_resigned.contact.full_name
+
+    get clients_path(q: { acceptance_eq: '2', active_eq: 'true' })
+    refute response.body.include? @client_accepted.contact.full_name
+    refute response.body.include? @client_rejected.contact.full_name
+    refute response.body.include? @client_resigned.contact.full_name
+
+    get clients_path(q: { acceptance_eq: '2', active_eq: 'false' })
+    refute response.body.include? @client_accepted.contact.full_name
+    refute response.body.include? @client_rejected.contact.full_name
+    refute response.body.include? @client_resigned.contact.full_name
+  end
+end
--- test/system/clients_filter_dropdowns_test.rb
+++ test/system/clients_filter_dropdowns_test.rb
@@ -4,9 +4,13 @@ class ClientsFilterDropdownsTest < ApplicationSystemTestCase
   def setup
     @user = create :user, role: 'superadmin'
     @accepted_mrs_same_age_old = create(:client, acceptance: 'accepted', salutation: 'mrs',
-      gender_request: 'same', age_request: 'age_old').contact.full_name
+      gender_request: 'same', age_request: 'age_old')
+    create :assignment_active, client: @accepted_mrs_same_age_old
+    @accepted_mrs_same_age_old = @accepted_mrs_same_age_old.contact.full_name
     @accepted_mr_no_matter_age_old = create(:client, acceptance: 'accepted', salutation: 'mr',
-      gender_request: 'no_matter', age_request: 'age_old').contact.full_name
+      gender_request: 'no_matter', age_request: 'age_old')
+    create :assignment_active, client: @accepted_mr_no_matter_age_old
+    @accepted_mr_no_matter_age_old = @accepted_mr_no_matter_age_old.contact.full_name
     @resigned_mrs_same_age_middle = create(:client, acceptance: 'resigned', salutation: 'mrs',
       gender_request: 'same', age_request: 'age_middle').contact.full_name
     @rejected_mr_no_matter_age_middle = create(:client, acceptance: 'rejected', salutation: 'mr',
@@ -61,6 +65,50 @@ class ClientsFilterDropdownsTest < ApplicationSystemTestCase
     within '.section-navigation' do
       click_link 'Salutation: Mr.'
       assert page.find('a.bg-success', text: 'Mr.').present?
+      click_link 'All'
+    end
+    visit current_url
+    within 'tbody' do
+      assert page.has_text? @accepted_mrs_same_age_old
+      assert page.has_text? @accepted_mr_no_matter_age_old
+      refute page.has_text? @resigned_mrs_same_age_middle
+      refute page.has_text? @rejected_mr_no_matter_age_middle
+    end
+    click_link 'Clear filters'
+    visit current_url
+    within 'tbody' do
+      assert page.has_text? @accepted_mrs_same_age_old
+      assert page.has_text? @accepted_mr_no_matter_age_old
+      assert page.has_text? @resigned_mrs_same_age_middle
+      assert page.has_text? @rejected_mr_no_matter_age_middle
+    end
+  end
+
+  test 'filter_acceptance_tandem_and_salutation_at_the_same_time' do
+    within '.section-navigation' do
+      click_link 'Acceptance'
+      click_link 'Angemeldet'
+    end
+    visit current_url
+    within '.section-navigation' do
+      click_link 'Tandem'
+      click_link 'Aktiv'
+    end
+    visit current_url
+    within '.section-navigation' do
+      click_link 'Salutation'
+      click_link 'Mrs.'
+    end
+    visit current_url
+    within 'tbody' do
+      assert page.has_text? @accepted_mrs_same_age_old
+      refute page.has_text? @accepted_mr_no_matter_age_old
+      refute page.has_text? @resigned_mrs_same_age_middle
+      refute page.has_text? @rejected_mr_no_matter_age_middle
+    end
+    within '.section-navigation' do
+      click_link 'Salutation: Mrs.'
+      assert page.find('a.bg-success', text: 'Mrs.').present?
       click_link 'All'
     end
     visit current_url
