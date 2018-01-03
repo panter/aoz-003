935aa932ec1561ffbf998adccacda25ad9223287
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2018-01-03 10:57:31 +0100

    active inactive only with scopes

--- db/migrate/20171221153738_change_client_state_to_acceptance.rb
+++ db/migrate/20171221153738_change_client_state_to_acceptance.rb
@@ -2,7 +2,6 @@ class ChangeClientStateToAcceptance < ActiveRecord::Migration[5.1]
   def change
     change_table :clients do |t|
       t.integer :acceptance, default: 0
-      t.boolean :active, default: false
     end
   end
 
