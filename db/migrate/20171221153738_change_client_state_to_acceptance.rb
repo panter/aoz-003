411ec3e7200393c252348050d719d4d350a4919b
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2018-01-03 15:51:05 +0100

    fix migration (remove column, migrate existing data)

--- db/migrate/20171221153738_change_client_state_to_acceptance.rb
+++ db/migrate/20171221153738_change_client_state_to_acceptance.rb
@@ -1,15 +1,29 @@
 class ChangeClientStateToAcceptance < ActiveRecord::Migration[5.1]
-  def change
-    change_table :clients do |t|
-      t.integer :acceptance, default: 0
-    end
-  end
-
   def up
+    add_column :clients, :acceptance, :integer, default: 0
+    Client.find_each do |client|
+      if ['registered', 'reserved', 'active'].include?(client.state)
+        client.update(acceptance: 'accepted')
+      elsif client.state == 'finished'
+        client.update(acceptance: 'resigned')
+      else
+        client.update(acceptance: 'rejected')
+      end
+    end
     remove_column :clients, :state
   end
 
   def down
     add_column :clients, :state, :string, default: 'registered'
+    Client.find_each do |client|
+      if client.acceptance == 'accepted'
+        client.update(state: 'registered')
+      elsif client.acceptance == 'resigned'
+        client.update(state: 'finished')
+      else
+        client.update(state: 'rejected')
+      end
+    end
+    remove_column :clients, :acceptance
   end
 end
