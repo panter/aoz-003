9e10f9b87d90e3784891017dc0eb977f4a9f72da
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2018-01-03 14:51:19 +0100

    make use of factory

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
   def setup
+    # client is by default accepted
     @client = create :client, :with_relatives, :with_language_skills
   end
 
@@ -14,22 +15,20 @@ class ClientTest < ActiveSupport::TestCase
     refute client.valid?
   end
 
-  test 'clients need accompanying' do
+  test 'a client without assignment should show up in need accompanying' do
     result = Client.need_accompanying
     assert_equal [@client], result.to_a
   end
 
   test 'a client with an active assignment should not show up in need accompanying' do
-    @client.create_assignment!(volunteer: create(:volunteer), creator: create(:user),
-      period_start: 10.days.ago, period_end: nil)
+    create :assignment_active, client: @client
     result = Client.need_accompanying
     assert_equal [], result.to_a
   end
 
   test 'a client with an inactive assignment should show up in need accompanying' do
     client = create :client
-    client.create_assignment!(volunteer: create(:volunteer), creator: create(:user),
-      period_start: 10.days.ago, period_end: 2.days.ago)
+    create :assignment_inactive, client: client
     result = Client.need_accompanying
     assert_equal [@client, client], result.to_a
   end
