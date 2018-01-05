2091201f6d159de2961f4b33ab71a8a26050f9a8
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2018-01-05 14:10:19 +0100

    scope cleanup

--- app/models/concerns/group_assignment_and_assignment_common.rb
+++ app/models/concerns/group_assignment_and_assignment_common.rb
@@ -15,11 +15,12 @@ module GroupAssignmentAndAssignmentCommon
     scope :end_before, ->(date) { where('period_end < ?', date) }
     scope :end_after, ->(date) { where('period_end > ?', date) }
     scope :end_within, ->(date_range) { where(period_end: date_range) }
+    scope :ended, (-> { where('period_end <= ?', Time.zone.today) })
     scope :end_in_future, (-> { where('period_end > ?', Time.zone.today) })
     scope :not_ended, (-> { no_end.or(end_in_future) })
 
     scope :no_start, (-> { where(period_start: nil) })
-    scope :started, (-> { where('period_start < ?', Time.zone.today) })
+    scope :started, (-> { where('period_start <= ?', Time.zone.today) })
     scope :will_start, (-> { where('period_start > ?', Time.zone.today) })
     scope :start_before, ->(date) { where('period_start < ?', date) }
     scope :start_at_or_before, ->(date) { where('period_start <= ?', date) }
@@ -38,7 +39,7 @@ module GroupAssignmentAndAssignmentCommon
     }
     scope :active, (-> { not_ended.started })
     scope :stay_active, (-> { active.no_end })
-    scope :inactive, (-> { ended.or(no_start) })
+    scope :inactive, (-> { ended.or(no_start).or(will_start) })
     scope :active_between, lambda { |start_date, end_date|
       no_end.start_before(end_date)
             .or(start_before(end_date).end_after(start_date))
--- app/models/group_assignment.rb
+++ app/models/group_assignment.rb
@@ -19,22 +19,6 @@ class GroupAssignment < ApplicationRecord
     message: 'Diese/r Freiwillige ist schon im Gruppenangebot'
   }
 
-  scope :active, lambda {
-    started.where(
-      'group_assignments.period_end > ? OR group_assignments.period_end IS NULL',
-      Time.zone.today
-    )
-  }
-  scope :started, lambda {
-    where(
-      'group_assignments.period_start < ? AND group_assignments.period_start IS NOT NULL',
-      Time.zone.today
-    )
-  }
-  scope :ended, lambda {
-    where('group_assignments.period_end < ?', Time.zone.today)
-  }
-
   def save_group_assignment_logs
     group_assignment_logs.create!(group_offer_id: group_offer_id, volunteer_id: volunteer_id,
       group_assignment_id: id, title: group_offer.title,
