c4af5436d3ba5ffbcea924f21e8bdb5a80e3035f
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2018-01-05 17:49:59 +0100

    system test for active inactive tandem

---
+++ test/system/assignment_filters_test.rb
@@ -0,0 +1,42 @@
+require 'application_system_test_case'
+
+class AssignmentFiltersTest < ApplicationSystemTestCase
+  def setup
+    @assignment_active = create :assignment_active
+    @assignment_inactive = create :assignment_inactive
+    login_as create(:user)
+    visit assignments_path
+  end
+
+  test 'filter by activity' do
+    assert page.has_link? @assignment_active.client.contact.full_name
+    assert page.has_link? @assignment_inactive.client.contact.full_name
+    within '.section-navigation' do
+      click_link 'Status'
+      click_link 'Aktiv'
+    end
+    visit current_url
+    within 'tbody' do
+      assert page.has_link? @assignment_active.client.contact.full_name
+      refute page.has_link? @assignment_inactive.client.contact.full_name
+    end
+    within '.section-navigation' do
+      click_link 'Status: Aktiv'
+      click_link 'Inaktiv'
+    end
+    visit current_url
+    within 'tbody' do
+      refute page.has_link? @assignment_active.client.contact.full_name
+      assert page.has_link? @assignment_inactive.client.contact.full_name
+    end
+    within '.section-navigation' do
+      click_link 'Status: Inaktiv'
+      click_link 'All'
+    end
+    visit current_url
+    within 'tbody' do
+      assert page.has_link? @assignment_active.client.contact.full_name
+      assert page.has_link? @assignment_inactive.client.contact.full_name
+    end
+  end
+end
