7df164cff79122273f27d11689621b48343a734f
Tugce Nur Tas <tas.tugcenur@gmail.com>
2018-01-10 10:41:52 +0100

    migrate cost-center field to client

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221153738) do
+ActiveRecord::Schema.define(version: 20180110083748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 20171221153738) do
     t.text "detailed_description"
     t.string "entry_date"
     t.integer "acceptance", default: 0
+    t.integer "cost_unit"
     t.index ["deleted_at"], name: "index_clients_on_deleted_at"
     t.index ["user_id"], name: "index_clients_on_user_id"
   end
