99be69a64170cf4e8db5c09205145e1b5440d686
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2018-01-12 10:59:10 +0100

    add volunteer journal consistent with client one

--- test/system/journals_test.rb
+++ test/system/journals_test.rb
@@ -5,24 +5,19 @@ class JournalsTest < ApplicationSystemTestCase
     @user = create :user
     @volunteer = create :volunteer, journals: [FactoryBot.create(:journal, user: @user)],
       acceptance: :accepted
-    @client = create :client, journals: [FactoryBot.create(:journal, user: @user)]
     @journal_volunteer = @volunteer.journals.first
-    @client_volunteer = @client.journals.first
     login_as @user
   end
 
-  test 'volunteer has link to his journal entry' do
+  test 'volunteer_has_link_to_their_journal_entry' do
     visit volunteer_path(@volunteer)
-    click_button 'Journal'
-    within '.collapse .table-responsive' do
-      assert page.has_link? @journal_volunteer.user.full_name
-      assert page.has_text? @journal_volunteer.body
-    end
+    click_link 'Journal'
+    assert page.has_link? @journal_volunteer.user.full_name
+    assert page.has_text? @journal_volunteer.body
   end
 
   test 'can_create_journal_entry_by_link_button_in_show' do
-    Journal.with_deleted(&:really_destroy!)
-    visit client_path(@client)
+    visit client_path(create(:client))
     first('a', text: 'Journal').click
     click_link 'New Journal'
     assert page.has_text? 'New Journal'
@@ -34,22 +29,17 @@ class JournalsTest < ApplicationSystemTestCase
   end
 
   test 'can_delete_a_journal_through_edit' do
-    superadmin = create :user
-    Journal.with_deleted.map(&:really_destroy!)
-    create :journal, journalable: @volunteer, user: superadmin, body: 'bogus_journal_text'
-    login_as superadmin
     visit volunteer_path(@volunteer)
 
-    click_button 'Journal'
-    assert page.has_text? 'bogus_journal_text'
+    click_link 'Journal'
+    assert page.has_text? @journal_volunteer.body
     first('a', text: 'Edit').click
     assert page.has_text? 'Edit Journal'
     page.accept_confirm do
       first('a', text: 'Delete').click
     end
     assert page.has_text? 'Journal was successfully deleted.'
-    click_button 'Journal'
-    refute page.has_link? @journal_volunteer.user.full_name
+    click_link 'Journal'
     refute page.has_text? @journal_volunteer.body
   end
 end
