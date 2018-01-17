bda01b1b34edb9975f4ed503c55236ed48765ef7
Kaspar Vollenweider <vok@panter.ch>
2018-01-17 10:11:02 +0100

    hopefully working config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,3 @@
-set :application, 'AOZ-003'
-set :repo_url, 'git@git.panter.ch:open-source/aoz-003.git'
+set :application, "AOZ-003"
+set :repo_url, "git@git.panter.ch:open-source/aoz-003.git"
 append :linked_files, '.env'
