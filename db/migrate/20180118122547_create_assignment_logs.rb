c0a1605f84b928dca6f19d05b5e1b325a7d9fc24
Kaspar Vollenweider <vok@panter.ch>
2018-01-18 14:23:10 +0100

    corrections in migration plus schema migrated

--- db/migrate/20180118122547_create_assignment_logs.rb
+++ db/migrate/20180118122547_create_assignment_logs.rb
@@ -4,10 +4,10 @@ class CreateAssignmentLogs < ActiveRecord::Migration[5.1]
       t.belongs_to :assignment, foreign_key: true, index: true
       t.belongs_to :volunteer, foreign_key: true, index: true
       t.belongs_to :client, foreign_key: true, index: true
-      t.belongs_to :creator, foreign_key: true, references: :users, index: true
-      t.belongs_to :period_end_set_by, references: :users, index: true
-      t.belongs_to :termination_submitted_by, references: :users, index: true
-      t.belongs_to :termination_verified_by, references: :users, index: true
+      t.references :creator, references: :users, index: true
+      t.references :period_end_set_by, references: :users, index: true
+      t.references :termination_submitted_by, references: :users, index: true
+      t.references :termination_verified_by, references: :users, index: true
 
       t.date :period_start
       t.date :period_end
