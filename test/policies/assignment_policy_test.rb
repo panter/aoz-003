44974b7a5d9a0d6aa371aaf6e6ab9e3c5336f622
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2018-01-18 14:09:18 +0100

    assignment policy tests

--- test/policies/assignment_policy_test.rb
+++ test/policies/assignment_policy_test.rb
@@ -2,40 +2,33 @@ require 'test_helper'
 
 class AssignmentPolicyTest < PolicyAssertions::Test
   def setup
-    @superadmin = create :user, role: 'superadmin'
-    @social_worker = create :user, role: 'social_worker'
-    @department_manager = create :department_manager
-    @volunteer = create :volunteer_with_user
-    @assignment = create :assignment, volunteer: @volunteer
-    @other_assignment = create :assignment, volunteer: (create :volunteer_with_user)
+    @actions = ['index?', 'terminated_index?', 'search?', 'new?', 'create?', 'find_client?',
+                'show?', 'edit?', 'update?', 'update_submitted_at?', 'terminate?',
+                'update_terminated_at?', 'last_submitted_hours_and_feedbacks?',
+                'supervisor?', 'verify_termination?', 'destroy?']
   end
 
   test 'superadmin_can_use_all_actions' do
-    assert_permit(@superadmin, Assignment, 'new?', 'create?', 'edit?', 'update?', 'show?', 'index?',
-      'destroy?', 'find_client?', 'supervisor?',
-      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
+    assert_permit(create(:user), Assignment, *@actions)
   end
 
-  test 'department_manager_can_use_all_actions_but_destroy' do
-    assert_permit(@department_manager, Assignment, 'new?', 'create?', 'edit?', 'update?', 'show?',
-      'index?', 'find_client?', 'supervisor?',
-      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
-    refute_permit(@department_manager, Assignment, 'destroy?')
+  test 'department_manager_can_use_all_actions_but_destroy_and_verify_termination' do
+    department_manager = create(:department_manager)
+    assert_permit(department_manager, Assignment, *@actions[0..-3])
+    refute_permit(department_manager, Assignment, *@actions[-2..-1])
   end
 
   test 'social_worker_has_no_access' do
-    refute_permit(@social_worker, Assignment, 'new?', 'update?', 'edit?', 'show?', 'index?',
-      'destroy?', 'find_client?', 'supervisor?',
-      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
+    refute_permit(create(:social_worker), Assignment, *@actions)
   end
 
   test 'volunteer_has_limited_access' do
-    assert_permit(@volunteer.user, @assignment, 'edit?', 'update?', 'show?',
-      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
-    refute_permit(@volunteer.user, Assignment, 'new?', 'create?', 'index?',
-      'destroy?', 'supervisor?', 'find_client?')
-    refute_permit(@volunteer.user, @other_assignment, 'new?', 'create?', 'edit?', 'update?',
-      'show?', 'index?', 'destroy?', 'supervisor?', 'find_client?',
-      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
+    volunteer = create :volunteer_with_user
+    assignment = create :assignment, volunteer: volunteer
+    other_assignment = create :assignment, volunteer: (create :volunteer_with_user)
+
+    assert_permit(volunteer.user, assignment, *@actions[6..12])
+    refute_permit(volunteer.user, Assignment, *@actions[0..5], *@actions[13..-1])
+    refute_permit(volunteer.user, other_assignment, *@actions)
   end
 end
