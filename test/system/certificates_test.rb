8810c0c976a216b7cf5052da53eeda705302c993
Chrysanthi Lagodimou <chrysa.lagodimou@gmail.com>
2018-01-19 11:53:02 +0100

    assignment kinds done should be uniq

--- test/system/certificates_test.rb
+++ test/system/certificates_test.rb
@@ -2,32 +2,28 @@ require 'application_system_test_case'
 
 class CertificatesTest < ApplicationSystemTestCase
   def setup
-    @user = create :user
-    @volunteer = create(
-      :volunteer, :with_assignment, user: create(:user_volunteer), acceptance: :resigned
-    )
+    @volunteer = create(:volunteer, :with_assignment, user: create(:user_volunteer))
     @assignment = @volunteer.assignments.first
-    @hour = create :hour, volunteer: @volunteer, hourable: @assignment, hours: 2,
-      minutes: 15
+    @hour = create :hour, volunteer: @volunteer, hourable: @assignment, hours: 2, minutes: 15
+    login_as create(:user)
   end
 
-  test 'volunteer user can not see create certificate button' do
+  test 'volunteer_user_cannot_see_create_certificate_button' do
     login_as @volunteer.user
     visit volunteer_path(@volunteer)
-    assert page.has_no_link? 'Create certificate'
+    refute page.has_link? 'Create certificate'
   end
 
-  test 'Creating volunteer certificate form has right content prefilled' do
+  test 'creating_volunteer_certificate_form_has_right_content_prefilled' do
     group_offer = create :group_offer, volunteers: [@volunteer]
     group_offer.group_assignments.last.update(period_start: 2.years.ago)
-    login_as @user
     visit volunteer_path(@volunteer)
     click_link 'Create certificate'
     assert page.has_text? 'New Certificate'
     assert page.has_field? 'Text body', text: 'Die **AOZ** ist ein Unternehmen der Stadt Zürich und'
     assert page.has_field? 'Hours', with: 2
     click_button 'Edit more fields'
-    assert page.has_field? 'Tandem', checked: true
+    assert page.has_field? 'Tandem', checked: true, count: 1
     assert page.has_field? 'Name', with: @volunteer.contact.full_name
     assert page.find_field('Street').value.include? @volunteer.contact.street
     assert page.find_field('City').value.include? @volunteer.contact.city
@@ -43,7 +39,6 @@ class CertificatesTest < ApplicationSystemTestCase
   end
 
   test 'updating_certificate_with_custom_values' do
-    login_as @user
     visit new_volunteer_certificate_path(@volunteer)
     click_button 'Create Certificate'
 
@@ -53,7 +48,7 @@ class CertificatesTest < ApplicationSystemTestCase
     fill_in 'Text body', with: '**Bold** or not *bold*, that is this tests Question?<br>***both***'
     assert page.has_text? 'Edit Certificate'
     click_button 'Edit more fields'
-    assert page.has_field? 'Tandem', checked: true
+    assert page.has_field? 'Tandem', checked: true, count: 1
     fill_in 'Hours', with: 555
     fill_in 'Name', with: 'This bogus test name'
     fill_in 'Institution', with: 'The Testology Institute'
@@ -64,14 +59,20 @@ class CertificatesTest < ApplicationSystemTestCase
     assert page.has_text? 'Bold or not bold, that is this tests Question? both'
   end
 
-  test 'volunteer that has only group offers can have certificate' do
+  test 'show_certificate_has_tandem_only_once' do
+    visit new_volunteer_certificate_path(@volunteer)
+    click_button 'Create Certificate'
+
+    visit volunteer_certificate_path(@volunteer, @volunteer.certificates.last)
+    assert page.has_text? 'Tandem', count: 1
+  end
+
+  test 'volunteer_that_has_only_group_offers_can_have_certificate' do
     volunteer = create :volunteer
     create :group_offer, volunteers: [volunteer]
     volunteer.group_assignments.last.update(period_start: 2.months.ago)
-    login_as @user
 
     visit volunteer_path(volunteer)
-    assert page.has_link? 'Create certificate'
     click_link 'Create certificate'
     page.find_button('Create Certificate').click
     assert page.has_text? 'Die AOZ ist ein Unternehmen der Stadt Zürich'
