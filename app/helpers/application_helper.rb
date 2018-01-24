c99c7137c163ee9539ec33e3b51b0b25b8fd02a0
Kaspar Vollenweider <vok@panter.ch>
2018-01-24 10:23:13 +0100

    move navigation button and icon helpers into own helper for better order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,33 +7,10 @@ module ApplicationHelper
     boostrap_row { f.error_notification } if f.error_notification.present?
   end
 
-  def button_link(text, target, type = 'default', dimension: nil)
-    btn_size = " btn-#{dimension}" if dimension
-    link_to text, target, class: "btn btn-#{type}#{btn_size}"
-  end
-
   def link_to_add_polymorphic_association(*args)
     name, f, association, html_options = *args
     html_options[:partial] = "#{association}/fields"
     link_to_add_association(name, f, association, html_options)
-  end
-
-  def form_navigation_btn(action, cols: 12, with_row: true, md_cols: nil, with_col: false)
-    button = make_nav_button(action)
-    button = bootstrap_col(cols, md_cols) { button } if with_col || with_row
-    button = boostrap_row { button } if with_row
-    button
-  end
-
-  def make_nav_button(action)
-    if action == :back
-      text = navigation_glyph(:back)
-      action = :index if action == :back
-    else
-      text = t_title(action)
-    end
-    button_link(text,
-      controller: controller_name, action: action, id: action == :index || params[:id])
   end
 
   def bootstrap_col(cols = 12, md_cols = nil)
@@ -104,42 +81,6 @@ module ApplicationHelper
 
   def search_parameters
     @search_parameters ||= params[:q]&.to_unsafe_hash || {}
-  end
-
-  def boolean_glyph(value)
-    if value
-      content_tag(:i, '', class: 'glyphicon glyphicon-ok text-success')
-    else
-      content_tag(:i, '', class: 'glyphicon glyphicon-remove text-danger')
-    end
-  end
-
-  def navigation_glyph(icon_type = :back)
-    translate_glyph = {
-      back: { text: 'Back', glyph: 'arrow-left' },
-      print: { text: 'Print', glyph: 'print' },
-      delete: { text: 'Delete', glyph: 'trash' }
-    }
-    glyph_span(translate_glyph[icon_type.to_sym])
-  end
-
-  def glyph_span(text: 'Back', glyph: 'arrow-left')
-    tag.span(class: "glyphicon glyphicon-#{glyph}") do
-      tag.span(text, class: 'sr-only')
-    end
-  end
-
-  def navigation_fa_icon(icon_type = :xlsx)
-    translate_fa = {
-      xlsx: { text: 'Excel sheet', fa_type: 'file-excel-o' }
-    }
-    fa_span(translate_fa[icon_type.to_sym])
-  end
-
-  def fa_span(text: 'xlsx', fa_type: 'file-excel-o')
-    tag.span(class: "fa fa-#{fa_type}") do
-      tag.span(text, class: 'sr-only')
-    end
   end
 
   def bootstrap_paginate(paginate_collection)
