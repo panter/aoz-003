5ba441526b315000a842ae132eb004a0e3611d4f
Kaspar Vollenweider <vok@panter.ch>
2018-01-25 14:58:32 +0100

    update group_assignment policy test

--- test/policies/group_assignments_policy_test.rb
+++ test/policies/group_assignments_policy_test.rb
@@ -7,31 +7,39 @@ class GroupAssignmentPolicyTest < PolicyAssertions::Test
     @other_volunteer = create :volunteer_with_user
     @department_manager = create :department_manager
     @social_worker = create :social_worker
-    @group_assignment = GroupAssignment.create(volunteer: @volunteer,
-      group_offer: (create :group_offer))
-    @other_group_assignment = GroupAssignment.create(volunteer: @other_volunteer,
-      group_offer: (create :group_offer))
+    @group_offer = create :group_offer, creator: @department_manager,
+      department: @department_manager.department.first
+    @group_assignment = create :group_assignment, volunteer: @volunteer, group_offer: @group_offer
+    @group_assignment_other_volunteer = create :group_assignment, group_offer: @group_offer,
+      volunteer: @other_volunteer
+    @group_assignment_other_group_offer = create :group_assignment
   end
 
   test 'superadmin_can_use_all_actions' do
-    assert_permit(@superadmin, GroupAssignment, 'show?', 'last_submitted_hours_and_feedbacks?',
-      'update_submitted_at?')
+    assert_permit(@superadmin, GroupAssignment, 'verify_termination?', 'set_end_today?', 'edit?',
+      'update?', 'show?', 'update_submitted_at?', 'terminate?', 'update_terminated_at?',
+      'last_submitted_hours_and_feedbacks?')
   end
 
   test 'department_manager_can_use_all_actions' do
-    assert_permit(@department_manager, GroupAssignment, 'show?',
-      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
+    assert_permit(@department_manager, GroupAssignment, 'verify_termination?', 'set_end_today?',
+      'edit?', 'update?', 'show?', 'update_submitted_at?', 'terminate?', 'update_terminated_at?',
+      'last_submitted_hours_and_feedbacks?')
   end
 
   test 'social_worker_has_no_access' do
-    refute_permit(@social_worker, GroupAssignment, 'show?', 'last_submitted_hours_and_feedbacks?',
-      'update_submitted_at?')
+    refute_permit(@social_worker, GroupAssignment, 'verify_termination?', 'set_end_today?',
+      'edit?', 'update?', 'show?', 'update_submitted_at?', 'terminate?', 'update_terminated_at?',
+      'last_submitted_hours_and_feedbacks?')
   end
 
   test 'volunteer_has_limited_access' do
-    assert_permit(@volunteer.user, @group_assignment, 'show?',
-      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
-    refute_permit(@volunteer.user, @other_group_assignment, 'show?',
-      'last_submitted_hours_and_feedbacks?', 'update_submitted_at?')
+    assert_permit(@volunteer.user, @group_assignment, 'update?', 'show?', 'update_submitted_at?',
+      'terminate?', 'update_terminated_at?', 'last_submitted_hours_and_feedbacks?')
+    refute_permit(@volunteer.user, @group_assignment, 'verify_termination?', 'set_end_today?',
+      'edit?')
+    refute_permit(@volunteer.user, @group_assignment_other_volunteer, 'update?', 'show?',
+      'update_submitted_at?', 'terminate?', 'update_terminated_at?', 'set_end_today?',
+      'last_submitted_hours_and_feedbacks?', 'verify_termination?', 'edit?')
   end
 end
