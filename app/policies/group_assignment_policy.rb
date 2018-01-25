0141fbf657ddaaa3f9d378792914fe06d2225d5d
Tugce Nur Tas <tas.tugcenur@gmail.com>
2018-01-25 15:20:50 +0100

    add scope to GroupAssignmentPolicy

--- app/policies/group_assignment_policy.rb
+++ app/policies/group_assignment_policy.rb
@@ -1,14 +1,23 @@
 class GroupAssignmentPolicy < ApplicationPolicy
-  alias_method :index?,  :admin_or_department_manager_or_volunteer_related?
-  alias_method :update?, :admin_or_department_manager_or_volunteer_related?
-  alias_method :show?,   :admin_or_department_manager_or_volunteer_related?
+  class Scope < ApplicationScope
+    def resolve
+      return all if superadmin?
+      return scope.where(creator_id: user.id) if department_manager?
+      none
+    end
+  end
+
+  alias_method :update?,              :admin_or_department_manager_or_volunteer_related?
+  alias_method :show?,                :admin_or_department_manager_or_volunteer_related?
   alias_method :update_submitted_at?, :admin_or_department_manager_or_volunteer_related?
   alias_method :last_submitted_hours_and_feedbacks?,
-    :admin_or_department_manager_or_volunteer_related?
+  :admin_or_department_manager_or_volunteer_related?
 
-  alias_method :edit?,          :superadmin_or_department_manager?
-  alias_method :update?,        :superadmin_or_department_manager?
-  alias_method :set_end_today?, :superadmin_or_department_manager?
+  alias_method :terminated_index?,    :superadmin_or_department_manager?
+  alias_method :edit?,                :superadmin_or_department_manager?
+  alias_method :update?,              :superadmin_or_department_manager?
+  alias_method :set_end_today?,       :superadmin_or_department_manager?
+  alias_method :terminate?,           :superadmin_or_department_manager?
 
-  alias_method :terminate?,     :superadmin_or_department_manager?
+  alias_method :verify_termination?,  :superadmin?
 end
