b833203d7135d7e661af9a8d99ec2c320db6c645
Tugce Nur Tas <tas.tugcenur@gmail.com>
2018-01-26 09:51:02 +0100

    remove not yet needed tests

--- test/system/group_assignment_termination_index_test.rb
+++ test/system/group_assignment_termination_index_test.rb
@@ -17,11 +17,6 @@ class GroupAssignmentTerminationIndexTest < ApplicationSystemTestCase
       period_start: 3.weeks.ago, period_end: 2.days.ago,
       termination_submitted_at: 2.days.ago, termination_submitted_by: @volunteer3.user
 
-    @verified = create :group_assignment, group_offer: @group_offer1, volunteer: @volunteer4,
-      period_start: 3.weeks.ago, period_end: 2.days.ago,
-      termination_submitted_at: 2.days.ago, termination_submitted_by: @volunteer4.user,
-      termination_verified_at: 2.days.ago, termination_verified_by: @superadmin
-
     login_as @superadmin
   end
 
@@ -38,7 +33,6 @@ class GroupAssignmentTerminationIndexTest < ApplicationSystemTestCase
     assert page.has_text? termination_index_table_text(@un_submitted)
     assert page.has_text? termination_index_table_text(@submitted)
     refute page.has_text? termination_index_table_text(@not_ended)
-    refute page.has_text? termination_index_table_text(@verified)
   end
 
   test 'filtering_submitted_terminations' do
@@ -59,32 +53,6 @@ class GroupAssignmentTerminationIndexTest < ApplicationSystemTestCase
     refute page.has_text? termination_index_table_text(@submitted)
   end
 
-  test 'filtering_for_only_verified' do
-    visit terminated_index_group_assignments_path(q: { termination_verified_by_id_null: 'true' })
-    click_link 'Quittiert: Unquittiert'
-    click_link exact_text: 'Quittiert'
-    visit current_url
-    refute page.has_text? termination_index_table_text(@un_submitted)
-    refute page.has_text? termination_index_table_text(@submitted)
-    assert page.has_text? termination_index_table_text(@verified)
-  end
-
-  test 'ended_assignment_can_be_verified' do
-    visit group_offers_path
-    click_link 'Beendete Begleitungen'
-    assert page.has_text? termination_index_table_text(@un_submitted)
-    assert page.has_text? termination_index_table_text(@submitted)
-    refute page.has_text? termination_index_table_text(@verified)
-
-    page.find_all('a', text: 'Beendigung Quittieren').first.click
-    click_link 'Beendigung Quittieren'
-
-    assert page.has_text? 'Beendete Begleitungen'
-    refute page.has_text? termination_index_table_text(@un_submitted)
-    refute page.has_text? termination_index_table_text(@submitted)
-    refute page.has_text? termination_index_table_text(@verified)
-  end
-
   test 'clear_filter_link_is_working_correctly' do
     visit group_offers_path
     click_link 'Beendete Begleitungen'
@@ -98,8 +66,7 @@ class GroupAssignmentTerminationIndexTest < ApplicationSystemTestCase
     visit current_url
     assert page.has_text? termination_index_table_text(@un_submitted)
     assert page.has_text? termination_index_table_text(@submitted)
-    # refute page.has_text? termination_index_table_text(@not_ended)
-    # refute page.has_text? termination_index_table_text(@verified)
+    refute page.has_text? termination_index_table_text(@not_ended)
   end
 
   test 'there_is_correct_links_to_the_termination_forms' do
@@ -116,33 +83,26 @@ class GroupAssignmentTerminationIndexTest < ApplicationSystemTestCase
       href: new_volunteer_certificate_path(@submitted.volunteer.id)
   end
 
-    # test 'assignment_quittieren_creates_a_assignment_log_record_from_assignment' do
-    #   visit terminated_index_group_assignments_path(q: { termination_verified_by_id_null: 'true' })
-    #   click_link 'Beendigung Quittieren', href: verify_termination_group_assignment_path(@submitted.id)
-    #   assert page.has_text? 'Der Einsatz wurde erfolgreich quittiert.'
-    #   assert_equal @submitted, GroupAssignmentLog.find_by(group_assignment_id: @submitted.id).group_assignment
-    # end
+  test 'there_is_correct_links_on_email_status_column' do
+    create :email_template_termination
+    visit terminated_index_group_assignments_path(q: { termination_verified_by_id_null: 'true' })
 
-  # test 'there_is_correct_links_on_email_status_column' do
-  #   create :email_template_termination
-  #   visit terminated_index_group_assignments_path(q: { termination_verified_by_id_null: 'true' })
+    # Assignment has an end-date, but no reminder mailing was created
+    click_link 'Beendigungs Email erstellen',
+      href: new_termination_group_assignment_reminder_mailings_path(@un_submitted)
+    click_button 'Erstellen und Vorschau anzeigen'
 
-  #   # Assignment has an end-date, but no reminder mailing was created
-  #   click_link 'Beendigungs Email erstellen',
-  #     href: new_termination_group_assignment_reminder_mailings_path(@un_submitted)
-  #   click_button 'Erstellen und Vorschau anzeigen'
-  #   click_link 'Zurück'
+    # Assignment has an end-date, reminder mailing was created, but not sent
+    visit terminated_index_group_assignments_path(q: { termination_verified_by_id_null: 'true' })
+    click_link 'Nicht versandt',
+      href: reminder_mailing_path(@un_submitted.reminder_mailings.termination.last)
+    click_link 'Email versenden'
 
-  #   # Assignment has an end-date, reminder mailing was created, but not sent
-  #   click_link 'Nicht versandt',
-  #     href: reminder_mailing_path(@un_submitted.reminder_mailings.termination.last)
-  #   click_link 'Email versenden'
-
-  #   # Assignment has an end-date, reminder mailing was created and was sent
-  #   visit terminated_index_group_assignments_path(q: { termination_verified_by_id_null: 'true' })
-  #   @un_submitted.reload
-  #   mailing = @un_submitted.reminder_mailings.termination.last
-  #   assert page.has_link? "Übermittelt am #{I18n.l(mailing.updated_at.to_date)}",
-  #     href: reminder_mailing_path(mailing)
-  # end
+    # Assignment has an end-date, reminder mailing was created and was sent
+    visit terminated_index_group_assignments_path(q: { termination_verified_by_id_null: 'true' })
+    @un_submitted.reload
+    mailing = @un_submitted.reminder_mailings.termination.last
+    assert page.has_link? "Übermittelt am #{I18n.l(mailing.updated_at.to_date)}",
+      href: reminder_mailing_path(mailing)
+  end
 end
