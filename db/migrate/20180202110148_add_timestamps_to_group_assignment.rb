be9278295e1841609733535e64eaa33e9d85386e
Tugce Nur Tas <tas.tugcenur@gmail.com>
2018-02-02 17:47:46 +0100

    fix migration null:true for deployment

--- db/migrate/20180202110148_add_timestamps_to_group_assignment.rb
+++ db/migrate/20180202110148_add_timestamps_to_group_assignment.rb
@@ -1,7 +1,7 @@
 class AddTimestampsToGroupAssignment < ActiveRecord::Migration[5.1]
   def change
     add_timestamps :group_assignments, null: true
-    GroupAssignment.update_all(created_at: Time.zone.now, updated_at: Time.zone.now)
+    GroupAssignment.with_deleted.update_all(created_at: Time.zone.now, updated_at: Time.zone.now)
     change_column_null :group_assignments, :created_at, false
     change_column_null :group_assignments, :updated_at, false
   end
