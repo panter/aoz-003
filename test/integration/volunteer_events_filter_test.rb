4f1b88d03cbcbdaad4e34c38ab839753161f18b2
Tugce Nur Tas <tas.tugcenur@gmail.com>
2018-02-14 16:15:40 +0100

    add intro course visited filter to volunteers index and integration test

---
+++ test/integration/volunteer_events_filter_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class VolunteerEventsFilterTest < ActionDispatch::IntegrationTest
+  def setup
+    @superadmin = create :user
+    @volunteer_intro_visited = create :volunteer_with_user, intro_course: true
+    @volunteer_intro_not_visited = create :volunteer_with_user, intro_course: false
+    login_as @superadmin
+  end
+
+  test 'Volunteer intro course visited filter returns correct volunteers' do
+    get volunteers_path(q: { intro_course_eq: 'true' })
+    assert response.body.include? @volunteer_intro_visited.full_name
+    refute response.body.include? @volunteer_intro_not_visited.full_name
+  end
+
+  test 'Volunteer intro course not visited filter returns correct volunteers' do
+    get volunteers_path(q: { intro_course_eq: 'false' })
+    assert response.body.include? @volunteer_intro_not_visited.full_name
+    refute response.body.include? @volunteer_intro_visited.full_name
+  end
+end
+
