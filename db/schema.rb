485937ed7ce490b8beca12d6e64bcbd5b718333b
Kaspar Vollenweider <vok@panter.ch>
2018-02-19 19:03:34 +0100

    schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180216165632) do
+ActiveRecord::Schema.define(version: 20180219131415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -405,7 +405,6 @@ ActiveRecord::Schema.define(version: 20180216165632) do
     t.index ["period_end_set_by_id"], name: "index_group_offers_on_period_end_set_by_id"
     t.index ["period_start"], name: "index_group_offers_on_period_start"
     t.index ["search_volunteer"], name: "index_group_offers_on_search_volunteer"
-    t.index ["termination_verified_by_id"], name: "index_group_offers_on_termination_verified_by_id"
   end
 
   create_table "hours", force: :cascade do |t|
