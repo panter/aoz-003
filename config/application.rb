fbd713ec179108edbbbbd7f7b84634036b2e0015
Kaspar Vollenweider <vok@panter.ch>
2018-02-19 18:11:31 +0100

    still save utc in db, just display CET

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Aoz
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = 'Zurich'
-    config.active_record.default_timezone = :local
 
     config.autoload_paths += Dir[config.root.join('lib/access_import/**/')]
     config.autoload_paths += Dir[config.root.join('lib/access_import/accessors/**/')]
