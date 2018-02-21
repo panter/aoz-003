0787cb00540a3f9886a90b650861493c1c5847c5
Kaspar Vollenweider <vok@panter.ch>
2018-02-21 17:12:06 +0100

    finding all active clients and volunteers

--- lib/access_import_test/access_imports_test.rb
+++ lib/access_import_test/access_imports_test.rb
@@ -1,16 +1,56 @@
 require 'tasks_test_helper'
 
 class AccessImportsTest < ActiveSupport::TestCase
-  test 'tryit out' do
+  test 'active_volunteers' do
     active_xls = extract_xlsx('ListeAktiveFreiwillige')
     found_active_volunteers = find_all_volunteers_in_xls(active_xls)
-    # all_xls = extract_xlsx('ListeAllerFreiwilligen')
-    # found_all_volunteers = find_all_volunteers_in_xls(all_xls)
     binding.pry
   end
 
-  def find_volunteer_from_row(hash)
-    found, query = narrow_query(Volunteer.joins(:contact), 'contacts.first_name = ?', hash[:first_name])
+  test 'active_clients' do
+    active_xls = extract_xlsx('ListeAktiveBegleitete')
+    found_active_clients = find_all_clients_in_xls(active_xls)
+    binding.pry
+  end
+
+  def find_all_volunteers_in_xls(xls)
+    query = Volunteer.joins(:contact)
+    parse_volunteers_xls_header(xls).each_with_index.map do |row, index|
+      [index, { found: find_from_row(row, query), row: row }]
+    end.to_h
+  end
+
+  def find_all_clients_in_xls(xls)
+    query = Client.joins(:contact)
+    parse_clients_xls_header(xls).each_with_index.map do |row, index|
+      [index, { found: find_from_row(row, query), row: row }]
+    end.to_h
+  end
+
+  def parse_clients_xls_header(xls)
+    xls.parse(
+      anrede: /Anrede/, last_name: /Nachname/, first_name: /Vorname/,
+      street: /Adresszeile\ 1/, extended: /Adresszeile\ 2/, postal_code: /PLZ/,
+      city: /Ort/, primary_phone: /Telefon/, secondary_phone: /Mobile/, email: /Email/,
+      birth_year: /Geburtsdatum/, gender: /Geschlecht/, nationality: /Nationalität/,
+      profession: /Beruf/, entry_date: /Eintritt\ CH/, period_start: /Beginn/, period_end: /Ende/,
+      persons_in_family: /Anzahl\ Personen/
+    )
+  end
+
+  def parse_volunteers_xls_header(xls)
+    xls.parse(
+      anrede: /Anrede/, last_name: /Nachname/, first_name: /Vorname/,
+      street: /Adresszeile\ 1/, extended: /Adresszeile\ 2/, postal_code: /PLZ/,
+      city: /Ort/, primary_phone: /Telefon/, secondary_phone: /Mobile/, email: /Email/,
+      birth_year: /Geburtsdatum/, gender: /Geschlecht/, nationality: /Nationalität/,
+      profession: /Beruf/, entry_date: /Eintritt\ CH/, period_start: /Beginn/, period_end: /Ende/,
+      intro_course: /Einführungskurs/, waive: /Spesenverzicht/, state: /Aktueller\ Status/
+    )
+  end
+
+  def find_from_row(hash, query)
+    found, query = narrow_query(query, 'contacts.first_name = ?', hash[:first_name])
     return query if found || (!found && !query)
 
     found, query = narrow_query(query, 'contacts.last_name = ?', hash[:last_name])
@@ -22,33 +62,15 @@ class AccessImportsTest < ActiveSupport::TestCase
     found, query = narrow_query(query, 'contacts.city = ?', hash[:city])
     return query if found || (!found && !query)
 
-    found, query = narrow_query(query, 'profession = ?', hash[:profession])
+    found, query = narrow_query(query, 'contacts.street = ?', hash[:street])
     return query if found || (!found && !query)
 
-
-    found, query = narrow_query(query, 'waive = ?', hash[:waive] == 'Ja')
+    found, query = narrow_query(query, 'contacts.extended = ?', hash[:extended])
     return query if found || (!found && !query)
-    # found, query = narrow_query(query, 'contacts.primary_email = ?', row[9])
-    # return query if found || (!found && !query)
-
-    # found, query = narrow_query(query, 'contacts.primary_phone = ?', row[8])
 
     found, query = narrow_query(query, 'birth_year = ?', hash[:birth_year])
     return [false, query] unless found
     query
-  end
-
-  def find_all_volunteers_in_xls(xls)
-    xls.parse(
-      anrede: /Anrede/, last_name: /Nachname/, first_name: /Vorname/,
-      street: /Adresszeile\ 1/, extended: /Adresszeile\ 2/, postal_code: /PLZ/,
-      city: /Ort/, primary_phone: /Telefon/, secondary_phone: /Mobile/, email: /Email/,
-      birth_year: /Geburtsdatum/, gender: /Geschlecht/, nationality: /Nationalität/,
-      profession: /Beruf/, entry_date: /Eintritt\ CH/, period_start: /Beginn/, period_end: /Ende/,
-      intro_course: /Einführungskurs/, waive: /Spesenverzicht/, state: /Aktueller\ Status/
-    ).each_with_index.map do |row, index|
-      [index, { found: find_volunteer_from_row(row), row: row }]
-    end.to_h
   end
 
   def narrow_query(query, sql_condition, value)
