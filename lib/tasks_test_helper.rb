79148817332a7a4b204e28c461b6bbd85db3e989
Kaspar Vollenweider <vok@panter.ch>
2018-02-23 18:28:54 +0100

    remove not needed methods

--- lib/tasks_test_helper.rb
+++ lib/tasks_test_helper.rb
@@ -3,7 +3,6 @@ require 'rails/test_help'
 require 'database_cleaner'
 require 'roo'
 
-
 class ActiveSupport::TestCase
   include FactoryBot::Syntax::Methods
   include Warden::Test::Helpers
@@ -22,17 +21,5 @@ class ActiveSupport::TestCase
 
   def extract_xlsx(filename)
     Roo::Spreadsheet.open("lib/access_import_test/#{filename}.xlsx", extension: 'xlsx')
-  end
-
-  def assert_xls_cols_equal(wb, row, offset, *columns)
-    columns.each_with_index do |column, index|
-      assert_equal column, wb.cell(row, index + 1 + offset)
-    end
-  end
-
-  def really_destroy_with_deleted(*models)
-    models.each do |model|
-      model.with_deleted.map(&:really_destroy!)
-    end
   end
 end
