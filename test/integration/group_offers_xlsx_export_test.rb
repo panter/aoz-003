077a570aa287efc63bb85ea9f46a392270e6d172
Markus Koller <markus-koller@gmx.ch>
2018-03-13 15:23:04 +0100

    Add offer type to group offer export

--- test/integration/group_offers_xlsx_export_test.rb
+++ test/integration/group_offers_xlsx_export_test.rb
@@ -10,7 +10,8 @@ class GroupOffersXlsxExportTest < ActionDispatch::IntegrationTest
     create :group_offer
     wb = get_xls_from_response(group_offers_url(format: :xlsx))
     assert_xls_cols_equal(wb, 1, 0, 'Bezeichnung', 'Ort', 'Verantwortliche/r', 'Zeitangaben',
-      'Zielpublikum', 'Dauer', 'Status des FW-Einsatzes', 'Freiwillige', 'Kategorie')
+      'Zielpublikum', 'Dauer', 'Internes oder externes Gruppenangebot',
+      'Status des FW-Einsatzes', 'Freiwillige', 'Kategorie')
     assert_xls_cols_equal(wb, 2, 0, GroupOffer.first.title)
   end
 end
