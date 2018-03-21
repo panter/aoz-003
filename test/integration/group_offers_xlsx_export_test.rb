6e3eba15eaf135f3c10839b7d33207a883a78770
Tugce Nur Tas <tas.tugcenur@gmail.com>
2018-03-21 20:00:08 +0100

    rename Status FW-Einatzes to FW-Nachfrage

--- test/integration/group_offers_xlsx_export_test.rb
+++ test/integration/group_offers_xlsx_export_test.rb
@@ -11,7 +11,7 @@ class GroupOffersXlsxExportTest < ActionDispatch::IntegrationTest
     wb = get_xls_from_response(group_offers_url(format: :xlsx))
     assert_xls_cols_equal(wb, 1, 0, 'Status', 'Bezeichnung', 'Ort', 'Verantwortliche/r',
       'Zeitangaben', 'Zielpublikum', 'Dauer', 'Internes oder externes Gruppenangebot',
-      'Status des FW-Einsatzes', 'Freiwillige', 'Kategorie')
+      'FW-Nachfrage', 'Freiwillige', 'Kategorie')
     assert_xls_cols_equal(wb, 2, 1, GroupOffer.first.title)
   end
 end
