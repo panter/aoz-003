aa107c925968fa48dfa793bd579036ff04316354
Markus Koller <markus-koller@gmx.ch>
2018-03-23 16:05:09 +0100

    Fix migration
    The default needs to be set before/simultaneously with disallowing
    NULL values.

--- db/migrate/20180320182120_add_group_offer_offer_type_default.rb
+++ db/migrate/20180320182120_add_group_offer_offer_type_default.rb
@@ -1,8 +1,9 @@
 class AddGroupOfferOfferTypeDefault < ActiveRecord::Migration[5.1]
-  def change
-    change_table :group_offers do |t|
-      t.change :offer_type, :string, null: false
-      t.change_default :offer_type, 'internal_offer'
-    end
+  def up
+    change_column :group_offers, :offer_type, :string, null: false, default: 'internal_offer'
+  end
+
+  def down
+    change_column :group_offers, :offer_type, :string, null: true, default: nil
   end
 end
