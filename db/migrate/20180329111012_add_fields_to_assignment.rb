fc14d85da036298d944f3f29da1e097a92467cfa
Emily Wangler <emily.wangler@renuo.ch>
2018-04-03 16:22:22 +0200

    add default values for agreement_text

--- db/migrate/20180329111012_add_fields_to_assignment.rb
+++ db/migrate/20180329111012_add_fields_to_assignment.rb
@@ -6,6 +6,15 @@ class AddFieldsToAssignment < ActiveRecord::Migration[5.1]
     add_column :assignments, :trial_period_end, :string
     add_column :assignments, :duration, :string
     add_column :assignments, :special_agreement, :string
-    add_column :assignments, :agreement_text, :text
+    add_column :assignments, :agreement_text, :text, default: <<~HEREDOC
+      Freiwillige beachten folgende Grundsätze während ihres Einsatzes in der AOZ:
+      * Verhaltenskodex für Freiwillige
+      * Rechte und Pflichten für Freiwillige
+      * AOZ Leitlinien Praktische Integrationsarbeit
+
+      Allenfalls auch
+      * Verpflichtungserklärung zum Schutz der unbegleiteten minderjährigen Asylsuchenden (MNA)
+      * Niederschwellige Gratis-Deutschkurse: Informationen für freiwillige Kursleitende
+    HEREDOC
   end
 end
--- db/migrate/20180329113151_add_fields_to_group_assignment.rb
+++ db/migrate/20180329113151_add_fields_to_group_assignment.rb
@@ -5,6 +5,15 @@ class AddFieldsToGroupAssignment < ActiveRecord::Migration[5.1]
     add_column :group_assignments, :happens_at, :string
     add_column :group_assignments, :frequency, :string
     add_column :group_assignments, :trial_period_end, :string
-    add_column :group_assignments, :agreement_text, :text
+    add_column :group_assignments, :agreement_text, :text, default: <<~HEREDOC
+      Freiwillige beachten folgende Grundsätze während ihres Einsatzes in der AOZ:
+      * Verhaltenskodex für Freiwillige
+      * Rechte und Pflichten für Freiwillige
+      * AOZ Leitlinien Praktische Integrationsarbeit
+
+      Allenfalls auch
+      * Verpflichtungserklärung zum Schutz der unbegleiteten minderjährigen Asylsuchenden (MNA)
+      * Niederschwellige Gratis-Deutschkurse: Informationen für freiwillige Kursleitende
+    HEREDOC
   end
 end
