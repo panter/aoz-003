1e7d74d46806fce94f1e745c7451d6574b106b50
Tugce Nur Tas <tas.tugcenur@gmail.com>
2018-04-03 20:23:52 +0200

    make volunteers own billing expense accessible

--- app/policies/billing_expense_policy.rb
+++ app/policies/billing_expense_policy.rb
@@ -1,9 +1,17 @@
 class BillingExpensePolicy < ApplicationPolicy
-  alias_method :index?,   :superadmin?
-  alias_method :show?,    :superadmin?
+  def index?
+    superadmin? || volunteer? && handle_record_or_class
+  end
+
+  def handle_record_or_class
+    record.class == Class ? true : user.volunteer.id == record.volunteer.id
+  end
+
+  alias_method :show?,    :superadmin_or_volunteers_record?
   alias_method :new?,     :superadmin?
   alias_method :edit?,    :superadmin?
   alias_method :create?,  :superadmin?
   alias_method :update?,  :superadmin?
   alias_method :destroy?, :superadmin?
+  alias_method :superadmin_privileges?, :superadmin?
 end
