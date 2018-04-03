93d3a8bcecbca58ffd7ab8ea0792029edd4049a7
Emily Wangler <emily.wangler@renuo.ch>
2018-04-03 15:48:54 +0200

    add missing 'working_percent' param to controller

--- app/controllers/concerns/volunteer_attributes.rb
+++ app/controllers/concerns/volunteer_attributes.rb
@@ -8,7 +8,7 @@ module VolunteerAttributes
         :profession, :education, :motivation, :experience, :expectations, :strengths,
         :skills, :interests, :man, :woman, :family, :kid, :unaccompanied, :other_offer_desc,
         :trial_period, :doc_sent, :bank_account, :evaluation, :own_kids, :rejection_type,
-        :rejection_text, :intro_course, :flexible, :morning, :afternoon, :evening,
+        :rejection_text, :intro_course, :flexible, :morning, :afternoon, :evening, :working_percent,
         :workday, :weekend, :detailed_description, contact_attributes,
         language_skills_attributes, availability_attributes, group_offer_category_ids: []
       ]
