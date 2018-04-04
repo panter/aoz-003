38a4ccc846c4946b4c80ca3bfe5f1af8954687a0
Tugce Nur Tas <tas.tugcenur@gmail.com>
2018-04-04 11:59:56 +0200

    add policy scope for volunteers billing expenses

--- app/policies/billing_expense_policy.rb
+++ app/policies/billing_expense_policy.rb
@@ -1,17 +1,18 @@
 class BillingExpensePolicy < ApplicationPolicy
-  def index?
-    superadmin? || volunteer? && handle_record_or_class
+  class Scope < ApplicationScope
+    def resolve
+      return all if superadmin?
+      return scope.where(volunteer: user.volunteer) if volunteer?
+      none
+    end
   end
 
-  def handle_record_or_class
-    record.class == Class ? true : user.volunteer.id == record.volunteer.id
-  end
-
-  alias_method :show?,    :superadmin_or_volunteers_record?
-  alias_method :new?,     :superadmin?
-  alias_method :edit?,    :superadmin?
-  alias_method :create?,  :superadmin?
-  alias_method :update?,  :superadmin?
-  alias_method :destroy?, :superadmin?
+  alias_method :index?,                 :superadmin_or_volunteer?
+  alias_method :show?,                  :superadmin_or_volunteer_related?
+  alias_method :new?,                   :superadmin?
+  alias_method :edit?,                  :superadmin?
+  alias_method :create?,                :superadmin?
+  alias_method :update?,                :superadmin?
+  alias_method :destroy?,               :superadmin?
   alias_method :superadmin_privileges?, :superadmin?
 end
