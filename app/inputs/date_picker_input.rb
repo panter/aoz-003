b275728dc2c488670ddd1f0d3bc346e284ce2740
Zsolt Benke <zsolt@decoding.io>
2018-05-01 14:28:25 +0200

    Fix date formatting in date pickers

--- app/inputs/date_picker_input.rb
+++ app/inputs/date_picker_input.rb
@@ -1,6 +1,11 @@
 class DatePickerInput < SimpleForm::Inputs::Base
   def input(wrapper_options)
-    @builder.text_field(attribute_name, input_html_options)
+    attribute_value = @builder.object.send(attribute_name.to_sym)
+    html_opts = input_html_options
+    html_opts = html_opts.merge(
+      value: localize(attribute_value)
+    ) unless attribute_value.nil?
+    @builder.text_field(attribute_name, html_opts)
   end
 
   def input_html_options
