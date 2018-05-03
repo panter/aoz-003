d62c54fabe4ed4f593b2a56217c1eecf870e3975
Tugce Nur Tas <tas.tugcenur@gmail.com>
2018-05-03 13:53:28 +0200

    remove seeking clients button for external volunteer and add system test

--- test/system/volunteers_test.rb
+++ test/system/volunteers_test.rb
@@ -338,4 +338,22 @@ class VolunteersTest < ApplicationSystemTestCase
     visit edit_volunteer_path(@resigned)
     assert page.has_field? 'Prozess', disabled: true
   end
+
+  test 'external volunteer can not get machted with a client' do
+    @external = create :volunteer_external, acceptance: :accepted
+    visit volunteers_path
+
+    # "Klient/en" suchen button is not shown on volunteer index
+    within page.find('tr', text: @external.full_name) do
+      assert_text 'Nicht zuweisbar'
+    end
+
+    # "Klient/en" suchen button is not shown on volunteer show
+    visit volunteer_path(@external)
+    refute page.has_button? 'Klienten suchen'
+
+    # "Klient/en" suchen button is not shown on volunteer edit
+    visit edit_volunteer_path(@external)
+    refute page.has_button? 'Klienten suchen'
+  end
 end
