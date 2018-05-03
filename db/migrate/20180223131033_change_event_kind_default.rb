0f2e29a865fd7e3d749ba21a4f9c38ce9ee299e8
Kaspar Vollenweider <vok@panter.ch>
2018-05-03 11:31:58 +0200

    fixing migration to be revertable and migrating on virgin db not resulting in altering the schema

--- db/migrate/20180223131033_change_event_kind_default.rb
+++ db/migrate/20180223131033_change_event_kind_default.rb
@@ -1,5 +1,6 @@
 class ChangeEventKindDefault < ActiveRecord::Migration[5.1]
   def change
-    change_column_null(:events, :kind, from: true)
+    change_column_default(:events, :kind, from: false, to: nil)
+    change_column_null(:events, :kind, true)
   end
 end
