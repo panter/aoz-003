9b80360b1d021b38127a439f6fd71bd86d1d95b8
Kaspar Vollenweider <vok@panter.ch>
2018-05-14 11:41:14 +0200

    remove obsolete application helper include

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-  include ApplicationHelper
-
   test 'nationality_name_helper_does_not_crash_on_record_with_invalid_country_letters' do
     valid_nationality_client = create :client, nationality: 'ER'
     invalid_nationality_client = create :client, nationality: 'KO'
