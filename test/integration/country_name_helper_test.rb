479a064d88896833317b064b4600921ae15a5f15
Kaspar Vollenweider <vok@panter.ch>
2018-05-14 10:17:21 +0200

    test for helper not to crash on wrong nationality letters

---
+++ test/integration/country_name_helper_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class CountryNameHelperTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
+
+  test 'nationality_name_helper_does_not_crash_on_record_with_invalid_country_letters' do
+    valid_nationality_client = create :client, nationality: 'ER'
+    invalid_nationality_client = create :client, nationality: 'KO'
+    assert_match '', nationality_name(nil)
+    assert_match 'Eritrea', nationality_name(valid_nationality_client.nationality)
+    assert_match '', nationality_name(invalid_nationality_client.nationality)
+  end
+end
