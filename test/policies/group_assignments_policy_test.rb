ad09c17f7de758879aad234d4037b4029b301242
Tugce Nur Tas <tas.tugcenur@gmail.com>
2018-05-14 14:43:14 +0200

    adjust group assignment comments policy in views

--- test/policies/group_assignments_policy_test.rb
+++ test/policies/group_assignments_policy_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class GroupAssignmentPolicyTest < PolicyAssertions::Test
   test 'superadmin_can_use_all_actions' do
-    assert_permit(create(:user), GroupAssignment, *actions_list)
+    assert_permit(create(:user), GroupAssignment, *actions_list, 'comments?')
   end
 
   test 'department_manager_has_limited_access' do
@@ -10,9 +10,9 @@ class GroupAssignmentPolicyTest < PolicyAssertions::Test
     department_manager_group_assignment = create :group_assignment,
       group_offer: create(:group_offer, creator: department_manager)
     refute_permit(department_manager, GroupAssignment, *actions_list(:verify_termination))
-    assert_permit(department_manager, GroupAssignment, *actions_list(:terminated_index))
+    assert_permit(department_manager, GroupAssignment, *actions_list(:terminated_index), 'comments?')
     assert_permit(department_manager, department_manager_group_assignment,
-      *actions_list(except: [:verify_termination, :terminated_index]))
+      *actions_list(except: [:verify_termination, :terminated_index]), 'comments?')
     refute_permit(department_manager, create(:group_assignment),
       *actions_list(
         except: [:verify_termination, :terminated_index, :hours_and_feedbacks_submitted]
@@ -20,14 +20,14 @@ class GroupAssignmentPolicyTest < PolicyAssertions::Test
   end
 
   test 'social_worker_has_no_access' do
-    refute_permit(create(:social_worker), GroupAssignment, *actions_list)
+    refute_permit(create(:social_worker), GroupAssignment, *actions_list, 'comments?')
   end
 
   test 'volunteer_has_limited_access' do
     volunteer = create :volunteer_with_user
     volunteer_group_assignment =  create :group_assignment, volunteer: volunteer
     refute_permit(volunteer.user, GroupAssignment,
-      *actions_list(
+      'comments?', *actions_list(
         :verify_termination, :terminated_index, :edit, :set_end_today, :update
       ))
     assert_permit(volunteer.user, volunteer_group_assignment,
@@ -36,7 +36,7 @@ class GroupAssignmentPolicyTest < PolicyAssertions::Test
         :last_submitted_hours_and_feedbacks, :hours_and_feedbacks_submitted
       ))
     refute_permit(volunteer.user, create(:group_assignment),
-      *actions_list(
+      'comments?', *actions_list(
         :terminate, :update_submitted_at, :show, :update_terminated_at,
         :last_submitted_hours_and_feedbacks
       ))
