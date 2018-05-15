71c201ef3755a1e76fcecfecdee72c4fde781b41
Zsolt Benke <zsolt@decoding.io>
2018-05-15 16:24:28 +0200

    Test for superadmins editing other users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,8 +3,6 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @superadmin = create :user, role: 'superadmin'
-    @social_worker = create :user, role: 'social_worker'
-    @department_manager = create :department_manager
   end
 
   test "non superadmins should not be allowed to update others" do
@@ -44,16 +42,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "superadmin should be allowed to update other users" do
-    old_encrypted_password = @department_manager.encrypted_password
-    params = {
-      user: { email: 'somebody@example.com', password: 'sekret' }
-    }
+    roles = User::ROLES.dup
+    (roles - ['superadmin']).each_with_index do |role, i|
+      user = role == 'volunteer' ? create(:user, :volunteer) : create(:user)
+      params = {
+        user: { email: "somebody_#{i}@example.com", password: 'sekret' }
+      }
+      user.update role: role
+      old_encrypted_password = user.encrypted_password
 
-    login_as @superadmin
-    put user_path(@department_manager), params: params
+      login_as @superadmin
+      put user_path(user), params: params
 
-    refute_equal old_encrypted_password, @department_manager.reload.encrypted_password
-    assert_equal params[:user][:email], @department_manager.email
-    assert_response :redirect
+      refute_equal old_encrypted_password, user.reload.encrypted_password
+      assert_equal params[:user][:email], user.email
+      assert_response :redirect
+    end
   end
 end
