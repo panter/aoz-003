0fc93875a2f471f94a7ad25cde4db6e81f959d5b
Markus Koller <markus-koller@gmx.ch>
2018-05-15 11:42:09 +0200

    Tweak display of volunteer experience

--- app/controllers/concerns/volunteer_attributes.rb
+++ app/controllers/concerns/volunteer_attributes.rb
@@ -9,8 +9,9 @@ module VolunteerAttributes
         :skills, :interests, :man, :woman, :family, :kid, :teenager, :unaccompanied, :other_offer_desc,
         :trial_period, :doc_sent, :bank_account, :evaluation, :own_kids, :rejection_type,
         :rejection_text, :intro_course, :flexible, :morning, :afternoon, :evening, :working_percent,
-        :workday, :weekend, :detailed_description, contact_attributes,
-        language_skills_attributes, availability_attributes, group_offer_category_ids: []
+        :workday, :weekend, :detailed_description, :volunteer_experience_desc,
+        contact_attributes, language_skills_attributes, availability_attributes,
+        group_offer_category_ids: []
       ]
     end
   end
