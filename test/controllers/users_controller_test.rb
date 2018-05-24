6c08de5f5f564d6d2043fbe2ee05550f01a8fcfb
Zsolt Benke <zsolt@decoding.io>
2018-05-24 15:38:38 +0200

    Fix failing tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,11 +8,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "non superadmins should not be allowed to update others" do
     roles = User::ROLES.dup
     (roles - ['superadmin']).each_with_index do |role, i|
-      user = role == 'volunteer' ? create(:user, :volunteer) : create(:user)
       params = {
         user: { email: "somebody_#{i}@example.com", role: role }
       }
-      user.update role: role
+      user = create_user(role: role)
 
       login_as user
       put user_path(@superadmin), params: params
@@ -26,11 +25,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "non superadmins should not be allowed to edit their own rule" do
     roles = User::ROLES.dup
     (roles - ['superadmin']).each_with_index do |role, i|
-      user = role == 'volunteer' ? create(:user, :volunteer) : create(:user)
       params = {
         user: { email: "somebody_#{i}@example.com", role: :superadmin }
       }
-      user.update role: role
+      user = create_user(role: role)
 
       login_as user
       put user_path(user), params: params
@@ -44,11 +42,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "superadmin should be allowed to update other users" do
     roles = User::ROLES.dup
     (roles - ['superadmin']).each_with_index do |role, i|
-      user = role == 'volunteer' ? create(:user, :volunteer) : create(:user)
       params = {
         user: { email: "somebody_#{i}@example.com", password: 'sekret' }
       }
-      user.update role: role
+      user = create_user(role: role)
       old_encrypted_password = user.encrypted_password
 
       login_as @superadmin
@@ -58,5 +55,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       assert_equal params[:user][:email], user.email
       assert_response :redirect
     end
+  end
+
+  private
+
+  def create_user(role:)
+    user = nil
+
+    if role == 'volunteer'
+      user = create(:user, :volunteer)
+      user.volunteer = create :volunteer
+    else
+      user = create(:user)
+    end
+
+    user.update role: role
+    user
   end
 end
