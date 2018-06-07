5e1f3bda33dd7767b0408daa46a5a1108c2b7045
Zsolt Benke <zsolt@decoding.io>
2018-06-07 09:41:47 +0200

    Assign volunteers to departments on migrate

--- db/migrate/20180601121236_add_department_to_volunteers.rb
+++ db/migrate/20180601121236_add_department_to_volunteers.rb
@@ -1,5 +1,20 @@
 class AddDepartmentToVolunteers < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_reference :volunteers, :department, foreign_key: true
+
+    Volunteer.all.each do |volunteer|
+      department = volunteer.registrar&.department.last
+
+      if department.present?
+        volunteer.department = department
+        volunteer.save
+
+        puts "Assigned volunteer to #{department}"
+      end
+    end
+  end
+
+  def down
+    remove_reference :volunteers, :department
   end
 end
