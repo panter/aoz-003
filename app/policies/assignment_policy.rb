6a9727da0f1a09061378130a5feed99e3df701d0
Zsolt Benke <zsolt@decoding.io>
2018-06-18 14:40:21 +0200

    Social_worker has access to assignments PDFs

--- app/policies/assignment_policy.rb
+++ app/policies/assignment_policy.rb
@@ -7,18 +7,23 @@ class AssignmentPolicy < ApplicationPolicy
     end
   end
 
+  def superadmin_or_department_manager_creation_or_volunteer_or_social_worker_related?
+    superadmin_or_department_manager_creation_or_volunteer_related? ||
+      (record_present? && social_worker? && user.clients.include?(record.client))
+  end
+
   # controller action policies
-  alias_method :index?,                :superadmin_or_department_manager?
-  alias_method :terminated_index?,     :superadmin_or_department_manager?
-  alias_method :volunteer_search?,     :superadmin_or_department_manager?
-  alias_method :client_search?,        :superadmin_or_department_manager?
-  alias_method :new?,                  :superadmin_or_department_manager?
-  alias_method :create?,               :superadmin_or_department_manager?
-  alias_method :find_client?,          :superadmin_or_department_manager?
-  alias_method :show_comments?,             :superadmin_or_department_manager?
+  alias_method :index?,            :superadmin_or_department_manager?
+  alias_method :terminated_index?, :superadmin_or_department_manager?
+  alias_method :volunteer_search?, :superadmin_or_department_manager?
+  alias_method :client_search?,    :superadmin_or_department_manager?
+  alias_method :new?,              :superadmin_or_department_manager?
+  alias_method :create?,           :superadmin_or_department_manager?
+  alias_method :find_client?,      :superadmin_or_department_manager?
+  alias_method :show_comments?,    :superadmin_or_department_manager?
 
   alias_method :show?,
-    :superadmin_or_department_manager_creation_or_volunteer_related?
+    :superadmin_or_department_manager_creation_or_volunteer_or_social_worker_related?
   alias_method :edit?,
     :superadmin_or_department_manager_creation?
   alias_method :update?,
