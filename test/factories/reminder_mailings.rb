86e874df18713cc22131982e144f2907dfe056b9
Kaspar Vollenweider <vok@panter.ch>
2018-07-06 10:27:52 +0200

    fix reminder mailing factory
    - it doesn't create assignments if reminder mailables are passed
      to the factory
    - it creates assignments that suite the conditions needed for the
      reminder mailing kind (trial_period, half_year, termination)

--- test/factories/reminder_mailings.rb
+++ test/factories/reminder_mailings.rb
@@ -1,3 +1,19 @@
+def create_reminder_mailable(reminder_mailing, volunteer)
+  case reminder_mailing.kind
+  when 'half_year'
+    create(:assignment, volunteer: volunteer, creator: reminder_mailing.creator,
+      period_start: FFaker::Time.between(6.months.ago, 12.months.ago))
+  when 'trial_period'
+    create(:assignment, volunteer: volunteer, creator: reminder_mailing.creator,
+      period_start: FFaker::Time.between(6.weeks.ago, 8.weeks.ago))
+  when 'termination'
+    start_date = FFaker::Time.between(1.year.ago, 2.years.ago)
+    create(:assignment, volunteer: volunteer, creator: reminder_mailing.creator,
+      period_start: start_date,
+      period_end: FFaker::Time.between(start_date + 100.days, 2.days.ago))
+  end
+end
+
 FactoryBot.define do
   factory :reminder_mailing do
     association :creator, factory: :user
@@ -23,10 +39,15 @@ FactoryBot.define do
     end
 
     after(:build) do |reminder_mailing|
-      volunteer = create(:volunteer)
-      assignment = create(:assignment, volunteer: volunteer, period_start: 6.weeks.ago.to_date + 5)
-      reminder_mailing.reminder_mailing_volunteers << ReminderMailingVolunteer.new(
-        volunteer: volunteer, reminder_mailable: assignment, picked: true)
+      if reminder_mailing.reminder_mailing_volunteers.any?
+        reminder_mailing.reminder_mailing_volunteers.each { |rmv| rmv.picked = true }
+      else
+        volunteer = create(:volunteer)
+        assignment = create_reminder_mailable(reminder_mailing, volunteer)
+        reminder_mailing.reminder_mailing_volunteers << ReminderMailingVolunteer.new(
+          volunteer: volunteer, reminder_mailable: assignment, picked: true
+        )
+      end
     end
   end
 end
