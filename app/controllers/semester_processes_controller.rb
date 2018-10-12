c14910ceca473d834a02a968107ea21b67aa3c42
Kaspar Vollenweider <vok@panter.ch>
2018-10-12 11:15:16 +0200

    chore(semester-process): minor view fixes

--- app/controllers/semester_processes_controller.rb
+++ app/controllers/semester_processes_controller.rb
@@ -22,6 +22,7 @@ class SemesterProcessesController < ApplicationController
     @semester_process.creator = current_user
     @semester_process.build_semester_volunteers(@volunteers, selected_volunteers)
     @semester_process.build_volunteers_hours_feedbacks_and_mails
+
     authorize @semester_process
     if @semester_process.save
       redirect_to @semester_process, notice: 'Semester process was successfully created.'
