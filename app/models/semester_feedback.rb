401d291c35dc295380ce6a089a4d57f3f2252f71
Tugce Nur Tas <tas.tugcenur@gmail.com>
2018-11-14 00:24:30 +0100

    make 3/4 semester feedback fields required

--- app/models/semester_feedback.rb
+++ app/models/semester_feedback.rb
@@ -5,4 +5,6 @@ class SemesterFeedback < ApplicationRecord
   belongs_to :semester_process_volunteer
   has_one :semester_process, through: :semester_process_volunteer
   has_one :volunteer, through: :semester_process_volunteer
+
+  validates :goals, :achievements, :future, presence: true
 end
