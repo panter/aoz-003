0732004414bf32242de3d680f1e1ffea1eba3100
Jiri Strojil <j.strojil@gmail.com>
2018-12-10 20:13:33 +0100

    Fixed logic

--- app/models/semester_process_volunteer_mission.rb
+++ app/models/semester_process_volunteer_mission.rb
@@ -4,13 +4,13 @@ class SemesterProcessVolunteerMission < ApplicationRecord
   delegate :volunteer, to: :semester_process_volunteer
   belongs_to :semester_process_volunteer
 
-  scope :active_missions, lambda {
+  scope :need_feedback, lambda {
     joins(semester_process_volunteer: [:semester_process]).includes(:assignment, :group_assignment)
-    .where("semester_process_volunteer_missions.assignment_id IS NOT NULL AND 
-           (assignments.period_end IS NULL OR assignments.period_end >= lower(semester_processes.semester)) 
+    .where("(semester_process_volunteer_missions.assignment_id IS NOT NULL AND 
+           assignments.period_end IS NULL)
             OR 
-            semester_process_volunteer_missions.group_assignment_id IS NOT NULL AND 
-           (group_assignments.period_end is NULL OR group_assignments.period_end >= lower(semester_processes.semester))")
+           (semester_process_volunteer_missions.group_assignment_id IS NOT NULL AND 
+           group_assignments.period_end is NULL)")
     .references(:assignments, :group_assignments)
   }
 end
