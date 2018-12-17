5f1d5a96a02da9f19559d2337cca85e8e50e68e4
Jiri Strojil <j.strojil@gmail.com>
2018-12-17 16:44:12 +0100

    Fixed more tests

--- test/controllers/journals_controller_test.rb
+++ test/controllers/journals_controller_test.rb
@@ -6,19 +6,7 @@ class JournalsControllerTest < ActionDispatch::IntegrationTest
     @reminder_mailing = create :reminder_mailing, :trial_period
     @assignment = @reminder_mailing.reminder_mailing_volunteers.first.reminder_mailable
     @volunteer = @assignment.volunteer
-    @feedback = create :feedback, feedbackable: @assignment, volunteer: @volunteer, author: @volunteer.user
     login_as @superadmin
-  end
-
-  test '#new prefilles with feedback quote if feedback_id param is passed for trial period' do
-    get new_volunteer_journal_path(@volunteer, feedback_id: @feedback.id)
-    @journal = @controller.instance_variable_get(:@journal)
-    assert_equal @journal.category, 'feedback'
-    assert_equal @journal.title, "Feedback vom #{I18n.l(@feedback.created_at.to_date)}: "
-    assert @journal.body.include? @feedback.goals
-    assert @journal.body.include? @feedback.achievements
-    assert @journal.body.include? @feedback.future
-    assert @journal.body.include? @feedback.comments
   end
 
   test '#new prefilling for semester feedback quote if semester_feedback_id param is passed works also' do
