326ba492b1f654400e1cd8375a3a0de65e2414b6
hussain <hus@panter.ch>
2019-09-27 15:48:33 +0200

    fixed date format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -76,7 +76,7 @@ module ApplicationHelper
   end
 
   def request_params_filter(query)
-    search_parameters.deep_merge(q: query)
+    { q: search_parameters.deep_merge(query) }
   end
 
   def search_parameters
