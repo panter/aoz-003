b3b7ddc3a2e98c8159e6a33a2d28cf821ce5bd70
Kaspar Vollenweider <vok@panter.ch>
2020-05-18 19:40:32 +0200

    upgrade rails from 5.1 to 5.2

--- Gemfile
+++ Gemfile
@@ -5,8 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'rails', '~> 5.1'
-# gem 'rails', '>= 5.2.0', '< 6.0.0'
+gem 'rails', '>= 5.2.0', '< 6.0.0'
 
 gem 'autocomplete_rails'
 gem 'caxlsx'
--- config/application.rb
+++ config/application.rb
@@ -10,14 +10,15 @@ Bundler.require(*Rails.groups)
 module Aoz
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
     config.time_zone = 'Zurich'
 
     config.autoload_paths += Dir[config.root.join('lib/access_import/**/')]
     config.autoload_paths += Dir[config.root.join('lib/access_import/accessors/**/')]
     WillPaginate.per_page = 20
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
